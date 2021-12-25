c1b3a83ecdd5142087616c190952fd9e95fe6986
yukieiji <yukieiji@users.noreply.github.com>
2021-12-26 05:42:58 +0900

    fix:cast error, why did TOR use float?

--- ExtremeRoles/OptionsHolder.cs
+++ ExtremeRoles/OptionsHolder.cs
@@ -56,39 +56,40 @@ namespace ExtremeRoles
                 (int)CommonOptionKey.MinCremateRoles, Design.ColoedString(
                     new Color(204f / 255f, 204f / 255f, 0, 1f),
                     CommonOptionKey.MinCremateRoles.ToString()),
-                0f, 0f, VanillaMaxPlayerNum, 1f, null, true);
+                0, 0, VanillaMaxPlayerNum, 1, null, true);
             CustomOption.Create(
                 (int)CommonOptionKey.MaxCremateRoles, Design.ColoedString(
                     new Color(204f / 255f, 204f / 255f, 0, 1f),
                     CommonOptionKey.MaxCremateRoles.ToString()),
-                0f, 0f, VanillaMaxPlayerNum, 1f);
+                0, 0, VanillaMaxPlayerNum, 1);
 
             CustomOption.Create(
                 (int)CommonOptionKey.MinNeutralRoles, Design.ColoedString(
                     new Color(204f / 255f, 204f / 255f, 0, 1f),
                     CommonOptionKey.MinNeutralRoles.ToString()),
-                0f, 0f, VanillaMaxPlayerNum, 1f);
+                0, 0, VanillaMaxPlayerNum, 1);
             CustomOption.Create(
                 (int)CommonOptionKey.MaxNeutralRoles, Design.ColoedString(
                     new Color(204f / 255f, 204f / 255f, 0, 1f),
                     CommonOptionKey.MaxNeutralRoles.ToString()),
-                0f, 0f, VanillaMaxPlayerNum, 1f);
+                0, 0, VanillaMaxPlayerNum, 1);
 
             CustomOption.Create(
                 (int)CommonOptionKey.MinImpostorRoles, Design.ColoedString(
                     new Color(204f / 255f, 204f / 255f, 0, 1f),
                     CommonOptionKey.MinImpostorRoles.ToString()),
-                0f, 0f, 3f, 1f);
+                0, 0, 3, 1);
             CustomOption.Create(
                 (int)CommonOptionKey.MaxImpostorRoles, Design.ColoedString(
                     new Color(204f / 255f, 204f / 255f, 0, 1f),
                     CommonOptionKey.MaxImpostorRoles.ToString()),
-                0f, 0f, 3f, 1f);
+                0, 0, 3, 1);
 
             CustomOption.Create(
                 (int)CommonOptionKey.NumMeatings,
                 CommonOptionKey.NumMeatings.ToString(),
                 10, 0, VanillaMaxPlayerNum, 1, null, true);
+
             var blockMeating = CustomOption.Create(
                 (int)CommonOptionKey.DisableSkipInEmergencyMeeting,
                 CommonOptionKey.DisableSkipInEmergencyMeeting.ToString(), false);
