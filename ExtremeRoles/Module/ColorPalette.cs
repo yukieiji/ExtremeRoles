f502e594f7d5178b5c1beba08e6e83e65b1097a3
yukieiji <yukieiji@users.noreply.github.com>
2021-12-26 20:51:59 +0900

    fix:role color setting

--- ExtremeRoles/Module/ColorPalette.cs
+++ ExtremeRoles/Module/ColorPalette.cs
@@ -5,10 +5,10 @@ namespace ExtremeRoles.Module
     public class ColorPalette
     {
         public readonly static Color DefaultColor = Color.white;
-        public readonly static Color MarineBlue = new Color(0, 255, 255);
-        public readonly static Color JackalBlue = new Color(30, 144, 255);
-        public readonly static Color LoverPink = new Color(255, 20, 147);
-        public readonly static Color AliceGold = new Color(255, 215, 0);
+        public readonly static Color MarineBlue = new Color32(0, 255, 255, byte.MaxValue);
+        public readonly static Color JackalBlue = new Color32(65, 105, 255, byte.MaxValue);
+        public readonly static Color LoverPink = new Color32(255, 20, 147, byte.MaxValue);
+        public readonly static Color AliceGold = new Color32(255, 215, 0, byte.MaxValue);
         public readonly static Color NeutralColor = Color.gray;
     }
 }
