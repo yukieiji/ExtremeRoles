2f21c3ade9949d124c4266549feff840007cc0a9
yukieiji <yukieiji@users.noreply.github.com>
2022-01-12 01:19:04 +0900

    fix:vital and security using

---
+++ ExtremeRoles/Patches/MapModule/SystemConsolePatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+
+namespace ExtremeRoles.Patches.MapModule
+{
+    [HarmonyPatch(typeof(SystemConsole), nameof(SystemConsole.CanUse))]
+    public static class SystemConsoleCanUsePatch
+    {
+        public static bool Prefix(
+            ref float __result, SystemConsole __instance,
+            [HarmonyArgument(0)] GameData.PlayerInfo pc,
+            [HarmonyArgument(1)] out bool canUse, [HarmonyArgument(2)] out bool couldUse)
+        {
+            canUse = couldUse = false;
+            __result = float.MaxValue;
+
+            var role = Roles.ExtremeRoleManager.GameRole[pc.PlayerId];
+            var icon = __instance.useIcon;
+
+            if ((icon == ImageNames.CamsButton) && role.CanUseSecurity) { return true; }
+            if ((icon == ImageNames.VitalsButton) && role.CanUseSecurity) { return true; }
+
+            return false;
+        }
+    }
+}
