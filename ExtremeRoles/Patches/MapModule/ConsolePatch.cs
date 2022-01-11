6a8790b99d0fd4161b73adf2cd8543393855b8fc
yukieiji <yukieiji@users.noreply.github.com>
2022-01-11 22:37:35 +0900

    feat:sabotage block code

--- ExtremeRoles/Patches/MapModule/ConsolePatch.cs
+++ ExtremeRoles/Patches/MapModule/ConsolePatch.cs
@@ -13,12 +13,44 @@ namespace ExtremeRoles.Patches.MapModule
             canUse = couldUse = false;
             __result = float.MaxValue;
 
-            //if (IsBlocked(__instance, pc.Object)) return false;
             if (__instance.AllowImpostor) { return true; }
             if (Roles.ExtremeRoleManager.GameRole[pc.PlayerId].HasTask) { return true; }
 
             return false;
         }
     }
+    [HarmonyPatch(typeof(Console), nameof(Console.Use))]
+    public static class ConsoleUsePatch
+    {
+        public static bool Prefix(Console __instance)
+        {
 
+            PlayerControl player = PlayerControl.LocalPlayer;
+
+            if (isSabotageBlock(__instance, player)) { return false; }
+
+            return true;
+        }
+
+        private static bool isSabotageBlock(
+            Console console, PlayerControl player)
+        {
+            switch(console.FindTask(player).TaskType)
+            {
+                case TaskTypes.FixLights:
+                case TaskTypes.FixComms:
+                case TaskTypes.StopCharles:
+                case TaskTypes.ResetSeismic:
+                case TaskTypes.ResetReactor:
+                case TaskTypes.RestoreOxy:
+                    return !Roles.ExtremeRoleManager.GameRole[
+                        player.PlayerId].CanRepairSabotage;
+
+                default:
+                    return false;
+            }
+            
+        }
+
+    }
 }
