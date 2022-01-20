88d76c79f4e58efdb9222270b067a91603d53482
yukieiji <yukieiji@users.noreply.github.com>
2022-01-21 01:24:31 +0900

    feat:updater

--- ExtremeRoles/Patches/Manager/MainMenuManager.cs
+++ ExtremeRoles/Patches/Manager/MainMenuManager.cs
@@ -1,16 +1,62 @@
-﻿using HarmonyLib;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+using Newtonsoft.Json.Linq;
+
+using HarmonyLib;
 
 using TMPro;
 using Twitch;
 
 using UnityEngine;
+
+using ExtremeRoles.Helper;
 using ExtremeRoles.Resources;
+
 
 namespace ExtremeRoles.Patches.Manager
 {
     [HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
     public static class MainMenuManagerStartPatch
     {
+        public static void Prefix(MainMenuManager __instance)
+        {
+            var template = GameObject.Find("ExitGameButton");
+            if (template == null) { return; }
+
+            var button = UnityEngine.Object.Instantiate(template, null);
+            button.transform.localPosition = new Vector3(
+                button.transform.localPosition.x,
+                button.transform.localPosition.y + 0.6f,
+                button.transform.localPosition.z);
+
+            PassiveButton passiveButton = button.GetComponent<PassiveButton>();
+            passiveButton.OnClick = new UnityEngine.UI.Button.ButtonClickedEvent();
+            passiveButton.OnClick.AddListener((UnityEngine.Events.UnityAction)onClick);
+
+            var text = button.transform.GetChild(0).GetComponent<TMPro.TMP_Text>();
+            __instance.StartCoroutine(Effects.Lerp(0.1f, new Action<float>((p) => {
+                text.SetText(Translation.GetString("updateButton"));
+            })));
+            if (Updater.InfoPopup == null)
+            {
+                TwitchManager man = DestroyableSingleton<TwitchManager>.Instance;
+                Updater.InfoPopup = UnityEngine.Object.Instantiate<GenericPopup>(man.TwitchPopup);
+                Updater.InfoPopup.TextAreaTMP.fontSize *= 0.7f;
+                Updater.InfoPopup.TextAreaTMP.enableAutoSizing = false;
+            }
+
+            void onClick()
+            {
+                Updater.ExecuteCheckUpdate();
+            }
+        }
+
         public static void Postfix(MainMenuManager __instance)
         {
             DestroyableSingleton<ModManager>.Instance.ShowModStamp();
@@ -26,28 +72,211 @@ namespace ExtremeRoles.Patches.Manager
             torLogo.transform.position = Vector3.up;
             var renderer = torLogo.AddComponent<SpriteRenderer>();
             renderer.sprite = Loader.CreateSpriteFromResources(
-                Path.TitleBurner, 300f);
+                Resources.Path.TitleBurner, 300f);
 
             var tmp = __instance.Announcement.transform.Find(
                 "Title_Text").gameObject.GetComponent<TextMeshPro>();
             tmp.alignment = TextAlignmentOptions.Center;
             tmp.transform.localPosition += Vector3.left * 0.2f;
-            Module.Prefab.Text = Object.Instantiate(tmp);
-            Object.Destroy(Module.Prefab.Text.GetComponent<
+            Module.Prefab.Text = UnityEngine.Object.Instantiate(tmp);
+            UnityEngine.Object.Destroy(Module.Prefab.Text.GetComponent<
                 TextTranslatorTMP>());
             Module.Prefab.Text.gameObject.SetActive(false);
-            Object.DontDestroyOnLoad(Module.Prefab.Text);
+            UnityEngine.Object.DontDestroyOnLoad(Module.Prefab.Text);
 
             if (Module.Prefab.Prop == null)
             {
                 TwitchManager man = DestroyableSingleton<TwitchManager>.Instance;
-                Module.Prefab.Prop = Object.Instantiate(man.TwitchPopup);
-                Object.DontDestroyOnLoad(
+                Module.Prefab.Prop = UnityEngine.Object.Instantiate(man.TwitchPopup);
+                UnityEngine.Object.DontDestroyOnLoad(
                     Module.Prefab.Prop);
                 Module.Prefab.Prop.name = "propForInEx";
                 Module.Prefab.Prop.gameObject.SetActive(false);
             }
 
         }
+
+        public static class Updater
+        {
+            public static GenericPopup InfoPopup;
+
+            private static string updateUri = null;
+            
+            private const string checkUrl = "https://api.github.com/repos/yukieiji/ExtremeRoles/releases/latest";
+            private static bool hasUpdate = false;
+            private static Task updateTask = null;
+
+            public static void ExecuteCheckUpdate()
+            {
+                string info = Translation.GetString("chekUpdateWait");
+                InfoPopup.Show(info); // Show originally
+                
+                checkForUpdate().GetAwaiter().GetResult();
+                if (hasUpdate)
+                {
+                    setPopupText(Translation.GetString("updateNow"));
+                    clearOldVersions();
+
+                    if (updateTask == null)
+                    {
+                        if (updateUri != null)
+                        {
+                            updateTask = downloadUpdate();
+                        }
+                        else
+                        {
+                            info = Translation.GetString("updateManually");
+                        }
+                    }
+                    else
+                    {
+                        info = Translation.GetString("updateInProgress");
+                    }
+
+                    InfoPopup.StartCoroutine(
+                        Effects.Lerp(0.01f, new Action<float>((p) => { setPopupText(info); })));
+
+                }
+                else
+                {
+                    setPopupText(Translation.GetString("latestNow"));
+                }
+
+            }
+            private static void clearOldVersions()
+            {
+                try
+                {
+                    DirectoryInfo d = new DirectoryInfo(
+                        System.IO.Path.GetDirectoryName(
+                            Application.dataPath) + @"\BepInEx\plugins");
+                    string[] files = d.GetFiles("*.old").Select(x => x.FullName).ToArray(); // Getting old versions
+                    foreach (string f in files)
+                    {
+                        File.Delete(f);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logging.Error("Exception occured when clearing old versions:\n" + e);
+                }
+            }
+
+
+            private static async Task<bool> checkForUpdate()
+            {
+                try
+                {
+                    HttpClient http = new HttpClient();
+                    http.DefaultRequestHeaders.Add("User-Agent", "ExtremeRoles Updater");
+                    var response = await http.GetAsync(
+                        new Uri(checkUrl),
+                        HttpCompletionOption.ResponseContentRead);
+                    if (response.StatusCode != HttpStatusCode.OK || response.Content == null)
+                    {
+                        Logging.Error("Server returned no data: " + response.StatusCode.ToString());
+                        return false;
+                    }
+                    string json = await response.Content.ReadAsStringAsync();
+                    JObject data = JObject.Parse(json);
+
+                    string tagname = data["tag_name"]?.ToString();
+                    if (tagname == null)
+                    {
+                        return false; // Something went wrong
+                    }
+                    // check version
+                    Version ver = Version.Parse(tagname.Replace("v", ""));
+                    int diff = Assembly.GetExecutingAssembly().GetName().Version.CompareTo(ver);
+                    if (diff < 0)
+                    { // Update required
+                        hasUpdate = true;
+                        JToken assets = data["assets"];
+                        if (!assets.HasValues)
+                        {
+                            return false;
+                        }
+                        for (JToken current = assets.First; current != null; current = current.Next)
+                        {
+                            string browser_download_url = current["browser_download_url"]?.ToString();
+                            if (browser_download_url != null && current["content_type"] != null)
+                            {
+                                if (current["content_type"].ToString().Equals("application/x-msdownload") &&
+                                    browser_download_url.EndsWith(".dll"))
+                                {
+                                    updateUri = browser_download_url;
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logging.Error(ex.ToString());
+                }
+                return false;
+            }
+
+            public static async Task<bool> downloadUpdate()
+            {
+                try
+                {
+                    HttpClient http = new HttpClient();
+                    http.DefaultRequestHeaders.Add("User-Agent", "ExtremeRoles Updater");
+                    var response = await http.GetAsync(
+                        new Uri(updateUri),
+                        HttpCompletionOption.ResponseContentRead);
+                    if (response.StatusCode != HttpStatusCode.OK || response.Content == null)
+                    {
+                        Logging.Error("Server returned no data: " + response.StatusCode.ToString());
+                        return false;
+                    }
+                    string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                    UriBuilder uri = new UriBuilder(codeBase);
+                    string fullname = Uri.UnescapeDataString(uri.Path);
+                    if (File.Exists(fullname + ".old")) // Clear old file in case it wasnt;
+                        File.Delete(fullname + ".old");
+
+                    File.Move(fullname, fullname + ".old"); // rename current executable to old
+
+                    using (var responseStream = await response.Content.ReadAsStreamAsync())
+                    {
+                        using (var fileStream = File.Create(fullname))
+                        { // probably want to have proper name here
+                            responseStream.CopyTo(fileStream);
+                        }
+                    }
+                    showPopup(Translation.GetString("updateRestart"));
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Logging.Error(ex.ToString());
+                    showPopup(Translation.GetString("updateManually"));
+                }
+                return false;
+            }
+
+            private static void showPopup(string message)
+            {
+                setPopupText(message);
+                InfoPopup.gameObject.SetActive(true);
+            }
+
+            public static void setPopupText(string message)
+            {
+                if (InfoPopup == null)
+                {
+                    return;
+                }
+                
+                if (InfoPopup.TextAreaTMP != null)
+                {
+                    InfoPopup.TextAreaTMP.text = message;
+                }
+            }
+        }
+
     }
 }
