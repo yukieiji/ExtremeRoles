9b13ff05370c6d6a7d4e729a564cc013a0fc6f4e
yukieiji <yukieiji@users.noreply.github.com>
2022-01-24 00:09:48 +0900

    fix:air ship spawn show when Assassin Meeting on

---
+++ ExtremeRoles/Patches/AirShipStatusPatch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace ExtremeRoles.Patches
+{
+    [HarmonyPatch(typeof(AirshipStatus), nameof(AirshipStatus.PrespawnStep))]
+    public static class AirshipStatusPrespawnStepPatch
+    {
+        public static bool Prefix()
+        {
+            return !ExtremeRolesPlugin.GameDataStore.AssassinMeetingTrigger;
+        }
+	}
+}
