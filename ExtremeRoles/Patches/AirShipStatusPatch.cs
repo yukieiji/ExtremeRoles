ab012f8d3ddca8488777203de38cd0f8517dd197
yukieiji <yukieiji@users.noreply.github.com>
2022-01-24 00:19:36 +0900

    fix:null error

--- ExtremeRoles/Patches/AirShipStatusPatch.cs
+++ ExtremeRoles/Patches/AirShipStatusPatch.cs
@@ -5,7 +5,7 @@ namespace ExtremeRoles.Patches
     [HarmonyPatch(typeof(AirshipStatus), nameof(AirshipStatus.PrespawnStep))]
     public static class AirshipStatusPrespawnStepPatch
     {
-        public static bool Prefix()
+        public static bool Prefix(AirshipStatus __instance)
         {
             return !ExtremeRolesPlugin.GameDataStore.AssassinMeetingTrigger;
         }
