34482c3dd06c5ef1d9e540ee23a11cc4f2f0afd1
yukieiji <yukieiji@users.noreply.github.com>
2022-02-14 00:18:15 +0900

    fix:neet use door log

---
+++ ExtremeRoles/Patches/MiniGame/SecurityLogGamePatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using HarmonyLib;
+
+namespace ExtremeRoles.Patches.MiniGame
+{
+
+    [HarmonyPatch(typeof(SecurityLogGame), nameof(SecurityLogGame.Update))]
+    public class SecurityLogGameUpdatePatch
+    {
+        public static bool Prefix(SecurityLogGame __instance)
+        {
+            if (Roles.ExtremeRoleManager.GameRole.Count == 0) { return true; }
+
+            if (Roles.ExtremeRoleManager.GameRole[
+                PlayerControl.LocalPlayer.PlayerId].CanUseSecurity) { return true; }
+
+            __instance.EntryPool.ReclaimAll();
+            __instance.SabText.text = Helper.Translation.GetString("youDonotUse");
+            __instance.SabText.gameObject.SetActive(true);
+
+            return false;
+        }
+    }
+}
