098947683d7c80cf03b4364e5dcce7f5617d2abd
yukieiji <yukieiji@users.noreply.github.com>
2022-02-19 21:03:42 +0900

    feat:mary, but tmp

--- ExtremeRoles/Roles/Solo/Impostor/Mary.cs
+++ ExtremeRoles/Roles/Solo/Impostor/Mary.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 using UnityEngine;
 
@@ -17,25 +17,37 @@ namespace ExtremeRoles.Roles.Solo.Impostor
 {
     public class Mary : SingleRoleBase, IRoleAbility, IRoleUpdate
     {
-        public class MaryCamp
+        public class Camp
         {
-            private Vent vent;
+            private GameObject body;
 
             public MaryCamp(
+                int activeNum,
                 float activateRange,
-                bool canSee, Vector2 pos)
+                bool canSee,
+                Vector2 pos)
             {
                 
             }
+
+            public void Update(PlayerControl rolePlayer)
+            {
+
+            }
+
+            public Vent GetConvertedVent()
+            {
+
+            }
+
         }
 
 
         public enum MaryOption
         {
-            
+            ActiveNum,
+            ActiveRange
         }
-
-       
 
         public RoleAbilityButtonBase Button
         {
@@ -45,6 +57,12 @@ namespace ExtremeRoles.Roles.Solo.Impostor
                 this.bombButton = value;
             }
         }
+
+        public List<Vent> ActiveVent = new List<Vent>();
+        public List<Camp> NoneActiveVent = new List<Camp>();
+        public int ActiveNum;
+        public float ActiveRange;
+
         private RoleAbilityButtonBase bombButton;
 
 
@@ -54,14 +72,67 @@ namespace ExtremeRoles.Roles.Solo.Impostor
             ExtremeRoleId.Mary.ToString(),
             Palette.ImpostorRed,
             true, false, true, true)
+        { }
+
+
+        public static void SetCamp(byte callerId)
         {
+            var rolePlayer = Player.GetPlayerControlById(callerId);
+            var mary = (Mary)ExtremeRoleManager.GameRole[rolePlayer.PlayerId];
+            var localPlayerRole = ExtremeRoleManager.GetLocalPlayerRole();
+
+            bool isMarlin = localPlayerRole.Id == ExtremeRoleId.Marlin;
+
+            mary.NoneActiveVent.Add(
+                new MaryCamp(
+                    mary.ActiveNum,
+                    mary.ActiveRange,
+                    localPlayerRole.IsImpostor() || isMarlin,
+                    rolePlayer.GetTruePosition()));
+        }
+
+        public static void ActivateVent(
+            byte callerId, int activateVentIndex)
+        {
+            var rolePlayer = Player.GetPlayerControlById(callerId);
+            var mary = (Mary)ExtremeRoleManager.GameRole[rolePlayer.PlayerId];
+
+            Vent newVent = mary.NoneActiveVent[activateVentIndex].GetConvertedVent();
+
+            if (mary.ActiveVent.Count > 0)
+            {
+                var leftVent = mary.ActiveVent[^1];
+                newVent.Left = leftVent;
+                leftVent.Right = newVent;
+            }
+            else
+            {
+                newVent.Left = null;
+            }
+
+            newVent.Right = null;
+            newVent.Center = null;
+
+            var allVents = ShipStatus.Instance.AllVents.ToList();
+            allVents.Add(newVent);
+            ShipStatus.Instance.AllVents = allVents.ToArray();
+
+            mary.ActiveVent.Add(newVent);
+
+            mary.NoneActiveVent.RemoveAt(activateVentIndex);
+
+        }
+
+        public static void StartVentAnimation()
+        {
+
         }
 
         public void CreateAbility()
         {
 
             this.CreateAbilityCountButton(
-                Translation.GetString("setBomb"),
+                Translation.GetString("setCamp"),
                 Loader.CreateSpriteFromResources(
                     Path.TestButton),
                 abilityCleanUp: CleanUp);
@@ -87,13 +158,32 @@ namespace ExtremeRoles.Roles.Solo.Impostor
             CustomOptionBase parentOps)
         {
             this.CreateAbilityCountOption(
-                parentOps, 2, 5, 2.5f);
-            
+                parentOps, 3, 5, 2.5f);
+            CustomOption.Create(
+                GetRoleOptionId((int)MaryOption.ActiveNum),
+                string.Concat(
+                    this.RoleName,
+                    MaryOption.ActiveNum.ToString()),
+                2, 1, 4, 1, parentOps);
+            CustomOption.Create(
+                GetRoleOptionId((int)MaryOption.ActiveRange),
+                string.Concat(
+                    this.RoleName,
+                    MaryOption.ActiveRange.ToString()),
+                1.0f, 0.1f, 3.0f, 0.1f, parentOps);
         }
 
         protected override void RoleSpecificInit()
         {
             this.RoleAbilityInit();
+
+            var allOption = OptionHolder.AllOption;
+
+            this.ActiveNum = allOption[
+                GetRoleOptionId((int)MaryOption.ActiveNum)].GetValue();
+            this.ActiveRange = allOption[
+                GetRoleOptionId((int)MaryOption.ActiveRange)].GetValue();
+
         }
 
         public void RoleAbilityResetOnMeetingStart()
@@ -108,7 +198,10 @@ namespace ExtremeRoles.Roles.Solo.Impostor
 
         public void Update(PlayerControl rolePlayer)
         {
-            
+            foreach(var camp in this.NoneActiveVent)
+            {
+                camp.Update(rolePlayer);
+            }
         }
     }
 }
