c4564555a6be47ca1742ecd751914110856ec372
yukieiji <yukieiji@users.noreply.github.com>
2022-03-04 13:06:52 +0900

    feat:load code

---
+++ ExtremeSkins/Patches/Manager/MainMenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Linq;
+using HarmonyLib;
+
+
+
+namespace ExtremeRoles.Patches.Manager
+{
+    [HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
+    public static class MainMenuManagerStartPatch
+    {
+        public static void Postfix(MainMenuManager __instance)
+        {
+            ExtremeSkins.ExtremeHatManager.CheckUpdate();
+
+
+
+
+            ExtremeSkins.ExtremeHatManager.Load();
+        }
+    }
+}
