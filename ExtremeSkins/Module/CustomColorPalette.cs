31270c40c5af407e82bca66ca7231fbd989dde08
yukieiji <yukieiji@users.noreply.github.com>
2022-03-05 00:03:11 +0900

    feat:new color

--- ExtremeSkins/Module/CustomColorPalette.cs
+++ ExtremeSkins/Module/CustomColorPalette.cs
@@ -14,7 +14,12 @@ namespace ExtremeSkins.Module
 
         public static readonly List<ColorData> CustomColor = new List<ColorData>()
         {
-            
+            new ColorData()
+            { 
+                Name = "testColor",
+                MainColor = new Color32(0xF7, 0x44, 0x17, byte.MaxValue),
+                ShadowColor = new Color32(0x9B, 0x2E, 0x0F, byte.MaxValue),
+            }
         };
     }
 }
---
+++ ExtremeSkins/Patches/Tab/PlayerTabPatch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using UnhollowerBaseLib;
+
+using UnityEngine;
+
+namespace ExtremeSkins.Patches.Tab
+{
+    [HarmonyPatch(typeof(PlayerTab), nameof(PlayerTab.OnEnable))]
+    public static class PlayerTabEnablePatch
+    {
+        public static void Postfix(PlayerTab __instance)
+        {   
+            // Replace instead
+            Il2CppArrayBase<ColorChip> chips = __instance.ColorChips.ToArray();
+
+            int cols = 7;
+
+            for (int i = 0; i < chips.Count; i++)
+            {
+                ColorChip chip = chips[i];
+                int row = i / cols, col = i % cols; // Dynamically do the positioning
+                chip.transform.localPosition = new Vector3(
+                    -0.975f + (col * 0.485f),
+                    1.475f - (row * 0.49f),
+                    chip.transform.localPosition.z);
+                chip.transform.localScale *= 0.78f;
+            }
+        }
+    }
+}
