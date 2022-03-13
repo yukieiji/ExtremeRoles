175c6cd1022253ea9de4f75d950e9bb3eadbeaaa
yukieiji <yukieiji@users.noreply.github.com>
2022-03-13 19:55:56 +0900

    feat:replace rng

---
+++ ExtremeRoles/Patches/HashRandomPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+
+namespace ExtremeRoles.Patches
+{
+    [HarmonyPatch(typeof(HashRandom), nameof(HashRandom.FastNext))]
+    public static class HashRandomFastNextPatch
+    {
+        public static bool Prefix(
+            ref int __result,
+            [HarmonyArgument(0)] int maxInt)
+        {
+            if (OptionHolder.AllOption[
+                (int)OptionHolder.CommonOptionKey.UseStrongRandomGen].GetValue())
+            {
+                __result = RandomGenerator.Instance.Next(maxInt);
+                return false;
+            }
+            return true;
+        }
+    }
+}
