5e1923112fb4ca622422694d47e0129134d92194
yukieiji <yukieiji@users.noreply.github.com>
2022-03-15 21:35:17 +0900

    feat: bakary new option, cooking time can change

--- ExtremeRoles/Roles/Solo/Crewmate/Bakary.cs
+++ ExtremeRoles/Roles/Solo/Crewmate/Bakary.cs
@@ -7,7 +7,9 @@ namespace ExtremeRoles.Roles.Solo.Crewmate
     {
         public enum BakaryOption
         {
-            ChangeCooking
+            ChangeCooking,
+            GoodBakeTime,
+            BadBakeTime,
         }
 
         public Bakary() : base(
@@ -21,18 +23,37 @@ namespace ExtremeRoles.Roles.Solo.Crewmate
         protected override void CreateSpecificOption(
             CustomOptionBase parentOps)
         {
-            CustomOption.Create(
+            var changeCooking = CustomOption.Create(
                 GetRoleOptionId((int)BakaryOption.ChangeCooking),
                 string.Concat(
                     this.RoleName,
                     BakaryOption.ChangeCooking.ToString()),
                 true, parentOps);
+            CustomOption.Create(
+                GetRoleOptionId((int)BakaryOption.GoodBakeTime),
+                string.Concat(
+                    this.RoleName,
+                    BakaryOption.GoodBakeTime.ToString()),
+                60.0f, 45.0f, 75.0f, 0.5f,
+                changeCooking, format: "unitSeconds", invert: true);
+            CustomOption.Create(
+                GetRoleOptionId((int)BakaryOption.BadBakeTime),
+                string.Concat(
+                    this.RoleName,
+                    BakaryOption.BadBakeTime.ToString()),
+                120.0f, 105.0f, 135.0f, 0.5f,
+                changeCooking, format: "unitSeconds", invert: true);
         }
 
         protected override void RoleSpecificInit()
         {
-            ExtremeRolesPlugin.GameDataStore.Union.SetCooking(OptionHolder.AllOption[
-                GetRoleOptionId((int)BakaryOption.ChangeCooking)].GetValue());
+            ExtremeRolesPlugin.GameDataStore.Union.SetCookingCondition(
+                OptionHolder.AllOption[
+                    GetRoleOptionId((int)BakaryOption.GoodBakeTime)].GetValue(),
+                OptionHolder.AllOption[
+                    GetRoleOptionId((int)BakaryOption.BadBakeTime)].GetValue(),
+                OptionHolder.AllOption[
+                    GetRoleOptionId((int)BakaryOption.ChangeCooking)].GetValue());
         }
     }
 }
