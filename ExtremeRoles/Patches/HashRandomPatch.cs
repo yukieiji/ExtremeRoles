c80d7628f0f2cb5095e90cab7180e962dabf586b
yukieiji <yukieiji@users.noreply.github.com>
2022-03-27 03:12:56 +0900

    feat:use hash random next replace pcg

--- ExtremeRoles/Patches/HashRandomPatch.cs
+++ ExtremeRoles/Patches/HashRandomPatch.cs
@@ -1,9 +1,29 @@
-﻿using HarmonyLib;
+﻿using System;
+using HarmonyLib;
 
 namespace ExtremeRoles.Patches
 {
     [HarmonyPatch(typeof(HashRandom), nameof(HashRandom.FastNext))]
     public static class HashRandomFastNextPatch
+    {
+        public static bool Prefix(
+            ref int __result,
+            [HarmonyArgument(0)] int maxInt)
+        {
+            if (OptionHolder.AllOption[
+                (int)OptionHolder.CommonOptionKey.UseStrongRandomGen].GetValue())
+            {
+                __result = RandomGenerator.Instance.Next(maxInt);
+                return false;
+            }
+            return true;
+        }
+    }
+    [HarmonyPatch(
+        typeof(HashRandom),
+        nameof(HashRandom.Next),
+        new Type[] { typeof(int) })]
+    public static class HashRandomNextPatch
     {
         public static bool Prefix(
             ref int __result,
