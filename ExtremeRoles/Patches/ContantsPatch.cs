c0bc86a6c05fc80ef431767bb6864c511388ef17
yukieiji <yukieiji@users.noreply.github.com>
2022-04-12 00:56:00 +0900

    remove:delks

--- ExtremeRoles/Patches/ContantsPatch.cs
+++ ExtremeRoles/Patches/ContantsPatch.cs
@@ -2,17 +2,6 @@
 
 namespace ExtremeRoles.Patches
 {
-    [HarmonyPatch(typeof(Constants), nameof(Constants.ShouldFlipSkeld))]
-    class ConstantsShouldFlipSkeldPatch
-    {
-        public static bool Prefix(ref bool __result)
-        {
-            if (PlayerControl.GameOptions == null) { return true; }
-            __result = PlayerControl.GameOptions.MapId == 3;
-            return false;
-        }
-    }
-
     [HarmonyPatch(typeof(Constants), nameof(Constants.ShouldHorseAround))]
     class ConstantsShouldHorseAroundPatch
     {
