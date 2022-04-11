18196af6c3b5b06e18c1ad901270a51afdad8a4b
yukieiji <yukieiji@users.noreply.github.com>
2022-04-12 02:14:19 +0900

    fix:map select option can't show when online

--- ExtremeRoles/Patches/Option/GameSettingMenuPatch.cs
+++ ExtremeRoles/Patches/Option/GameSettingMenuPatch.cs
@@ -19,8 +19,9 @@ namespace ExtremeRoles.Patches.Option
         {
             // Setup mapNameTransform
             var mapNameTransform = __instance.AllItems.FirstOrDefault(
-                x => x.gameObject.activeSelf && x.name.Equals("MapName", StringComparison.OrdinalIgnoreCase));
-            if (mapNameTransform == null) return;
+                x => x.name.Equals("MapName", StringComparison.OrdinalIgnoreCase));
+
+            if (mapNameTransform == null) { return; }
 
             var options = new Il2CppSystem.Collections.Generic.List<Il2CppSystem.Collections.Generic.KeyValuePair<string, int>>();
             for (int i = 0; i < Constants.MapNames.Length; ++i)
@@ -31,6 +32,29 @@ namespace ExtremeRoles.Patches.Option
                 options.Add(kvp);
             }
             mapNameTransform.GetComponent<KeyValueOption>().Values = options;
+            mapNameTransform.gameObject.SetActive(true);
+
+            if (AmongUsClient.Instance.GameMode != GameModes.OnlineGame) { return; }
+
+            foreach (Transform opt in __instance.AllItems.ToList())
+            {
+                float offset = -0.5f;
+                string name = opt.name;
+
+                if (name.Equals("MapName", StringComparison.OrdinalIgnoreCase))
+                {
+                    offset = 0.25f;
+                }
+                if (name.Equals("ResetToDefault", StringComparison.OrdinalIgnoreCase))
+                {
+                    offset = 0f;
+                }
+                opt.position += new Vector3(0, offset, 0);
+            }
+
+            __instance.Scroller.ContentYBounds.max += 0.5F;
+
+
         }
     }
 }
