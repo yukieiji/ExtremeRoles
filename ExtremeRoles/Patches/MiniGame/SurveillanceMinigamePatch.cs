549b35ed2e384dc1ff8b43e3c1a691e80838fb6d
yukieiji <yukieiji@users.noreply.github.com>
2022-05-20 22:48:22 +0900

    fix:skeld camera

--- ExtremeRoles/Patches/MiniGame/SurveillanceMinigamePatch.cs
+++ ExtremeRoles/Patches/MiniGame/SurveillanceMinigamePatch.cs
@@ -10,6 +10,9 @@ namespace ExtremeRoles.Patches.MiniGame
     {
         public static void Postfix(SurveillanceMinigame __instance)
         {
+            SurveillanceMinigameUpdatePatch.Timer = SurveillanceMinigameUpdatePatch.ChangeTime;
+            SurveillanceMinigameUpdatePatch.Page = 0;
+
             if (ShipStatus.Instance.AllCameras.Length > 4 && __instance.FilteredRooms.Length > 0)
             {
                 __instance.textures = __instance.textures.ToList().Concat(
@@ -32,12 +35,20 @@ namespace ExtremeRoles.Patches.MiniGame
     [HarmonyPatch(typeof(SurveillanceMinigame), nameof(SurveillanceMinigame.Update))]
     public static class SurveillanceMinigameUpdatePatch
     {
+        public const float ChangeTime = 4.0f;
+        public static float Timer;
+        public static int Page;
+
         public static bool Prefix(SurveillanceMinigame __instance)
         {
             if (Roles.ExtremeRoleManager.GameRole.Count == 0) { return true; }
 
             if (Roles.ExtremeRoleManager.GameRole[
-                PlayerControl.LocalPlayer.PlayerId].CanUseSecurity) { return true; }
+                PlayerControl.LocalPlayer.PlayerId].CanUseSecurity)
+            {
+                updateCamera(__instance);
+                return false;
+            }
 
             __instance.isStatic = true;
             for (int i = 0; i < __instance.ViewPorts.Length; ++i)
@@ -49,5 +60,55 @@ namespace ExtremeRoles.Patches.MiniGame
 
             return false;
         }
+
+        private static void updateCamera(SurveillanceMinigame instance)
+        {
+            Timer -= Time.deltaTime;
+            int numberOfPages = Mathf.CeilToInt(ShipStatus.Instance.AllCameras.Length / 4f);
+
+            bool update = false;
+
+            if (Timer < 0.0f || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                update = true;
+                Timer = ChangeTime;
+                Page = (Page + 1) % numberOfPages;
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                Page = (Page + numberOfPages - 1) % numberOfPages;
+                update = true;
+                Timer = ChangeTime;
+            }
+
+            if ((instance.isStatic || update) && !PlayerTask.PlayerHasTaskOfType<IHudOverrideTask>(PlayerControl.LocalPlayer))
+            {
+                instance.isStatic = false;
+                for (int i = 0; i < instance.ViewPorts.Length; i++)
+                {
+                    instance.ViewPorts[i].sharedMaterial = instance.DefaultMaterial;
+                    instance.SabText[i].gameObject.SetActive(false);
+                    if (Page * 4 + i < instance.textures.Length)
+                    {
+                        instance.ViewPorts[i].material.SetTexture(
+                            "_MainTex", instance.textures[Page * 4 + i]);
+                    }
+                    else
+                    {
+                        instance.ViewPorts[i].sharedMaterial = instance.StaticMaterial;
+                    }
+                }
+            }
+            else if (!instance.isStatic && PlayerTask.PlayerHasTaskOfType<HudOverrideTask>(PlayerControl.LocalPlayer))
+            {
+                instance.isStatic = true;
+                for (int j = 0; j < instance.ViewPorts.Length; j++)
+                {
+                    instance.ViewPorts[j].sharedMaterial = instance.StaticMaterial;
+                    instance.SabText[j].gameObject.SetActive(true);
+                }
+            }
+        }
+
     }
 }
