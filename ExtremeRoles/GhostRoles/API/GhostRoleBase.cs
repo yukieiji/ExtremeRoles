fe6e37588c872e76958a13b7c9a5942e649c8967
yukieiji <yukieiji@users.noreply.github.com>
2022-05-22 17:31:42 +0900

    feat:GhostRole templates

---
+++ ExtremeRoles/GhostRoles/API/GhostRoleBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtremeRoles.GhostRoles.API
+{
+    public class GhostRoleBase
+    {
+    }
+}
---
+++ ExtremeRoles/GhostRoles/ExtremeGhostRoleManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+
+using ExtremeRoles.GhostRoles.API;
+
+namespace ExtremeRoles.GhostRoles
+{
+    public enum GhostRoleId
+    {
+
+    }
+
+    public class ExtremeGhostRoleManager
+    {
+        public struct GhostRoleAssignData
+        {
+
+        }
+
+        public static Dictionary<byte, GhostRoleBase> GameRole = new Dictionary<byte, GhostRoleBase>();
+
+        public static readonly Dictionary<GhostRoleId, GhostRoleBase> AllGhostRole = new Dictionary<GhostRoleId, GhostRoleBase>();
+
+        private static GhostRoleAssignData assignData;
+
+        public static void AssignGhostRoleToPlayer(PlayerControl player)
+        {
+
+        }
+
+        public static void CreateGhostRoleOption()
+        {
+
+        }
+
+        public static GhostRoleBase GetLocalPlayerGhostRole()
+        {
+            byte playerId = PlayerControl.LocalPlayer.PlayerId;
+
+            if (!GameRole.ContainsKey(playerId))
+            {
+                return null;
+            }
+            else
+            {
+                return GameRole[playerId];
+            }
+        }
+        public static T GetSafeCastedGhostRole<T>(byte playerId) where T : GhostRoleBase
+        {
+            if (!GameRole.ContainsKey(playerId)) { return null; }
+
+            var role = GameRole[playerId] as T;
+
+            if (role != null)
+            {
+                return role;
+            }
+
+            return null;
+
+        }
+
+        public static T GetSafeCastedLocalPlayerRole<T>() where T : GhostRoleBase
+        {
+
+            byte playerId = PlayerControl.LocalPlayer.PlayerId;
+
+            if (!GameRole.ContainsKey(playerId)) { return null; }
+
+            var role = GameRole[playerId] as T;
+
+            if (role != null)
+            {
+                return role;
+            }
+
+            return null;
+
+        }
+
+        public static void Initialize()
+        {
+            GameRole.Clear();
+        }
+
+        public static void SetGhostRoleAssignData(
+            GhostRoleAssignData data)
+        {
+            assignData = data;
+        }
+
+        public static void SetGhostRoleToPlayerId(byte playerId)
+        {
+
+        }
+    }
+}
