99e1dd1d7a5392ce276f5b28152fa2f8345f4c8b
yukieiji <yukieiji@users.noreply.github.com>
2022-06-12 21:44:51 +0900

    feat:performance utiles

---
+++ ExtremeRoles/Performance/CachedPlayerControl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+// from TOR : https://github.com/Eisbison/TheOtherRoles/blob/main/TheOtherRoles/Players/CachedPlayer.cs
+
+namespace ExtremeRoles.Performance
+{
+    public class CachedPlayerControl
+	{
+		public static readonly Dictionary<IntPtr, CachedPlayerControl> PlayerPtrs = new Dictionary<IntPtr, CachedPlayerControl>();
+		public static readonly List<CachedPlayerControl> AllPlayers = new List<CachedPlayerControl>();
+		public static CachedPlayerControl LocalPlayer;
+
+		public Transform transform;
+		public PlayerControl PlayerControl;
+		public PlayerPhysics PlayerPhysics;
+		public CustomNetworkTransform NetTransform;
+		public GameData.PlayerInfo Data;
+		public byte PlayerId;
+
+		public CachedPlayerControl(PlayerControl pc)
+        {
+			transform = pc.transform;
+			PlayerControl = pc;
+			PlayerPhysics = pc.MyPhysics;
+            NetTransform = pc.NetTransform;
+			AllPlayers.Add(this);
+			PlayerPtrs[pc.Pointer] = this;
+		}
+
+		public static implicit operator bool(CachedPlayerControl player)
+		{
+			return player != null && player.PlayerControl;
+		}
+
+		public static implicit operator PlayerControl(CachedPlayerControl player) => player.PlayerControl;
+
+		public static implicit operator PlayerPhysics(CachedPlayerControl player) => player.PlayerPhysics;
+	}
+}
---
+++ ExtremeRoles/Performance/CachedShipStatus.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Il2CppSystem;
+
+// from TOR : https://github.com/Eisbison/TheOtherRoles/blob/main/TheOtherRoles/Utilities/MapUtilities.cs
+
+namespace ExtremeRoles.Performance
+{
+    public static class CachedShipStatus
+    {
+        public static ShipStatus Instance { get; set; }
+        public static Dictionary<SystemTypes, Object> Systems
+        {
+            get
+            {
+                if (allSystems.Count == 0) { setSystem(); }
+                return allSystems;
+            }
+        }
+        private static readonly Dictionary<SystemTypes, Object> allSystems = new Dictionary<SystemTypes, Object>();
+        
+        public static void SetUp(ShipStatus instance)
+        {
+            Instance = instance;
+        }
+
+        public static void Destroy()
+        {
+            Instance = null;
+            allSystems.Clear();
+        }
+
+        private static void setSystem()
+        {
+            if (!Instance) { return; }
+
+            var systems = Instance.Systems;
+            if (systems.Count <= 0) { return; }
+
+            foreach (var systemTypes in SystemTypeHelpers.AllTypes)
+            {
+                if (!systems.ContainsKey(systemTypes)) { continue; }
+                allSystems[systemTypes] = systems[systemTypes].TryCast<Object>();
+            }
+        }
+    }
+}
