e22d78fdc4a9d00a66c5eaddaec869bf45d614d3
yukieiji <yukieiji@users.noreply.github.com>
2022-06-12 15:57:24 +0900

    refactor:arrow

--- ExtremeRoles/Module/Arrow.cs
+++ ExtremeRoles/Module/Arrow.cs
@@ -13,8 +13,9 @@ namespace ExtremeRoles.Module
         private const float yMaxSize = 0.525f;
 
         private Vector3 target;
-        private SpriteRenderer image;
         private GameObject body;
+        private SpriteRenderer image;
+        private ArrowBehaviour arrowBehaviour;
 
         public Arrow(Color color)
         {
@@ -24,8 +25,11 @@ namespace ExtremeRoles.Module
             if (Prefab.Arrow != null)
             {
                 this.image.sprite = Prefab.Arrow;
+                this.body.transform.localScale = new Vector3(xzMaxSize, yMaxSize, xzMaxSize);
             }
             this.image.color = color;
+            this.arrowBehaviour = this.body.AddComponent<ArrowBehaviour>();
+            this.arrowBehaviour.image = this.image;
         }
 
         public void Update()
@@ -52,35 +56,8 @@ namespace ExtremeRoles.Module
                 this.target = target.Value;
             }
 
-            Camera main = Camera.main;
-
-            Vector2 vector = this.target - main.transform.position;
-            float num = vector.magnitude / (main.orthographicSize * 0.65f);
-            this.image.enabled = ((double)num > 0.3);
-            Vector2 vector2 = main.WorldToViewportPoint(this.target);
-
-            if (between(vector2.x, 0f, 1f) && between(vector2.y, 0f, 1f))
-            {
-                this.body.transform.position = this.target - (Vector3)vector.normalized * 0.6f;
-                float xzSize = Mathf.Clamp(num, 0f, xzMaxSize);
-                float ySize = Mathf.Clamp(num, 0f, yMaxSize);
-                this.body.transform.localScale = new Vector3(xzSize, ySize, xzSize);
-            }
-            else
-            {
-                Vector2 vector3 = new Vector2(
-                    Mathf.Clamp(vector2.x * 2f - 1f, -1f, 1f),
-                    Mathf.Clamp(vector2.y * 2f - 1f, -1f, 1f));
-                float orthographicSize = main.orthographicSize;
-                float num3 = main.orthographicSize * main.aspect;
-                Vector3 vector4 = new Vector3(
-                    Mathf.LerpUnclamped(0f, num3 * 0.88f, vector3.x),
-                    Mathf.LerpUnclamped(0f, orthographicSize * 0.79f, vector3.y), 0f);
-                this.body.transform.position = main.transform.position + vector4;
-                this.body.transform.localScale = new Vector3(xzMaxSize, yMaxSize, xzMaxSize);
-            }
-
-            lookAt2d(this.body.transform, this.target);
+            this.arrowBehaviour.target = this.target;
+            this.arrowBehaviour.Update();
         }
 
         public void Clear()
@@ -93,23 +70,6 @@ namespace ExtremeRoles.Module
             {
                 this.body.SetActive(active);
             }
-        }
-
-        private bool between(float value, float min, float max)
-        {
-            return value > min && value < max;
-        }
-
-        private void lookAt2d(Transform transform, Vector3 target)
-        {
-            Vector3 vector = target - transform.position;
-            vector.Normalize();
-            float num = Mathf.Atan2(vector.y, vector.x);
-            if (transform.lossyScale.x < 0f)
-            {
-                num += 3.1415927f;
-            }
-            transform.rotation = Quaternion.Euler(0f, 0f, num * 57.29578f);
         }
     }
 }
