6e9be4f71c127ae67374dafc0d225fa2d1da2b62
yukieiji <yukieiji@users.noreply.github.com>
2022-06-19 04:28:35 +0900

    change:comands

--- ExtremeRoles/Compat/BepInExInstaller.bat
+++ ExtremeRoles/Compat/BepInExInstaller.bat
@@ -1,11 +1,9 @@
-﻿@echo off
-echo "Killing AmongUs......"
+﻿chcp 65001
+
 taskkill /im "Among Us.exe"
 
-echo "Waiting 10 seconds"
 powershell sleep 10
 
-echo "Remove Old BepInEx......"
 rd /s /q %2\BepInEx\core\
 rd /s /q %2\BepInEx\unhollowed\
 rd /s /q %2\BepInEx\unity-libs\
@@ -14,8 +12,4 @@ del /q %2\changelog.txt
 del /q %2\doorstop_config.ini
 del /q %2\winhttp.dll
 
-echo "BepInEx Installing...."
 xcopy /q /s /e /c /-y %1 %2
-
-echo "Update Complete!! Please Restart Game"
-pause
