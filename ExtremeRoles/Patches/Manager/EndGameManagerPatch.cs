3988ec9dca72c019587ec511b628a743afcaf03e
yukieiji <yukieiji@users.noreply.github.com>
2022-06-22 22:54:05 +0900

    fix:ghost

--- ExtremeRoles/Patches/Manager/EndGameManagerPatch.cs
+++ ExtremeRoles/Patches/Manager/EndGameManagerPatch.cs
@@ -76,6 +76,8 @@ namespace ExtremeRoles.Patches.Manager
 
                 if (winningPlayerData.IsDead)
                 {
+                    poolablePlayer.cosmetics.currentBodySprite.BodySprite.sprite =
+                        poolablePlayer.cosmetics.currentBodySprite.GhostSprite;
                     poolablePlayer.cosmetics.SetGhost();
                     poolablePlayer.SetDeadFlipX(i % 2 == 0);
                 }
