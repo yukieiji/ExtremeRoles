8603533495bfe1d7de60659345b0e60aa57e28ac
yukieiji <yukieiji@users.noreply.github.com>
2022-06-27 13:36:50 +0900

    fix:postion

--- ExtremeRoles/Patches/VersionShowerPatch.cs
+++ ExtremeRoles/Patches/VersionShowerPatch.cs
@@ -14,8 +14,10 @@ namespace ExtremeRoles.Patches
             var amongUsLogo = GameObject.Find("bannerLogo_AmongUs");
             if (amongUsLogo == null) { return; }
 
-            var modTitle = Object.Instantiate(__instance.text);
-            modTitle.transform.position = new Vector3(0, 0.3f, 0);
+            var modTitle = Object.Instantiate(
+                __instance.text, amongUsLogo.transform);
+            modTitle.transform.localPosition = new Vector3(0, -2.1575f, 0);
+            modTitle.transform.localScale = new Vector3(1.075f, 1.075f, 1.0f);
             modTitle.SetText(
                 string.Concat(
                     Helper.Translation.GetString("version"),
@@ -23,16 +25,15 @@ namespace ExtremeRoles.Patches
             modTitle.alignment = TMPro.TextAlignmentOptions.Center;
             modTitle.fontSize *= 1.5f;
 
-            var credentials = Object.Instantiate(modTitle);
-            credentials.transform.position = new Vector3(0, -0.25f, 0);
+            var credentials = Object.Instantiate(
+                modTitle, modTitle.transform);
+            credentials.transform.localPosition = new Vector3(0, -0.5f, 0);
+            credentials.transform.localScale = new Vector3(0.9f, 0.9f, 1.0f);
             credentials.SetText(
                 string.Concat(
                     Helper.Translation.GetString("developer"),"yukieiji"));
             credentials.alignment = TMPro.TextAlignmentOptions.Center;
             credentials.fontSize *= 0.85f;
-
-            modTitle.transform.SetParent(amongUsLogo.transform);
-            credentials.transform.SetParent(amongUsLogo.transform);
         }
     }
 }
