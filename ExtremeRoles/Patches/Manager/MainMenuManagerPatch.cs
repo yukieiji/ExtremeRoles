7b43516e4a68baf39891c44f130ec6a590b3a82d
yukieiji <yukieiji@users.noreply.github.com>
2022-07-14 14:32:40 +0900

    remove:dupe code

--- ExtremeRoles/Patches/Manager/MainMenuManagerPatch.cs
+++ ExtremeRoles/Patches/Manager/MainMenuManagerPatch.cs
@@ -27,8 +27,6 @@ namespace ExtremeRoles.Patches.Manager
     {
         public static void Prefix(MainMenuManager __instance)
         {
-            
-            DestroyableSingleton<ModManager>.Instance.ShowModStamp();
 
             var template = GameObject.Find("ExitGameButton");
             if (template == null) { return; }
