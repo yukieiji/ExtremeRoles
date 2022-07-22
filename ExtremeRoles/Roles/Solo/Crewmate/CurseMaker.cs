b5ad3de8aa3ef0a43ed99098e1e32a5b055b5070
yukieiji <yukieiji@users.noreply.github.com>
2022-07-23 00:32:22 +0900

    fix:cursemaker option

--- ExtremeRoles/Roles/Solo/Crewmate/CurseMaker.cs
+++ ExtremeRoles/Roles/Solo/Crewmate/CurseMaker.cs
@@ -304,14 +304,14 @@ namespace ExtremeRoles.Roles.Solo.Crewmate
                  taskBoostOpt,
                  format: OptionUnit.Percentage,
                  invert: true,
-                 enableCheckOption: parentOps);
+                 enableCheckOption: taskBoostOpt);
 
             var reduceTimeOpt = CreateFloatDynamicOption(
                 CurseMakerOption.ReduceSearchDeadBodyTime,
                 30f, 0.5f, 0.5f, taskBoostOpt,
                 format: OptionUnit.Second,
                 invert: true,
-                enableCheckOption: parentOps);
+                enableCheckOption: taskBoostOpt);
 
             searchTimeOpt.SetUpdateOption(reduceTimeOpt);
         }
