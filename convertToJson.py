4017553bdbeb101513ff95acd0417eed7927f752
yukieiji <yukieiji@yukieiji.com>
2022-08-09 22:22:40 +0900

    feat:create dir

--- convertToJson.py
+++ convertToJson.py
@@ -39,7 +39,9 @@ def stringToJson(filename, outputFile):
       
       if data:
         stringData[name] = data
-    
+
+  os.makedirs(os.path.dirname(outputFile))
+  
   with open(outputFile, "w") as f:
     json.dump(stringData, f, indent=4)
 
