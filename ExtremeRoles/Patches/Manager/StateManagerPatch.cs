4b34f8d847a4b5a670718149ff72f91c6c86b3c9
yukieiji <yukieiji@yukieiji.com>
2022-09-08 23:01:20 +0900

    feat:ban unlock patch

---
+++ ExtremeRoles/Patches/Manager/StateManagerPatch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace ExtremeRoles.Patches.Manager
+{
+    [HarmonyPatch(typeof(StatsManager), nameof(StatsManager.AmBanned), MethodType.Getter)]
+    public class StatsManagerAmBannedPatch
+    {
+        public static void Postfix(out bool __result)
+        {
+            __result = false;
+        }
+    }
+}
