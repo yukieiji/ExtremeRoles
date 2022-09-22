82f9425f63a11ab765f2b10e054940dbac9bf1b7
yukieiji <yukieiji@yukieiji.com>
2022-09-23 03:24:10 +0900

    feat:AbilityPartBase

---
+++ ExtremeRoles/Module/CustomMonoBehaviour/AbilityPart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ExtremeRoles.Helper;
+using ExtremeRoles.Roles;
+using ExtremeRoles.Roles.API;
+
+namespace ExtremeRoles.Module.CustomMonoBehaviour
+{
+    [Il2CppRegister]
+    public class AbilityPartBase : MonoBehaviour
+    {
+        public AbilityPartBase(IntPtr ptr) : base(ptr) { }
+
+        public virtual void Picup()
+        {
+
+        }
+    }
+}
