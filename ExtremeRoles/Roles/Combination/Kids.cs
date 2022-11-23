2c772502941d74f34412da29e5f234351d1d539c
yukieiji <yuki31gakki97@yahoo.co.jp>
2022-11-23 12:02:40 +0900

    feat:Dqlinquent comp

--- ExtremeRoles/Roles/Combination/Kids.cs
+++ ExtremeRoles/Roles/Combination/Kids.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
 using System.Collections.Generic;
+
+using Hazel;
+using UnityEngine;
 
 using ExtremeRoles.Helper;
 using ExtremeRoles.Module;
@@ -10,7 +13,6 @@ using ExtremeRoles.Roles.API;
 using ExtremeRoles.Roles.API.Interface;
 using ExtremeRoles.Resources;
 using ExtremeRoles.Performance;
-using Hazel;
 
 namespace ExtremeRoles.Roles.Combination
 {
@@ -54,18 +56,164 @@ namespace ExtremeRoles.Roles.Combination
 
     public sealed class Delinquent : MultiAssignRoleBase, IRoleAbility
     {
+        public sealed class DelinquentAbilityButton : RoleAbilityButtonBase
+        {
+            public int CurAbilityNum
+            {
+                get => this.abilityNum;
+            }
+
+            private int abilityNum = 0;
+            private TMPro.TextMeshPro abilityCountText = null;
+            private Sprite bombScribe;
+
+            public DelinquentAbilityButton(
+                string buttonText,
+                Func<bool> ability,
+                Func<bool> canUse,
+                Sprite scribeSprite,
+                Sprite bombSprite) : base(
+                    buttonText,
+                    ability,
+                    canUse,
+                    scribeSprite,
+                    new Vector3(-1.8f, -0.06f, 0),
+                    null,
+                    null,
+                    KeyCode.F, false)
+            {
+                this.bombScribe = bombSprite;
+                this.abilityCountText = GameObject.Instantiate(
+                    this.Button.cooldownTimerText,
+                    this.Button.cooldownTimerText.transform.parent);
+                updateAbilityInfoText();
+                this.abilityCountText.enableWordWrapping = false;
+                this.abilityCountText.transform.localScale = Vector3.one * 0.5f;
+                this.abilityCountText.transform.localPosition += new Vector3(-0.05f, 0.65f, 0);
+            }
+
+            public void UpdateAbilityCount(int newCount)
+            {
+                this.abilityNum = newCount + 1;
+                this.updateAbilityInfoText();
+            }
+
+            protected override void AbilityButtonUpdate()
+            {
+                if (this.CanUse() && this.abilityNum > 0)
+                {
+                    this.Button.graphic.color = this.Button.buttonLabelText.color = Palette.EnabledColor;
+                    this.Button.graphic.material.SetFloat("_Desat", 0f);
+                }
+                else
+                {
+                    this.Button.graphic.color = this.Button.buttonLabelText.color = Palette.DisabledClear;
+                    this.Button.graphic.material.SetFloat("_Desat", 1f);
+                }
+                if (this.abilityNum == 0)
+                {
+                    Button.SetCoolDown(0, this.CoolTime);
+                    return;
+                }
+                if (this.Timer >= 0)
+                {
+                    bool abilityOn = this.IsHasCleanUp() && IsAbilityOn;
+
+                    PlayerControl localPlayer = CachedPlayerControl.LocalPlayer;
+
+                    if (abilityOn ||
+                        localPlayer.IsKillTimerEnabled ||
+                        localPlayer.ForceKillTimerContinue)
+                    {
+                        this.Timer -= Time.deltaTime;
+                    }
+                    if (abilityOn)
+                    {
+                        if (!this.AbilityCheck())
+                        {
+                            this.Timer = 0;
+                            this.IsAbilityOn = false;
+                        }
+                    }
+                }
+
+                if (this.abilityNum > 0)
+                {
+                    Button.SetCoolDown(
+                        this.Timer,
+                        (this.IsHasCleanUp() && this.IsAbilityOn) ? this.AbilityActiveTime : this.CoolTime);
+                }
+            }
+
+            protected override void OnClickEvent()
+            {
+                if (this.CanUse() &&
+                    this.Timer < 0f &&
+                    this.abilityNum > 0 &&
+                    !this.IsAbilityOn)
+                {
+                    Button.graphic.color = this.DisableColor;
+
+                    if (this.UseAbility())
+                    {
+                        this.updateAbility();
+                        this.ResetCoolTimer();
+                    }
+                }
+            }
+
+            private void updateAbility()
+            {
+                --this.abilityNum;
+                bool updateBomb = this.abilityNum > 1;
+                if (updateBomb)
+                {
+                    this.ButtonSprite = this.bombScribe;
+                }
+                if (this.abilityCountText != null)
+                {
+                    if (!updateBomb)
+                    {
+                        updateAbilityInfoText();
+                    }
+                    else
+                    {
+                        this.abilityCountText.gameObject.SetActive(false);
+                    }
+                }
+            }
+
+            private void updateAbilityInfoText()
+            {
+                this.abilityCountText.text = Translation.GetString("scribeText") + 
+                    string.Format(
+                        Translation.GetString(OptionUnit.Shot.ToString()), this.abilityNum);
+            }
+
+        }
+
         public RoleAbilityButtonBase Button
         { 
-            get => throw new System.NotImplementedException(); 
-            set => throw new System.NotImplementedException();
+            get => this.abilityButton; 
+            set
+            {
+                this.abilityButton = value;
+            }
         }
 
         public bool WinCheckEnable => this.isWinCheck;
         public float Range => this.range;
 
+        public enum DelinqentOption
+        {
+            Range,
+        }
+
         private Kids.AbilityType curAbilityType;
         private float range;
         private bool isWinCheck;
+
+        private RoleAbilityButtonBase abilityButton;
 
         public Delinquent() : base(
             ExtremeRoleId.Delinquent,
@@ -104,10 +252,15 @@ namespace ExtremeRoles.Roles.Combination
 
         public void CreateAbility()
         {
-            this.CreateAbilityCountButton(
+            this.Button = new DelinquentAbilityButton(
                 Translation.GetString("scribble"),
+                this.UseAbility,
+                this.IsAbilityUse,
+                Loader.CreateSpriteFromResources(
+                    Path.TestButton),
                 Loader.CreateSpriteFromResources(
                     Path.TestButton));
+            this.RoleAbilityInit();
         }
 
         public bool IsAbilityUse() => this.curAbilityType switch
@@ -122,12 +275,12 @@ namespace ExtremeRoles.Roles.Combination
 
         public void RoleAbilityResetOnMeetingEnd()
         {
-            throw new System.NotImplementedException();
+            return;
         }
 
         public void RoleAbilityResetOnMeetingStart()
         {
-            throw new System.NotImplementedException();
+            return;
         }
 
         public bool UseAbility()
@@ -143,12 +296,17 @@ namespace ExtremeRoles.Roles.Combination
 
         protected override void CreateSpecificOption(IOption parentOps)
         {
-            
+            CreateFloatOption(
+                DelinqentOption.Range,
+                3.6f, 1.0f, 5.0f, 0.1f,
+                parentOps);
         }
 
         protected override void RoleSpecificInit()
         {
-            
+            this.range = OptionHolder.AllOption[
+                GetRoleOptionId(DelinqentOption.Range)].GetValue();
+            this.RoleAbilityInit();
         }
         
     }
