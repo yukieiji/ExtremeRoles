89c4bf4d74e90c095f4fa55fb534875827529f04
yukieiji <yuki31gakki97@yahoo.co.jp>
2022-12-12 06:45:49 +0900

    feat:HauntMenu team texts

---
+++ ExtremeRoles/Patches/MiniGame/HauntMenuMinigamePatch.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+
+using ExtremeRoles.Roles;
+
+
+namespace ExtremeRoles.Patches.MiniGame
+{
+    [HarmonyPatch(typeof(HauntMenuMinigame), nameof(HauntMenuMinigame.SetFilterText))]
+    public static class HauntMenuMinigameFilterTextPatch
+    {
+        public static bool Prefix(HauntMenuMinigame __instance)
+        {
+            if (ExtremeRoleManager.GameRole.Count == 0) { return true; }
+
+            var role = ExtremeRoleManager.GetLocalPlayerRole();
+            var isBlocked = role.IsBlockShowPlayingRoleInfo();
+
+            var targetRoleTeam = ExtremeRoleManager.GameRole[__instance.HauntTarget.PlayerId].Team;
+
+            __instance.FilterText.text =
+                isBlocked ||
+                (
+                    role.IsImpostor() &&
+                    ExtremeRolesPlugin.ShipState.IsAssassinAssign
+                ) ? 
+                    Helper.Translation.GetString("???") : 
+                    Helper.Translation.GetString(targetRoleTeam.ToString());
+
+            return false;
+        }
+    }
+}
