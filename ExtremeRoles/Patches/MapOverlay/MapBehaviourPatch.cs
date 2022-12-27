055bb4677d9075b7d19ba1e8c521a112bf4e2ca3
yukieiji <yuki31gakki97@yahoo.co.jp>
2022-12-27 20:59:40 +0900

    remove:unecessary patches

--- ExtremeRoles/Patches/MapOverlay/MapBehaviourPatch.cs
+++ ExtremeRoles/Patches/MapOverlay/MapBehaviourPatch.cs
@@ -6,26 +6,6 @@ using ExtremeRoles.Performance;
 
 namespace ExtremeRoles.Patches.MapOverlay
 {
-    [HarmonyPatch(typeof(MapBehaviour), "IsOpenStopped", MethodType.Getter)]
-    public static class MapBehaviourIsOpenStoppedPatch
-    {
-        public static bool Prefix(
-            MapBehaviour __instance,
-            ref bool __result)
-        {
-            if (Roles.ExtremeRoleManager.GameRole.Count == 0) { return true; }
-
-            var admin = Roles.ExtremeRoleManager.GetSafeCastedLocalPlayerRole<
-                Roles.Solo.Crewmate.Supervisor>();
-            
-            if (admin == null) { return true; }
-            if (!admin.Button.IsAbilityActive()) { return true; }
-
-            __result = false;
-            return false;
-        }
-    }
-
     [HarmonyPatch(typeof(MapBehaviour), nameof(MapBehaviour.GenericShow))]
     public static class MapBehaviourShowNormalMapPatch
     {
@@ -37,20 +17,13 @@ namespace ExtremeRoles.Patches.MapOverlay
     [HarmonyPatch(typeof(MapBehaviour), nameof(MapBehaviour.ShowCountOverlay))]
     public static class MapBehaviourShowCountOverlayPatch
     {
-        public static bool Prefix(MapBehaviour __instance)
+        public static bool Prefix()
         {
             if (Roles.ExtremeRoleManager.GameRole.Count == 0) { return true; }
-            if (Roles.ExtremeRoleManager.GetLocalPlayerRole().CanUseAdmin() ||
-                MapCountOverlayUpdatePatch.IsAbilityUse()) { return true; }
 
-            __instance.GenericShow();
-            __instance.ColorControl.SetColor(new Color(0.05f, 0.2f, 1f, 1f));
-            __instance.taskOverlay.Hide();
-            __instance.HerePoint.enabled = false;
-
-            FastDestroyableSingleton<HudManager>.Instance.SetHudActive(false);
-            
-            return false;
+            return 
+                Roles.ExtremeRoleManager.GetLocalPlayerRole().CanUseAdmin() ||
+                MapCountOverlayUpdatePatch.IsAbilityUse();
         }
     }
 }
