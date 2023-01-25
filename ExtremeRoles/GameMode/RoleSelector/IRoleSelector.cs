d581fc7a10e4cd86e1eed689f0052f3e04d78139
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-01-25 10:19:01 +0900

    change:prop

--- ExtremeRoles/GameMode/RoleSelector/IRoleSelector.cs
+++ ExtremeRoles/GameMode/RoleSelector/IRoleSelector.cs
@@ -12,12 +12,12 @@ namespace ExtremeRoles.GameMode.RoleSelector
     public interface IRoleSelector
     {
         public bool CanUseXion { get; }
+        public bool IsVanillaRoleToMultiAssign { get; }
 
         public IReadOnlyList<int> UseNormalRoleOptionId { get; }
         public IReadOnlyList<int> UseCombRoleOptionId { get; }
         public IReadOnlyList<int> UseGhostRoleOptionId { get; }
 
-        // public SingleRoleBase GetVanilaRole(RoleTypes roleId);
         public bool IsValidRoleOption(IOption option);
     }
 }
