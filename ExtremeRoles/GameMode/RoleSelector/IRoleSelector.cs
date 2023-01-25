b2b95db38f3b6afd309f7e6bc122fee642c8942f
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-01-25 10:09:59 +0900

    feat:IRoleSelector

---
+++ ExtremeRoles/GameMode/RoleSelector/IRoleSelector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using AmongUs.GameOptions;
+
+using ExtremeRoles.Module;
+using ExtremeRoles.Roles.API;
+
+namespace ExtremeRoles.GameMode.RoleSelector
+{
+    public interface IRoleSelector
+    {
+        public bool CanUseXion { get; }
+
+        public IReadOnlyList<int> UseNormalRoleOptionId { get; }
+        public IReadOnlyList<int> UseCombRoleOptionId { get; }
+        public IReadOnlyList<int> UseGhostRoleOptionId { get; }
+
+        // public SingleRoleBase GetVanilaRole(RoleTypes roleId);
+        public bool IsValidRoleOption(IOption option);
+    }
+}
