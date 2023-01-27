f86fbd58df54941113fce8d5d80e33c28faf9efd
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-01-27 20:49:33 +0900

    feat:multiAssignVanillaRoleAssign

--- ExtremeRoles/Roles/Solo/VanillaRoleWrapper.cs
+++ ExtremeRoles/Roles/Solo/VanillaRoleWrapper.cs
@@ -4,77 +4,75 @@ using AmongUs.GameOptions;
 using ExtremeRoles.Module;
 using ExtremeRoles.Roles.API;
 using ExtremeRoles.Performance;
+using ExtremeRoles.Helper;
 
 namespace ExtremeRoles.Roles.Solo
 {
-    public sealed class VanillaRoleWrapper : SingleRoleBase
+    public sealed class VanillaRoleWrapper : MultiAssignRoleBase
     {
         public RoleTypes VanilaRoleId;
 
-        public VanillaRoleWrapper(
-            RoleTypes id) : base()
+        private VanillaRoleWrapper(RoleTypes id, bool isImpostor) : base(
+            ExtremeRoleId.VanillaRole,
+            isImpostor ? ExtremeRoleType.Impostor : ExtremeRoleType.Crewmate,
+            id.ToString(),
+            isImpostor ? Palette.ImpostorRed : Palette.White,
+            isImpostor, !isImpostor,
+            isImpostor, isImpostor)
         {
             this.VanilaRoleId = id;
-            this.Id = ExtremeRoleId.VanillaRole;
-            this.RawRoleName = this.VanilaRoleId.ToString();
 
             var curOption = GameOptionsManager.Instance.CurrentGameOptions;
-
+            if (this.CanKill)
+            {
+                this.KillCoolTime = curOption.GetFloat(FloatOptionNames.KillCooldown);
+                this.KillRange = curOption.GetInt(Int32OptionNames.KillDistance);
+            }
             switch (id)
             {
-                case RoleTypes.Shapeshifter:
-                case RoleTypes.Impostor:
-                    this.Team = ExtremeRoleType.Impostor;
-                    this.NameColor = Palette.ImpostorRed;
-                    this.CanKill = true;
-                    this.UseVent = curOption.GameMode == GameModes.Normal;
-                    this.UseSabotage = curOption.GameMode == GameModes.Normal;
-                    this.HasTask = false;
-                    this.KillCoolTime = curOption.GetFloat(
-                        FloatOptionNames.KillCooldown);
-                    this.KillRange = curOption.GetInt(
-                        Int32OptionNames.KillDistance);
-                    break;
                 case RoleTypes.Engineer:
-                    this.Team = ExtremeRoleType.Crewmate;
                     this.UseVent = true;
-                    this.NameColor = Palette.White;
-                    break;
-                case RoleTypes.Crewmate:
-                case RoleTypes.Scientist:
-                    this.Team = ExtremeRoleType.Crewmate;
-                    this.NameColor = Palette.White;
                     break;
                 default:
                     break;
-            };
+            }
         }
+
+
+        public VanillaRoleWrapper(
+            RoleTypes id) : 
+            this(id, id == RoleTypes.Impostor || id == RoleTypes.Shapeshifter)
+        { }
 
         public override string GetFullDescription()
         {
-            return Helper.Translation.GetString(
+            return Translation.GetString(
                 $"{this.VanilaRoleId}FullDescription");
         }
 
         public override string GetImportantText(bool isContainFakeTask = true)
         {
-            if(this.IsImpostor())
+
+            if (this.AnotherRole == null)
             {
-                return string.Concat(new string[]
-                {
-                    FastDestroyableSingleton<TranslationController>.Instance.GetString(
-                        StringNames.ImpostorTask, Array.Empty<Il2CppSystem.Object>()),
-                    "\r\n",
-                    Palette.ImpostorRed.ToTextColor(),
-                    FastDestroyableSingleton<TranslationController>.Instance.GetString(
-                        StringNames.FakeTasks, Array.Empty<Il2CppSystem.Object>()),
-                    "</color>"
-                });
+                return getVanilaImportantText();
             }
 
-            return Helper.Design.ColoedString(
-                this.NameColor,
-                $"{this.GetColoredRoleName()}: {Helper.Translation.GetString("crewImportantText")}");
+            string baseString = getVanilaImportantText(false);
+            string anotherRoleString = this.AnotherRole.GetImportantText(false);
+
+            baseString = $"{baseString}\r\n{anotherRoleString}";
+
+            if (isContainFakeTask && (!this.HasTask || !this.AnotherRole.HasTask))
+            {
+                string fakeTaskString = Design.ColoedString(
+                    this.NameColor,
+                    FastDestroyableSingleton<TranslationController>.Instance.GetString(
+                        StringNames.FakeTasks, Array.Empty<Il2CppSystem.Object>()));
+                baseString = $"{baseString}\r\n{fakeTaskString}";
+            }
+
+            return baseString;
 
         }
         protected override void CommonInit()
@@ -89,6 +87,29 @@ namespace ExtremeRoles.Roles.Solo
         protected override void CreateSpecificOption(IOption parentOps)
         {
             throw new System.Exception("Don't call this class method!!");
+        }
+
+        private string getVanilaImportantText(bool isContainFakeTask = true)
+        {
+            if (this.IsImpostor())
+            {
+                var trans = FastDestroyableSingleton<TranslationController>.Instance;
+
+                return string.Concat(new string[]
+                {
+                    trans.GetString(StringNames.ImpostorTask, Array.Empty<Il2CppSystem.Object>()),
+                    "\r\n",
+                    Palette.ImpostorRed.ToTextColor(),
+                    isContainFakeTask ? 
+                        trans.GetString(StringNames.FakeTasks, Array.Empty<Il2CppSystem.Object>()) : 
+                        string.Empty,
+                    "</color>"
+                });
+            }
+
+            return Design.ColoedString(
+                this.NameColor,
+                $"{this.GetColoredRoleName()}: {Translation.GetString("crewImportantText")}");
         }
     }
 }
