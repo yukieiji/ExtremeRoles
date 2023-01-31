aedad0822727914a26827bfaaef46b386f1d1b8d
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-02-01 00:17:44 +0900

    refactor:pep8

--- convertToJson.py
+++ convertToJson.py
@@ -1,5 +1,6 @@
 import os
 import json
+from typing import Dict
 from openpyxl import load_workbook
 
 WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -10,24 +11,28 @@ EXTREMERORLS_OUT_FILE = os.path.join(WORKING_DIR, "ExtremeRoles", "Resources", "
 EXTREMESKIN_IN_FILE = os.path.join(WORKING_DIR, "ExtremeSkinsTransData.xlsx")
 EXTREMESKIN_OUT_FILE = os.path.join(WORKING_DIR, "ExtremeSkins", "Resources", "LangData", "stringData.json")
 
-def isRequireUpdate(newJson, outputFile):
+def is_require_update(new_json : Dict[str, str], output_file : str) -> bool:
+
   try:
-    with open(outputFile, "r") as f:
-      oldJson = json.load(f)
-    oldJsonStr = json.dumps(oldJson)
+    with open(output_file, "r") as f:
+      old_json = json.load(f)
+    old_json_str = json.dumps(old_json)
   except Exception:
     return True
   else:
-    newJsonStr = json.dumps(newJson)
-    return oldJsonStr != newJsonStr
+    new_json_str = json.dumps(new_json)
+
+    return old_json_str != new_json_str
 
 
-def stringToJson(filename, outputFile):
-  wb = load_workbook(filename, read_only = True)
+def xlsx_to_json(file_name : str, output_file : str) -> None:
 
-  stringData = {}
+  wb = load_workbook(file_name, read_only = True)
+
+  xlsx_data = {}
+
   for s in wb:
-    rows = s.iter_rows(min_col = 1, min_row = 2, max_col = 17, max_row = None)
+    rows = s.iter_rows(min_col=1, min_row=2, max_col=17, max_row=None)
     headers = []
     for header in s[1]:
       if header.value:
@@ -41,19 +46,22 @@ def stringToJson(filename, outputFile):
 
       data = {}
 
-      for i, string in enumerate(row[1:]):
-        if string.value:
+      for i, cell in enumerate(row[1:]):
+        if cell.value:
           # I hate excel why did I do this to myself
-          data[str(i)] = string.value.replace("\r", "").replace("_x000D_", "").replace("\\n", "\n")
+          data[str(i)] = cell.value.replace("\r", "").replace("_x000D_", "").replace("\\n", "\n")
 
       if data:
-        stringData[name] = data
+        xlsx_data[name] = data
 
-  if isRequireUpdate(stringData, outputFile):
-    os.makedirs(os.path.dirname(outputFile), exist_ok=True)
-    with open(outputFile, "w") as f:
-      json.dump(stringData, f, indent=4)
+  if is_require_update(xlsx_data, output_file):
+
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
+    with open(output_file, "w") as f:
+      json.dump(xlsx_data, f, indent=4)
+
 
 if __name__ == "__main__":
-  stringToJson(EXTREMERORLS_IN_FILE, EXTREMERORLS_OUT_FILE)
-  stringToJson(EXTREMESKIN_IN_FILE, EXTREMESKIN_OUT_FILE)
+  xlsx_to_json(EXTREMERORLS_IN_FILE, EXTREMERORLS_OUT_FILE)
+  xlsx_to_json(EXTREMESKIN_IN_FILE, EXTREMESKIN_OUT_FILE)
