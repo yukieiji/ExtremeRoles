ade487ae50d29951e10ca4e63060be6677d0d744
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-02-05 16:35:50 +0900

    refactor:List<string>

--- ExtremeRoles/Patches/Option/IGameOptionsExtensionsPatch.cs
+++ ExtremeRoles/Patches/Option/IGameOptionsExtensionsPatch.cs
@@ -43,32 +43,23 @@ namespace ExtremeRoles.Patches.Option
 
             if (ExtremeGameModeManager.Instance == null) { return; }
 
-            List<string> pages = new List<string>()
+            List<string> hudOptionPage = new List<string>()
             {
                 __result
             };
 
-            List<string> allOptionStr = new List<string>();
-
-            var allOption = OptionHolder.AllOption;
-
-            allOptionStr.Add(
-                allOption[(int)OptionHolder.CommonOptionKey.PresetSelection].ToHudString());
-
-            StringBuilder rngOptBuilder = new StringBuilder();
-            rngOptBuilder.AppendLine(
-                allOption[(int)OptionHolder.CommonOptionKey.UseStrongRandomGen].ToHudString());
-            rngOptBuilder.AppendLine(
-                allOption[(int)OptionHolder.CommonOptionKey.UsePrngAlgorithm].ToHudString());
-            allOptionStr.Add(rngOptBuilder.ToString().Trim('\r', '\n'));
-
-            allOptionStr.Add(createRoleSpawnNumOptions());
+            List<string> allOptionStr = new List<string>()
+            {
+                getOption(OptionHolder.CommonOptionKey.PresetSelection).ToHudString(),
+                createRNGSetting(),
+                createRoleSpawnNumOptions()
+            };
 
             // Xionが使えるかはIRoleSelectorで定義されてるのでそれから
             allOptionStr.Add(
                 Design.ColoedString(
                     ColorPalette.XionBlue,
-                    allOption[(int)OptionHolder.CommonOptionKey.UseXion].ToHudString()));
+                    getOption(OptionHolder.CommonOptionKey.UseXion).ToHudString()));
 
             allOptionStr.Add(ExtremeGameModeManager.Instance.ShipOption.ToHudString());
 
@@ -101,7 +92,7 @@ namespace ExtremeRoles.Patches.Option
 
                 if (lineCount + lines > maxLines)
                 {
-                    pages.Add(pageBuilder.ToString());
+                    hudOptionPage.Add(pageBuilder.ToString());
                     pageBuilder.Clear();
                     lineCount = 0;
                 }
@@ -110,17 +101,17 @@ namespace ExtremeRoles.Patches.Option
                 lineCount += lines + 1;
             }
 
-            string pageStr = pageBuilder.ToString().Trim('\r', '\n');
-            if (pageStr != string.Empty)
+            if (pageBuilder.Length != 0)
             {
-                pages.Add(pageStr);
+                hudOptionPage.Add(
+                    pageBuilder.ToString().Trim('\r', '\n'));
             }
 
-            int numPages = pages.Count;
+            int numPages = hudOptionPage.Count;
             int counter = OptionHolder.OptionsPage = OptionHolder.OptionsPage % numPages;
 
             __result = string.Concat(
-                pages[counter].Trim('\r', '\n'),
+                hudOptionPage[counter].Trim('\r', '\n'),
                 "\n\n",
                 translate("pressTabForMore"),
                 $" ({counter + 1}/{numPages})");
@@ -129,16 +120,14 @@ namespace ExtremeRoles.Patches.Option
 
         private static string createRoleSpawnNumOptions()
         {
-            var allOption = OptionHolder.AllOption;
-
             StringBuilder entry = new StringBuilder();
 
             // 生存役職周り
             string optionName = Design.ColoedString(
                 new Color(204f / 255f, 204f / 255f, 0, 1f),
                 translate("crewmateRoles"));
-            int min = allOption[(int)OptionHolder.CommonOptionKey.MinCrewmateRoles].GetValue();
-            int max = allOption[(int)OptionHolder.CommonOptionKey.MaxCrewmateRoles].GetValue();
+            int min = getOption(OptionHolder.CommonOptionKey.MinCrewmateRoles).GetValue();
+            int max = getOption(OptionHolder.CommonOptionKey.MaxCrewmateRoles).GetValue();
             if (min > max) { min = max; }
             string optionValue = (min == max) ? $"{max}" : $"{min} - {max}";
             entry.AppendLine($"{optionName}: {optionValue}");
@@ -146,8 +135,8 @@ namespace ExtremeRoles.Patches.Option
             optionName = Design.ColoedString(
                 new Color(204f / 255f, 204f / 255f, 0, 1f),
                 translate("neutralRoles"));
-            min = allOption[(int)OptionHolder.CommonOptionKey.MinNeutralRoles].GetValue();
-            max = allOption[(int)OptionHolder.CommonOptionKey.MaxNeutralRoles].GetValue();
+            min = getOption(OptionHolder.CommonOptionKey.MinNeutralRoles).GetValue();
+            max = getOption(OptionHolder.CommonOptionKey.MaxNeutralRoles).GetValue();
             if (min > max) { min = max; }
             optionValue = (min == max) ? $"{max}" : $"{min} - {max}";
             entry.AppendLine($"{optionName}: {optionValue}");
@@ -155,21 +144,21 @@ namespace ExtremeRoles.Patches.Option
             optionName = Design.ColoedString(
                 new Color(204f / 255f, 204f / 255f, 0, 1f),
                 translate("impostorRoles"));
-            min = allOption[(int)OptionHolder.CommonOptionKey.MinImpostorRoles].GetValue();
-            max = allOption[(int)OptionHolder.CommonOptionKey.MaxImpostorRoles].GetValue();
+            min = getOption(OptionHolder.CommonOptionKey.MinImpostorRoles).GetValue();
+            max = getOption(OptionHolder.CommonOptionKey.MaxImpostorRoles).GetValue();
 
             if (min > max) { min = max; }
             optionValue = (min == max) ? $"{max}" : $"{min} - {max}";
             entry.AppendLine($"{optionName}: {optionValue}");
 
-            entry.AppendLine("");
+            entry.AppendLine();
 
             // 幽霊役職周り
             optionName = Design.ColoedString(
                 new Color(204f / 255f, 204f / 255f, 0, 1f),
                 translate("crewmateGhostRoles"));
-            min = allOption[(int)OptionHolder.CommonOptionKey.MinCrewmateGhostRoles].GetValue();
-            max = allOption[(int)OptionHolder.CommonOptionKey.MaxCrewmateGhostRoles].GetValue();
+            min = getOption(OptionHolder.CommonOptionKey.MinCrewmateGhostRoles).GetValue();
+            max = getOption(OptionHolder.CommonOptionKey.MaxCrewmateGhostRoles).GetValue();
             if (min > max) { min = max; }
             optionValue = (min == max) ? $"{max}" : $"{min} - {max}";
             entry.AppendLine($"{optionName}: {optionValue}");
@@ -177,8 +166,8 @@ namespace ExtremeRoles.Patches.Option
             optionName = Design.ColoedString(
                 new Color(204f / 255f, 204f / 255f, 0, 1f),
                 translate("neutralGhostRoles"));
-            min = allOption[(int)OptionHolder.CommonOptionKey.MinNeutralGhostRoles].GetValue();
-            max = allOption[(int)OptionHolder.CommonOptionKey.MaxNeutralGhostRoles].GetValue();
+            min = getOption(OptionHolder.CommonOptionKey.MinNeutralGhostRoles).GetValue();
+            max = getOption(OptionHolder.CommonOptionKey.MaxNeutralGhostRoles).GetValue();
             if (min > max) { min = max; }
             optionValue = (min == max) ? $"{max}" : $"{min} - {max}";
             entry.AppendLine($"{optionName}: {optionValue}");
@@ -186,8 +175,8 @@ namespace ExtremeRoles.Patches.Option
             optionName = Design.ColoedString(
                 new Color(204f / 255f, 204f / 255f, 0, 1f),
                 translate("impostorGhostRoles"));
-            min = allOption[(int)OptionHolder.CommonOptionKey.MinImpostorGhostRoles].GetValue();
-            max = allOption[(int)OptionHolder.CommonOptionKey.MaxImpostorGhostRoles].GetValue();
+            min = getOption(OptionHolder.CommonOptionKey.MinImpostorGhostRoles).GetValue();
+            max = getOption(OptionHolder.CommonOptionKey.MaxImpostorGhostRoles).GetValue();
 
             if (min > max) { min = max; }
             optionValue = (min == max) ? $"{max}" : $"{min} - {max}";
@@ -196,9 +185,23 @@ namespace ExtremeRoles.Patches.Option
             return entry.ToString().Trim('\r', '\n');
         }
 
+        private static string createRNGSetting()
+        {
+            StringBuilder rngOptBuilder = new StringBuilder();
+            rngOptBuilder.AppendLine(
+                getOption(OptionHolder.CommonOptionKey.UseStrongRandomGen).ToHudString());
+            rngOptBuilder.AppendLine(
+                getOption(OptionHolder.CommonOptionKey.UsePrngAlgorithm).ToHudString());
+
+            return rngOptBuilder.ToString().Trim('\r', '\n');
+        }
+
         private static string translate(string key)
         {
             return Translation.GetString(key);
         }
+
+        private static IOption getOption(OptionHolder.CommonOptionKey optionKey)
+            => OptionHolder.AllOption[(int)optionKey];
     }
 }
