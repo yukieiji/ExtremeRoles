53df1f6ddb8a3d4229662d378ab8de6561ab7c62
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-02-20 20:07:50 +0900

    feat:RoleButtonActivator

---
+++ ExtremeRoles/Module/ButtonAutoActivator/RoleButtonActivator.cs
@@ -0,0 +1,24 @@
+﻿using ExtremeRoles.Module.Interface;
+using ExtremeRoles.Performance;
+
+namespace ExtremeRoles.Module.ButtonAutoActivator
+{
+    public sealed class RoleButtonActivator : IButtonAutoActivator
+    {
+        public bool IsActive()
+        {
+            PlayerControl localPlayer = CachedPlayerControl.LocalPlayer;
+
+            return
+                (
+                    localPlayer.IsKillTimerEnabled ||
+                    localPlayer.ForceKillTimerContinue ||
+                    FastDestroyableSingleton<HudManager>.Instance.UseButton.isActiveAndEnabled
+                ) &&
+                localPlayer.Data != null &&
+                MeetingHud.Instance == null &&
+                ExileController.Instance == null &&
+                !localPlayer.Data.IsDead;
+        }
+    }
+}
