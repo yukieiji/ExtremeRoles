caeb78dcf3e0112415d20031c81127c2b5c0d805
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-03-05 11:57:50 +0900

    refactor

--- ExtremeRoles/Compat/BepInExUpdater.cs
+++ ExtremeRoles/Compat/BepInExUpdater.cs
@@ -5,11 +5,14 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
+
 using BepInEx;
-using BepInEx.Unity.IL2CPP;
 using BepInEx.Unity.IL2CPP.Utils;
+
 using Il2CppInterop.Runtime.Attributes;
+
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -51,14 +54,6 @@ namespace ExtremeRoles.Compat
                 IntPtr.Zero,
                 showStr, "Extreme Roles", 0));
 
-            UnityWebRequest www = UnityWebRequest.Get(bepInExDownloadURL);
-            yield return www.SendWebRequest();
-            if (www.isNetworkError || www.isHttpError)
-            {
-                ExtremeRolesPlugin.Logger.LogInfo(www.error);
-                yield break;
-            }
-
             string tmpFolder = Path.Combine(Paths.GameRootPath, "tmp");
             string zipPath = Path.Combine(tmpFolder, "BepInEx.zip");
             string extractPath = Path.Combine(tmpFolder, "BepInEx");
@@ -68,30 +63,65 @@ namespace ExtremeRoles.Compat
                 Directory.Delete(tmpFolder, true);
             }
             Directory.CreateDirectory(tmpFolder);
-            
-            File.WriteAllBytes(zipPath, www.downloadHandler.data);
-            
+
+            yield return dlBepInExZip(zipPath);
+
             ZipFile.ExtractToDirectory(zipPath, extractPath);
 
-            Assembly asm = Assembly.GetExecutingAssembly();
-            string exePath = asm.GetManifestResourceNames().FirstOrDefault(n => n.EndsWith(exeFileName));
-
-            using (var resource = asm.GetManifestResourceStream(exePath))
-            {
-                using (var file = new FileStream(
-                    Path.Combine(tmpFolder, exeFileName),
-                    FileMode.OpenOrCreate, FileAccess.Write))
-                {
-                    resource!.CopyTo(file);
-                }
-            }
-
+            extractExtremeBepInExInstaller(tmpFolder);
 
             Process.Start(
                 Path.Combine(Paths.GameRootPath, "tmp", exeFileName),
                 $"{Paths.GameRootPath} {extractPath} {(uint)DataManager.Settings.Language.CurrentLanguage}");
 
             Application.Quit();
+        }
+
+        private static IEnumerator dlBepInExZip(string saveZipPath)
+        {
+
+            UnityWebRequest www = UnityWebRequest.Get(bepInExDownloadURL);
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                ExtremeRolesPlugin.Logger.LogInfo(www.error);
+                yield break;
+            }
+
+            File.WriteAllBytes(saveZipPath, www.downloadHandler.data);
+        }
+
+        private static void extractExtremeBepInExInstaller(string extractTmpFolder)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string exePath = asm.GetManifestResourceNames().FirstOrDefault(n => n.EndsWith(exeFileName));
+
+            using (var resource = asm.GetManifestResourceStream(exePath))
+            {
+                using (var file = new FileStream(
+                    Path.Combine(extractTmpFolder, exeFileName),
+                    FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    resource!.CopyTo(file);
+                }
+            }
+        }
+
+        // ここでBepInExのcfgをdlしてコピーする
+        private static void replaceConfigValue(string configPath)
+        {
+            string configStr;
+            using (StreamReader prevLog = new StreamReader(configPath))
+            {
+                configStr = prevLog.ReadToEnd();
+            }
+
+            configStr = configStr.Replace(
+                "[Logging.Console]\r\n\r\n## Enables showing a console for log output.\r\n# Setting type: Boolean\r\n# Default value: true\r\nEnabled = true",
+                "[Logging.Console]\r\n\r\n## Enables showing a console for log output.\r\n# Setting type: Boolean\r\n# Default value: true\r\nEnabled = false");
+
+            using StreamWriter newLog = new StreamWriter(configPath, true, Encoding.UTF8);
+            newLog.Write(configStr);
         }
     }
 }
