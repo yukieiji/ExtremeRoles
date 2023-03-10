48f449cdcfdca22efa7b6ebf9d34ef5c4335fa4b
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-03-10 22:22:31 +0900

    feat:play logic

--- ExtremeRoles/Module/CustomMonoBehaviour/DlayableVideoPlayer.cs
+++ ExtremeRoles/Module/CustomMonoBehaviour/DlayableVideoPlayer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Video;
 
 namespace ExtremeRoles.Module.CustomMonoBehaviour;
 
@@ -8,17 +9,25 @@ public sealed class DlayableVideoPlayer : MonoBehaviour
 {
     private float delayTimer = 0.0f;
     private bool isPlayed = false;
-    
+
+    private SpriteRenderer thumbnail;
+    private VideoPlayer player;
+
     public DlayableVideoPlayer(IntPtr ptr) : base(ptr) { }
 
     public void Awake()
     {
+        this.thumbnail = this.gameObject.AddComponent<SpriteRenderer>();
+        this.player = this.gameObject.AddComponent<VideoPlayer>();
 
+        this.player.source = VideoSource.Url;
+        this.player.renderMode = VideoRenderMode.MaterialOverride;
+        this.player.isLooping = true;
     }
 
     public void SetVideoPath(string path)
     {
-
+        this.player.url = path;
     }
 
     public void SetTimer(float time)
@@ -37,9 +46,10 @@ public sealed class DlayableVideoPlayer : MonoBehaviour
 
         checkAndPlay();
     }
+
     private void checkAndPlay()
     {
         if (this.delayTimer > 0.0f) { return; }
-        
+        this.player.Play();
     }
 }
