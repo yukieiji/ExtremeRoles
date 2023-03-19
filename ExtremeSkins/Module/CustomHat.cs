c80b4a30584de4c1092910d58741706bbc441625
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-03-19 18:06:24 +0900

    change:can dupe skins

--- ExtremeSkins/Module/CustomHat.cs
+++ ExtremeSkins/Module/CustomHat.cs
@@ -1,153 +1,155 @@
-﻿using ExtremeSkins.Module.Interface;
+﻿using System.IO;
+
 using UnityEngine;
+
+using ExtremeSkins.Module.Interface;
 
 using ExtremeRoles.Performance;
 
-namespace ExtremeSkins.Module
-{
+namespace ExtremeSkins.Module;
+
 
 #if WITHHAT
-    public sealed class CustomHat : ICustomCosmicData<HatData>
-    {
-        public const string FrontImageName = "front.png";
-        public const string FrontFlipImageName = "front_flip.png";
-        public const string BackImageName = "back.png";
-        public const string BackFlipImageName = "back_flip.png";
-        public const string ClimbImageName = "climb.png";
+public sealed class CustomHat : ICustomCosmicData<HatData>
+{
+    public const string FrontImageName = "front.png";
+    public const string FrontFlipImageName = "front_flip.png";
+    public const string BackImageName = "back.png";
+    public const string BackFlipImageName = "back_flip.png";
+    public const string ClimbImageName = "climb.png";
 
-        public HatData Data
-        { 
-            get => this.hat; 
-        }
-
-        public string Author
-        { 
-            get => this.author;
-        }
-        public string Name
-        { 
-            get => this.name; 
-        }
-
-        public string Id
-        { 
-            get => $"hat_{name}"; 
-        }
-
-        private bool hasFrontFlip;
-        private bool hasBackFlip;
-
-        private bool hasShader;
-        private bool hasBack;
-        private bool hasClimb;
-        private bool isBounce;
-
-        private string folderPath;
-
-        private string name;
-        private string author;
-
-        private HatData hat;
-
-        public CustomHat(
-            string folderPath,
-            string author,
-            string name,
-            bool hasFrontFlip,
-            bool hasBack,
-            bool hasBackFlip,
-            bool hasClimb,
-            bool isBounce,
-            bool hasShader)
-        {
-            this.folderPath = folderPath;
-            this.author = author;
-            this.name = name;
-            
-            this.hasFrontFlip = hasFrontFlip;
-            this.hasBack = hasBack;
-            this.hasBackFlip = hasBackFlip;
-            this.hasClimb = hasClimb;
-            this.hasShader = hasShader;
-
-            this.isBounce = isBounce;
-        }
-
-        public HatData GetData()
-        {
-            if (this.hat != null) { return this.hat; }
-
-            this.hat = ScriptableObject.CreateInstance<HatData>();
-
-            this.hat.name = Helper.Translation.GetString(this.Name);
-            this.hat.displayOrder = 99;
-            this.hat.ProductId = this.Id;
-            this.hat.InFront = !this.hasBack;
-            this.hat.NoBounce = !this.isBounce;
-            this.hat.ChipOffset = new Vector2(0f, 0.2f);
-            this.hat.Free = true;
-            this.hat.NotInStore = true;
-
-            this.hat.hatViewData.viewData = ScriptableObject.CreateInstance<HatViewData>();
-
-            this.hat.hatViewData.viewData.MainImage = loadHatSprite(
-                string.Concat(this.folderPath, @"\", FrontImageName));
-            
-            if (this.hasFrontFlip)
-            {
-                this.hat.hatViewData.viewData.LeftMainImage = loadHatSprite(
-                    string.Concat(this.folderPath, @"\", FrontFlipImageName));
-            }
-
-            if (this.hasBack)
-            {
-                this.hat.hatViewData.viewData.BackImage = loadHatSprite(
-                    string.Concat(this.folderPath, @"\", BackImageName));
-            }
-            if (this.hasBackFlip)
-            {
-                this.hat.hatViewData.viewData.LeftBackImage = loadHatSprite(
-                    string.Concat(this.folderPath, @"\", BackFlipImageName));
-            }
-
-            if (this.hasClimb)
-            {
-                this.hat.hatViewData.viewData.ClimbImage = loadHatSprite(
-                    string.Concat(this.folderPath, @"\", ClimbImageName));
-            }
-
-            if (this.hasShader)
-            {
-                Material altShader = new Material(
-                    FastDestroyableSingleton<HatManager>.Instance.PlayerMaterial);
-                altShader.shader = Shader.Find("Unlit/PlayerShader");
-
-                this.hat.hatViewData.viewData.AltShader = altShader;
-            }
-
-            return this.hat;
-
-        }
-
-        private Sprite loadHatSprite(
-            string path)
-        {
-            Texture2D texture = Loader.LoadTextureFromDisk(path);
-            if (texture == null)
-            {
-                return null;
-            }
-            Sprite sprite = Sprite.Create(
-                texture, new Rect(0, 0, texture.width, texture.height),
-                new Vector2(0.53f, 0.575f), texture.width * 0.375f);
-            if (sprite == null)
-            {
-                return null;
-            }
-            texture.hideFlags |= HideFlags.HideAndDontSave | HideFlags.DontUnloadUnusedAsset;
-            sprite.hideFlags |= HideFlags.HideAndDontSave | HideFlags.DontUnloadUnusedAsset;
-            return sprite;
-        }
+    public HatData Data
+    { 
+        get => this.hat; 
     }
-#endif
+
+    public string Author
+    { 
+        get => this.author;
+    }
+    public string Name
+    { 
+        get => this.name; 
+    }
+
+    public string Id
+    { 
+        get => $"hat_{Path.GetDirectoryName(this.folderPath)}_{this.author}_{this.name}"; 
+    }
+
+    private bool hasFrontFlip;
+    private bool hasBackFlip;
+
+    private bool hasShader;
+    private bool hasBack;
+    private bool hasClimb;
+    private bool isBounce;
+
+    private string folderPath;
+
+    private string name;
+    private string author;
+
+    private HatData hat;
+
+    public CustomHat(
+        string folderPath,
+        string author,
+        string name,
+        bool hasFrontFlip,
+        bool hasBack,
+        bool hasBackFlip,
+        bool hasClimb,
+        bool isBounce,
+        bool hasShader)
+    {
+        this.folderPath = folderPath;
+        this.author = author;
+        this.name = name;
+        
+        this.hasFrontFlip = hasFrontFlip;
+        this.hasBack = hasBack;
+        this.hasBackFlip = hasBackFlip;
+        this.hasClimb = hasClimb;
+        this.hasShader = hasShader;
+
+        this.isBounce = isBounce;
+    }
+
+    public HatData GetData()
+    {
+        if (this.hat != null) { return this.hat; }
+
+        this.hat = ScriptableObject.CreateInstance<HatData>();
+
+        this.hat.name = Helper.Translation.GetString(this.Name);
+        this.hat.displayOrder = 99;
+        this.hat.ProductId = this.Id;
+        this.hat.InFront = !this.hasBack;
+        this.hat.NoBounce = !this.isBounce;
+        this.hat.ChipOffset = new Vector2(0f, 0.2f);
+        this.hat.Free = true;
+        this.hat.NotInStore = true;
+
+        this.hat.hatViewData.viewData = ScriptableObject.CreateInstance<HatViewData>();
+
+        this.hat.hatViewData.viewData.MainImage = loadHatSprite(
+            string.Concat(this.folderPath, @"\", FrontImageName));
+        
+        if (this.hasFrontFlip)
+        {
+            this.hat.hatViewData.viewData.LeftMainImage = loadHatSprite(
+                string.Concat(this.folderPath, @"\", FrontFlipImageName));
+        }
+
+        if (this.hasBack)
+        {
+            this.hat.hatViewData.viewData.BackImage = loadHatSprite(
+                string.Concat(this.folderPath, @"\", BackImageName));
+        }
+        if (this.hasBackFlip)
+        {
+            this.hat.hatViewData.viewData.LeftBackImage = loadHatSprite(
+                string.Concat(this.folderPath, @"\", BackFlipImageName));
+        }
+
+        if (this.hasClimb)
+        {
+            this.hat.hatViewData.viewData.ClimbImage = loadHatSprite(
+                string.Concat(this.folderPath, @"\", ClimbImageName));
+        }
+
+        if (this.hasShader)
+        {
+            Material altShader = new Material(
+                FastDestroyableSingleton<HatManager>.Instance.PlayerMaterial);
+            altShader.shader = Shader.Find("Unlit/PlayerShader");
+
+            this.hat.hatViewData.viewData.AltShader = altShader;
+        }
+
+        return this.hat;
+
+    }
+
+    private Sprite loadHatSprite(
+        string path)
+    {
+        Texture2D texture = Loader.LoadTextureFromDisk(path);
+        if (texture == null)
+        {
+            return null;
+        }
+        Sprite sprite = Sprite.Create(
+            texture, new Rect(0, 0, texture.width, texture.height),
+            new Vector2(0.53f, 0.575f), texture.width * 0.375f);
+        if (sprite == null)
+        {
+            return null;
+        }
+        texture.hideFlags |= HideFlags.HideAndDontSave | HideFlags.DontUnloadUnusedAsset;
+        sprite.hideFlags |= HideFlags.HideAndDontSave | HideFlags.DontUnloadUnusedAsset;
+        return sprite;
+    }
 }
+#endif
--- ExtremeSkins/Module/CustomNamePlate.cs
+++ ExtremeSkins/Module/CustomNamePlate.cs
@@ -1,89 +1,88 @@
-﻿using ExtremeSkins.Module.Interface;
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace ExtremeSkins.Module
-{
+using ExtremeSkins.Module.Interface;
+
+namespace ExtremeSkins.Module;
+
 
 #if WITHNAMEPLATE
-    public sealed class CustomNamePlate : ICustomCosmicData<NamePlateData>
+public sealed class CustomNamePlate : ICustomCosmicData<NamePlateData>
+{
+
+    public NamePlateData Data
     {
-
-        public NamePlateData Data
-        {
-            get => this.namePlate;
-        }
-
-        public string Author
-        {
-            get => this.author;
-        }
-        public string Name
-        {
-            get => this.name;
-        }
-
-        public string Id
-        {
-            get => $"namePlate_{this.name}";
-        }
-
-        private NamePlateData namePlate;
-
-        private string name;
-        private string author;
-        private string imgPath;
-
-        public CustomNamePlate(
-            string imgPath,
-            string author,
-            string name)
-        {
-            this.imgPath = imgPath;
-            this.author = author;
-            this.name = name;
-        }
-
-        public NamePlateData GetData()
-        {
-            if (this.namePlate != null) { return this.namePlate; }
-
-            this.namePlate = ScriptableObject.CreateInstance<NamePlateData>();
-            this.namePlate.name = Helper.Translation.GetString(this.Name);
-            this.namePlate.displayOrder = 99;
-            this.namePlate.ProductId = this.Id;
-            this.namePlate.ChipOffset = new Vector2(0f, 0.2f);
-            this.namePlate.Free = true;
-            this.namePlate.NotInStore = true;
-
-            this.namePlate.viewData.viewData = ScriptableObject.CreateInstance<NamePlateViewData>();
-            this.namePlate.viewData.viewData.Image = loadNamePlateSprite(this.imgPath);
-
-            return this.namePlate;
-
-        }
-
-        private Sprite loadNamePlateSprite(
-            string path)
-        {
-            Texture2D texture = Loader.LoadTextureFromDisk(path);
-            if (texture == null)
-            {
-                return null;
-            }
-            Sprite sprite = Sprite.Create(
-                texture,
-                new Rect(0, 0, texture.width, texture.height),
-                new Vector2(0.5f, 0.5f), 100f);
-            if (sprite == null)
-            {
-                return null;
-            }
-            texture.hideFlags |= HideFlags.HideAndDontSave | HideFlags.DontUnloadUnusedAsset;
-            sprite.hideFlags |= HideFlags.HideAndDontSave | HideFlags.DontUnloadUnusedAsset;
-            return sprite;
-        }
+        get => this.namePlate;
     }
 
-#endif
+    public string Author
+    {
+        get => this.author;
+    }
+    public string Name
+    {
+        get => this.name;
+    }
 
+    public string Id
+    {
+        get => $"namePlate_{this.author}_{this.name}";
+    }
+
+    private NamePlateData namePlate;
+
+    private string name;
+    private string author;
+    private string imgPath;
+
+    public CustomNamePlate(
+        string imgPath,
+        string author,
+        string name)
+    {
+        this.imgPath = imgPath;
+        this.author = author;
+        this.name = name;
+    }
+
+    public NamePlateData GetData()
+    {
+        if (this.namePlate != null) { return this.namePlate; }
+
+        this.namePlate = ScriptableObject.CreateInstance<NamePlateData>();
+        this.namePlate.name = Helper.Translation.GetString(this.Name);
+        this.namePlate.displayOrder = 99;
+        this.namePlate.ProductId = this.Id;
+        this.namePlate.ChipOffset = new Vector2(0f, 0.2f);
+        this.namePlate.Free = true;
+        this.namePlate.NotInStore = true;
+
+        this.namePlate.viewData.viewData = ScriptableObject.CreateInstance<NamePlateViewData>();
+        this.namePlate.viewData.viewData.Image = loadNamePlateSprite(this.imgPath);
+
+        return this.namePlate;
+
+    }
+
+    private Sprite loadNamePlateSprite(
+        string path)
+    {
+        Texture2D texture = Loader.LoadTextureFromDisk(path);
+        if (texture == null)
+        {
+            return null;
+        }
+        Sprite sprite = Sprite.Create(
+            texture,
+            new Rect(0, 0, texture.width, texture.height),
+            new Vector2(0.5f, 0.5f), 100f);
+        if (sprite == null)
+        {
+            return null;
+        }
+        texture.hideFlags |= HideFlags.HideAndDontSave | HideFlags.DontUnloadUnusedAsset;
+        sprite.hideFlags |= HideFlags.HideAndDontSave | HideFlags.DontUnloadUnusedAsset;
+        return sprite;
+    }
 }
+
+#endif
