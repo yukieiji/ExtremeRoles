fc72268334fc7e5b83f89cd6c50ef4a0f9f205b9
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-03-27 21:50:51 +0900

    feat:usable over ride logic

---
+++ ExtremeRoles/Patches/Button/PassiveButtonPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+
+using ExtremeRoles.Module.RoleAssign;
+using ExtremeRoles.Roles;
+using ExtremeRoles.Roles.API.Interface;
+
+namespace ExtremeRoles.Patches.Button;
+
+[HarmonyPatch(typeof(PassiveButton), nameof(PassiveButton.ReceiveClickDown))]
+public static class PassiveButtonReceiveClickDownPatch
+{
+    public static bool Prefix()
+    {
+        if (ExtremeRoleManager.GameRole.Count == 0 ||
+            !RoleAssignState.Instance.IsRoleSetUpEnd) { return true; }
+
+        var (useRole, anotherUseRole) =
+            ExtremeRoleManager.GetInterfaceCastedLocalRole<IRoleUsableOverride>();
+
+        return
+            (useRole is null && anotherUseRole is null) ||
+            (useRole.EnableVentButton && anotherUseRole is null) ||
+            (useRole is null && anotherUseRole.EnableUseButton) ||
+            (useRole.EnableUseButton && anotherUseRole.EnableUseButton);
+    }
+}
