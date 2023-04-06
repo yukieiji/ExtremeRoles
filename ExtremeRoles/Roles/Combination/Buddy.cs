4f53b638113495e8d96d7c45c722d568da0e3b34
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-04-06 21:18:16 +0900

    fix:null error

--- ExtremeRoles/Roles/Combination/Buddy.cs
+++ ExtremeRoles/Roles/Combination/Buddy.cs
@@ -9,292 +9,295 @@ using ExtremeRoles.Roles.API;
 using ExtremeRoles.Roles.API.Interface;
 using ExtremeRoles.Performance;
 
-namespace ExtremeRoles.Roles.Combination
-{
-    public sealed class BuddyManager : FlexibleCombinationRoleManagerBase
-    {
-        public BuddyManager() : base(
-            new Buddy(),
-            canAssignImposter: false)
-        { }
+namespace ExtremeRoles.Roles.Combination;
 
+public sealed class BuddyManager : FlexibleCombinationRoleManagerBase
+{
+    public BuddyManager() : base(
+        new Buddy(),
+        canAssignImposter: false)
+    { }
+
+}
+
+public sealed class Buddy : MultiAssignRoleBase, IRoleAwake<RoleTypes>, IRoleSpecialSetUp
+{
+    public bool IsAwake => this.awake;
+
+    public RoleTypes NoneAwakeRole => RoleTypes.Crewmate;
+
+    public enum BuddyOption
+    {
+        AwakeTaskGage,
     }
 
-    public sealed class Buddy : MultiAssignRoleBase, IRoleAwake<RoleTypes>, IRoleSpecialSetUp
+    public sealed class BuddyContainer
     {
-        public bool IsAwake => this.awake;
+        private HashSet<GameData.PlayerInfo> buddy = new HashSet<GameData.PlayerInfo>();
+        private HashSet<byte> bytedBuddy = new HashSet<byte>();
 
-        public RoleTypes NoneAwakeRole => RoleTypes.Crewmate;
+        public HashSet<GameData.PlayerInfo> PlayerInfo => this.buddy;
 
-        public enum BuddyOption
+        public BuddyContainer()
         {
-            AwakeTaskGage,
+            this.buddy.Clear();
+            this.bytedBuddy.Clear();
         }
 
-        public sealed class BuddyContainer
+        public string GetAllPlayerName()
         {
-            private HashSet<GameData.PlayerInfo> buddy = new HashSet<GameData.PlayerInfo>();
-            private HashSet<byte> bytedBuddy = new HashSet<byte>();
+            List<string> playerName = new List<string>();
 
-            public HashSet<GameData.PlayerInfo> PlayerInfo => this.buddy;
-
-            public BuddyContainer()
+            foreach (GameData.PlayerInfo player in this.PlayerInfo)
             {
-                this.buddy.Clear();
-                this.bytedBuddy.Clear();
-            }
-
-            public string GetAllPlayerName()
-            {
-                List<string> playerName = new List<string>();
-
-                foreach (GameData.PlayerInfo player in this.PlayerInfo)
+                if (player.PlayerId == CachedPlayerControl.LocalPlayer.PlayerId)
                 {
-                    if (player.PlayerId == CachedPlayerControl.LocalPlayer.PlayerId)
-                    {
-                        continue;
-                    }
-
-                    int count = playerName.Count;
-                    if (count > 1)
-                    {
-                        playerName.Add(Translation.GetString("andFirst"));
-                    }
-                    else if (count == 1)
-                    {
-                        playerName.Add(Translation.GetString("and"));
-                    }
-                    playerName.Add(player.PlayerName);
+                    continue;
                 }
-                return string.Concat(playerName);
-            }
 
-            public bool Contains(GameData.PlayerInfo player) => this.buddy.Contains(player);
-
-            public bool Contains(byte playerId) => this.bytedBuddy.Contains(playerId);
-
-            public void Add(GameData.PlayerInfo player)
-            {
-                this.buddy.Add(player);
-                this.bytedBuddy.Add(player.PlayerId);
-            }
-        }
-
-        private float awakeTaskGage;
-        private bool awake;
-        private bool awakeHasOtherVision;
-        private BuddyContainer buddy;
-        private SingleRoleBase hiddeRole = null;
-
-        public Buddy() : base(
-            ExtremeRoleId.Buddy,
-            ExtremeRoleType.Crewmate,
-            ExtremeRoleId.Buddy.ToString(),
-            ColorPalette.BuddyOrange,
-            false, true,
-            false, false,
-            tab: OptionTab.Combination)
-        { }
-
-        public string GetFakeOptionString() => "";
-
-        public void IntroBeginSetUp()
-        {
-            this.buddy = this.getSameBuddy();
-
-            if (IsAwake || 
-                !this.CanHasAnotherRole || 
-                this.AnotherRole == null) { return; }
-
-            this.hiddeRole = this.AnotherRole;
-            this.CanHasAnotherRole = false;
-            this.AnotherRole = null;
-        }
-
-        public void IntroEndSetUp()
-        {
-            return;
-        }
-
-        public void Update(PlayerControl rolePlayer)
-        {
-            if (!this.awake &&
-                this.buddy != null &&
-                rolePlayer != null &&
-                rolePlayer.Data != null &&
-                rolePlayer.Data.Tasks.Count != 0)
-            {
-                foreach (GameData.PlayerInfo playerId in this.buddy.PlayerInfo)
+                int count = playerName.Count;
+                if (count > 1)
                 {
-                    if (Player.GetPlayerTaskGage(playerId) < this.awakeTaskGage)
-                    {
-                        if (this.hiddeRole is IRoleAbility abilityRole)
-                        {
-                            abilityRole.Button.SetButtonShow(false);
-                        }
-                        return;
-                    }
+                    playerName.Add(Translation.GetString("andFirst"));
                 }
-                this.awake = true;
-                this.HasOtherVision = this.awakeHasOtherVision;
-                if (this.hiddeRole != null)
+                else if (count == 1)
                 {
-                    this.AnotherRole = this.hiddeRole;
-                    this.CanHasAnotherRole = true;
-                    if (this.AnotherRole is IRoleAbility abilityRole)
+                    playerName.Add(Translation.GetString("and"));
+                }
+                playerName.Add(player.PlayerName);
+            }
+            return string.Concat(playerName);
+        }
+
+        public bool Contains(GameData.PlayerInfo player) => this.buddy.Contains(player);
+
+        public bool Contains(byte playerId) => this.bytedBuddy.Contains(playerId);
+
+        public void Add(GameData.PlayerInfo player)
+        {
+            this.buddy.Add(player);
+            this.bytedBuddy.Add(player.PlayerId);
+        }
+    }
+
+    private float awakeTaskGage;
+    private bool awake;
+    private bool awakeHasOtherVision;
+    private BuddyContainer buddy;
+    private SingleRoleBase hiddeRole = null;
+
+    public Buddy() : base(
+        ExtremeRoleId.Buddy,
+        ExtremeRoleType.Crewmate,
+        ExtremeRoleId.Buddy.ToString(),
+        ColorPalette.BuddyOrange,
+        false, true,
+        false, false,
+        tab: OptionTab.Combination)
+    { }
+
+    public string GetFakeOptionString() => "";
+
+    public void IntroBeginSetUp()
+    {
+        this.buddy = this.getSameBuddy();
+
+        if (IsAwake || 
+            !this.CanHasAnotherRole || 
+            this.AnotherRole == null) { return; }
+
+        this.hiddeRole = this.AnotherRole;
+        this.CanHasAnotherRole = false;
+        this.AnotherRole = null;
+    }
+
+    public void IntroEndSetUp()
+    {
+        return;
+    }
+
+    public void Update(PlayerControl rolePlayer)
+    {
+        if (!this.awake &&
+            this.buddy is not null &&
+            rolePlayer &&
+            rolePlayer.Data != null &&
+            rolePlayer.Data.Tasks.Count != 0)
+        {
+            foreach (GameData.PlayerInfo playerId in this.buddy.PlayerInfo)
+            {
+                if (Player.GetPlayerTaskGage(playerId) < this.awakeTaskGage)
+                {
+                    if (this.hiddeRole is IRoleAbility abilityRole)
                     {
-                        abilityRole.Button.SetButtonShow(true);
+                        abilityRole.Button.SetButtonShow(false);
                     }
-                    this.hiddeRole = null;
+                    return;
                 }
             }
-        }
-
-        public override Color GetTargetRoleSeeColor(
-            SingleRoleBase targetRole, byte targetPlayerId)
-        {
-            if (IsAwake && 
-                this.buddy is not null &&
-                this.buddy.Contains(targetPlayerId))
+            this.awake = true;
+            this.HasOtherVision = this.awakeHasOtherVision;
+            if (this.hiddeRole != null)
             {
-                return ColorPalette.BuddyOrange;
-            }
-            return base.GetTargetRoleSeeColor(targetRole, targetPlayerId);
-        }
-
-        public override string GetColoredRoleName(bool isTruthColor = false)
-        {
-            if (isTruthColor || IsAwake)
-            {
-                return base.GetColoredRoleName();
-            }
-            else
-            {
-                return Design.ColoedString(
-                    Palette.White,
-                    Translation.GetString(RoleTypes.Crewmate.ToString()));
-            }
-        }
-
-        public override string GetFullDescription()
-        {
-            if (IsAwake)
-            {
-                return string.Format(
-                    base.GetFullDescription(),
-                    this.buddy == null ? string.Empty : this.buddy.GetAllPlayerName());
-            }
-            else
-            {
-                return Translation.GetString(
-                    $"{RoleTypes.Crewmate}FullDescription");
-            }
-        }
-
-        public override string GetImportantText(bool isContainFakeTask = true)
-        {
-            if (IsAwake)
-            {
-                return base.GetImportantText(isContainFakeTask);
-
-            }
-            else
-            {
-                return Design.ColoedString(
-                    Palette.White,
-                    $"{this.GetColoredRoleName()}: {Translation.GetString("crewImportantText")}");
-            }
-        }
-
-        public override string GetIntroDescription()
-        {
-            if (IsAwake)
-            {
-                return string.Format(
-                    base.GetIntroDescription(),
-                    this.buddy == null ? string.Empty : this.buddy.GetAllPlayerName());
-            }
-            else
-            {
-                return Design.ColoedString(
-                    Palette.CrewmateBlue,
-                    CachedPlayerControl.LocalPlayer.Data.Role.Blurb);
-            }
-        }
-
-        public override string GetRoleTag() => "●";
-
-        public override Color GetNameColor(bool isTruthColor = false)
-        {
-            if (isTruthColor || IsAwake)
-            {
-                return base.GetNameColor(isTruthColor);
-            }
-            else
-            {
-                return Palette.White;
-            }
-        }
-
-        public override string GetRolePlayerNameTag(
-            SingleRoleBase targetRole, byte targetPlayerId)
-        {
-            if (IsAwake &&
-                this.buddy.Contains(targetPlayerId))
-            {
-                return Design.ColoedString(
-                    ColorPalette.BuddyOrange,
-                    $" {GetRoleTag()}");
-            }
-
-            return base.GetRolePlayerNameTag(targetRole, targetPlayerId);
-        }
-
-        protected override void CreateSpecificOption(IOption parentOps)
-        {
-            CreateIntOption(
-                BuddyOption.AwakeTaskGage,
-                50, 0, 100, 10,
-                parentOps,
-                format: OptionUnit.Percentage);
-        }
-
-        protected override void RoleSpecificInit()
-        {
-            this.awakeTaskGage = (float)OptionHolder.AllOption[
-               GetRoleOptionId(BuddyOption.AwakeTaskGage)].GetValue() / 100.0f;
-
-            this.awakeHasOtherVision = this.HasOtherVision;
-
-            if (this.awakeTaskGage <= 0.0f)
-            {
-                this.awake = true;
-                this.HasOtherVision = this.awakeHasOtherVision;
-            }
-            else
-            {
-                this.awake = false;
-                this.HasOtherVision = false;
-            }
-        }
-
-        private BuddyContainer getSameBuddy()
-        {
-
-            BuddyContainer buddy = new BuddyContainer();
-
-            foreach (var (playerId, role) in ExtremeRoleManager.GameRole)
-            {
-                var player = GameData.Instance.GetPlayerById(playerId);
-                if (this.IsSameControlId(role) &&
-                    (!player.IsDead || !player.Disconnected))
+                this.AnotherRole = this.hiddeRole;
+                this.CanHasAnotherRole = true;
+                if (this.AnotherRole is IRoleAbility abilityRole)
                 {
-                    buddy.Add(player);
+                    abilityRole.Button.SetButtonShow(true);
                 }
+                this.hiddeRole = null;
             }
+        }
+    }
 
-            return buddy;
+    public override Color GetTargetRoleSeeColor(
+        SingleRoleBase targetRole, byte targetPlayerId)
+    {
+        if (IsAwake && 
+            this.buddy is not null &&
+            this.buddy.Contains(targetPlayerId))
+        {
+            return ColorPalette.BuddyOrange;
+        }
+        return base.GetTargetRoleSeeColor(targetRole, targetPlayerId);
+    }
+
+    public override string GetColoredRoleName(bool isTruthColor = false)
+    {
+        if (isTruthColor || IsAwake)
+        {
+            return base.GetColoredRoleName();
+        }
+        else
+        {
+            return Design.ColoedString(
+                Palette.White,
+                Translation.GetString(RoleTypes.Crewmate.ToString()));
+        }
+    }
+
+    public override string GetFullDescription()
+    {
+        if (IsAwake)
+        {
+            return string.Format(
+                base.GetFullDescription(),
+                this.buddy is null ? 
+                    string.Empty : this.buddy.GetAllPlayerName());
+        }
+        else
+        {
+            return Translation.GetString(
+                $"{RoleTypes.Crewmate}FullDescription");
+        }
+    }
+
+    public override string GetImportantText(bool isContainFakeTask = true)
+    {
+        if (IsAwake)
+        {
+            return base.GetImportantText(isContainFakeTask);
 
         }
+        else
+        {
+            return Design.ColoedString(
+                Palette.White,
+                $"{this.GetColoredRoleName()}: {Translation.GetString("crewImportantText")}");
+        }
+    }
+
+    public override string GetIntroDescription()
+    {
+        if (IsAwake)
+        {
+            return string.Format(
+                base.GetIntroDescription(),
+                this.buddy is null ? 
+                    string.Empty : this.buddy.GetAllPlayerName());
+        }
+        else
+        {
+            return Design.ColoedString(
+                Palette.CrewmateBlue,
+                CachedPlayerControl.LocalPlayer.Data.Role.Blurb);
+        }
+    }
+
+    public override string GetRoleTag() => "●";
+
+    public override Color GetNameColor(bool isTruthColor = false)
+    {
+        if (isTruthColor || IsAwake)
+        {
+            return base.GetNameColor(isTruthColor);
+        }
+        else
+        {
+            return Palette.White;
+        }
+    }
+
+    public override string GetRolePlayerNameTag(
+        SingleRoleBase targetRole, byte targetPlayerId)
+    {
+        if (IsAwake &&
+            this.buddy is not null &&
+            this.buddy.Contains(targetPlayerId))
+        {
+            return Design.ColoedString(
+                ColorPalette.BuddyOrange,
+                $" {GetRoleTag()}");
+        }
+
+        return base.GetRolePlayerNameTag(targetRole, targetPlayerId);
+    }
+
+    protected override void CreateSpecificOption(IOption parentOps)
+    {
+        CreateIntOption(
+            BuddyOption.AwakeTaskGage,
+            50, 0, 100, 10,
+            parentOps,
+            format: OptionUnit.Percentage);
+    }
+
+    protected override void RoleSpecificInit()
+    {
+        this.awakeTaskGage = (float)OptionHolder.AllOption[
+           GetRoleOptionId(BuddyOption.AwakeTaskGage)].GetValue() / 100.0f;
+
+        this.awakeHasOtherVision = this.HasOtherVision;
+
+        if (this.awakeTaskGage <= 0.0f)
+        {
+            this.awake = true;
+            this.HasOtherVision = this.awakeHasOtherVision;
+        }
+        else
+        {
+            this.awake = false;
+            this.HasOtherVision = false;
+        }
+    }
+
+    private BuddyContainer getSameBuddy()
+    {
+
+        BuddyContainer buddy = new BuddyContainer();
+
+        foreach (var (playerId, role) in ExtremeRoleManager.GameRole)
+        {
+            var player = GameData.Instance.GetPlayerById(playerId);
+            if (player != null &&
+                this.IsSameControlId(role) &&
+                (!player.IsDead || !player.Disconnected))
+            {
+                buddy.Add(player);
+            }
+        }
+
+        return buddy;
+
     }
 }
