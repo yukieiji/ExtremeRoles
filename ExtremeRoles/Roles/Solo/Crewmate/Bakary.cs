f2d171ab1828b48ed933fda1b2c7589d011b2c4b
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-04-16 22:13:46 +0900

    refactor:Bakary to new options

--- ExtremeRoles/Roles/Solo/Crewmate/Bakary.cs
+++ ExtremeRoles/Roles/Solo/Crewmate/Bakary.cs
@@ -1,45 +1,45 @@
 ﻿using ExtremeRoles.Module;
+using ExtremeRoles.Module.CustomOption;
 using ExtremeRoles.Roles.API;
 
-namespace ExtremeRoles.Roles.Solo.Crewmate
+namespace ExtremeRoles.Roles.Solo.Crewmate;
+
+public sealed class Bakary : SingleRoleBase
 {
-    public sealed class Bakary : SingleRoleBase
+    public enum BakaryOption
     {
-        public enum BakaryOption
-        {
-            ChangeCooking,
-            GoodBakeTime,
-            BadBakeTime,
-        }
-
-        public Bakary() : base(
-            ExtremeRoleId.Bakary,
-            ExtremeRoleType.Crewmate,
-            ExtremeRoleId.Bakary.ToString(),
-            ColorPalette.BakaryWheatColor,
-            false, true, false, false)
-        { }
-
-        protected override void CreateSpecificOption(
-            IOption parentOps)
-        {
-            var changeCooking = CreateBoolOption(
-                BakaryOption.ChangeCooking,
-                true, parentOps);
-
-            CreateFloatOption(
-                BakaryOption.GoodBakeTime,
-                60.0f, 45.0f, 75.0f, 0.5f,
-                changeCooking, format: OptionUnit.Second,
-                invert: true, enableCheckOption: parentOps);
-            CreateFloatOption(
-                BakaryOption.BadBakeTime,
-                120.0f, 105.0f, 135.0f, 0.5f,
-                changeCooking, format: OptionUnit.Second,
-                invert: true, enableCheckOption: parentOps);
-        }
-
-        protected override void RoleSpecificInit()
-        { }
+        ChangeCooking,
+        GoodBakeTime,
+        BadBakeTime,
     }
+
+    public Bakary() : base(
+        ExtremeRoleId.Bakary,
+        ExtremeRoleType.Crewmate,
+        ExtremeRoleId.Bakary.ToString(),
+        ColorPalette.BakaryWheatColor,
+        false, true, false, false)
+    { }
+
+    protected override void CreateSpecificOption(
+        IOptionInfo parentOps)
+    {
+        var changeCooking = CreateBoolOption(
+            BakaryOption.ChangeCooking,
+            true, parentOps);
+
+        CreateFloatOption(
+            BakaryOption.GoodBakeTime,
+            60.0f, 45.0f, 75.0f, 0.5f,
+            changeCooking, format: OptionUnit.Second,
+            invert: true, enableCheckOption: parentOps);
+        CreateFloatOption(
+            BakaryOption.BadBakeTime,
+            120.0f, 105.0f, 135.0f, 0.5f,
+            changeCooking, format: OptionUnit.Second,
+            invert: true, enableCheckOption: parentOps);
+    }
+
+    protected override void RoleSpecificInit()
+    { }
 }
