e29a3d80823ba64f369c4c8d867016a079b268db
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-04-17 15:05:28 +0900

    feat:team show logics

--- ExtremeRoles/Patches/Controller/ExileControllerPatch.cs
+++ ExtremeRoles/Patches/Controller/ExileControllerPatch.cs
@@ -1,13 +1,20 @@
-﻿using HarmonyLib;
+﻿using System;
+using System.Linq;
+using HarmonyLib;
 
 using AmongUs.GameOptions;
 
+using ExtremeRoles.GameMode;
+using ExtremeRoles.GameMode.Option.ShipGlobal;
 using ExtremeRoles.Module;
+using ExtremeRoles.Module.ExtremeShipStatus;
 using ExtremeRoles.GhostRoles;
 using ExtremeRoles.Roles;
 using ExtremeRoles.Roles.API;
 using ExtremeRoles.Roles.API.Interface;
 using ExtremeRoles.Performance;
+
+using Il2CppObject = Il2CppSystem.Object;
 
 namespace ExtremeRoles.Patches.Controller;
 
@@ -21,43 +28,20 @@ public static class ExileControllerBeginePatch
     {
 
         var state = ExtremeRolesPlugin.ShipState;
+        bool confirmImp = GameManager.Instance.LogicOptions.GetConfirmImpostor();
+        var shipOption = ExtremeGameModeManager.Instance.ShipOption;
 
-        if (!state.AssassinMeetingTrigger) { return true; }
+        if (state.AssassinMeetingTrigger)
+        {
+            assassinMeetingEndBegin(__instance, state);
+            return false; 
+        }
+        else if (GameManager.Instance.LogicOptions.GetConfirmImpostor())
+        {
 
-			if (__instance.specialInputHandler != null)
-			{
-				__instance.specialInputHandler.disableVirtualCursor = true;
-			}
-			ExileController.Instance = __instance;
-			ControllerManager.Instance.CloseAndResetAll();
-
-			__instance.exiled = null;
-			__instance.Text.gameObject.SetActive(false);
-        __instance.Text.text = string.Empty;
-
-        GameData.PlayerInfo player = GameData.Instance.GetPlayerById(
-            state.IsMarinPlayerId);
-
-        string printStr;
-
-            if (state.IsAssassinateMarin)
-            {
-                printStr = player?.PlayerName + Helper.Translation.GetString(
-                    "assassinateMarinSucsess");
-            }
-            else
-            {
-                printStr = player?.PlayerName + Helper.Translation.GetString(
-                    "assassinateMarinFail");
-            }       
-            
-            if (__instance.Player)
-            {
-                __instance.Player.gameObject.SetActive(false);
-            }
-            __instance.completeString = printStr;
-			__instance.ImpostorText.text = string.Empty;
-			__instance.StartCoroutine(__instance.Animate());
+            __instance.StartCoroutine(__instance.Animate());
+            return false;
+        }
         return false;
     }
 
@@ -85,6 +69,209 @@ public static class ExileControllerBeginePatch
 
         __instance.StartCoroutine(
             Effects.Bloop(0.25f, infoText.transform, 1f, 0.5f));
+    }
+
+    private static void assassinMeetingEndBegin(
+        ExileController instance, ExtremeShipStatus state)
+    {
+        setExiledTarget(instance, null);
+        GameData.PlayerInfo player = GameData.Instance.GetPlayerById(
+            state.IsMarinPlayerId);
+
+        string transKey = state.IsAssassinAssign ?
+            "assassinateMarinSucsess" : "assassinateMarinFail";
+        string printStr = $"{player?.PlayerName}{Helper.Translation.GetString(transKey)}";
+
+        if (instance.Player)
+        {
+            instance.Player.gameObject.SetActive(false);
+        }
+        instance.completeString = printStr;
+        instance.ImpostorText.text = string.Empty;
+
+        instance.StartCoroutine(instance.Animate());
+    }
+
+    private static void confirmExil(
+        ExileController instance,
+        GameData.PlayerInfo exiled,
+        ConfirmExilMode mode, bool isShowRole, bool tie)
+    {
+        setExiledTarget(instance, exiled);
+        var transController = FastDestroyableSingleton<TranslationController>.Instance;
+
+        var allPlayer = GameData.Instance.AllPlayers.ToArray();
+        var alivePlayers = allPlayer.Where(
+            x => x.PlayerId != exiled.PlayerId && x.IsDead && x.Disconnected);
+        var allRoles = ExtremeRoleManager.GameRole;
+
+        int aliveImpNum = Enumerable.Count(
+            alivePlayers,
+            (GameData.PlayerInfo p) =>
+            {
+                return allRoles[p.PlayerId].IsImpostor();
+            });
+        int aliveCrewNum = Enumerable.Count(
+            alivePlayers,
+            (GameData.PlayerInfo p) =>
+            {
+                return allRoles[p.PlayerId].IsCrewmate();
+            });
+        int aliveNeutNum = Enumerable.Count(
+            alivePlayers,
+            (GameData.PlayerInfo p) =>
+            {
+                return allRoles[p.PlayerId].IsNeutral();
+            });
+
+        string completeString = string.Empty;
+
+        if (exiled != null)
+        {
+            string playerName = exiled.PlayerName;
+            var exiledPlayerRole = allRoles[exiled.PlayerId];
+            switch (mode)
+            {
+                case ConfirmExilMode.Impostor:
+                    int allImpNum = Enumerable.Count(
+                        allPlayer, (GameData.PlayerInfo p) => allRoles[p.PlayerId].IsImpostor());
+                    bool isExiledIsImp = exiledPlayerRole.IsImpostor();
+                    StringNames stringKey;
+                    if (isExiledIsImp && allImpNum > 1)
+                    {
+                        stringKey = StringNames.ExileTextPP;
+                    }
+                    else if (isExiledIsImp)
+                    {
+                        stringKey = StringNames.ExileTextSP;
+                    }
+                    else if (allImpNum > 1)
+                    {
+                        stringKey = StringNames.ExileTextPN;
+                    }
+                    else
+                    {
+                        stringKey = StringNames.ExileTextSN;
+                    }
+                    completeString = transController.GetString(
+                        stringKey, new Il2CppObject[] { playerName });
+                    break;
+                case ConfirmExilMode.Crewmate:
+                    int allCrewNum = Enumerable.Count(
+                        allPlayer, (GameData.PlayerInfo p) => allRoles[p.PlayerId].IsCrewmate());
+                    bool isExiledIsCrew = exiledPlayerRole.IsCrewmate();
+                    string transCrewStringKey;
+                    if (isExiledIsCrew && allCrewNum > 1)
+                    {
+                        transCrewStringKey = "ExileTextPPCrew";
+                    }
+                    else if (isExiledIsCrew)
+                    {
+                        transCrewStringKey = "ExileTextSPCrew";
+                    }
+                    else if (allCrewNum > 1)
+                    {
+                        transCrewStringKey = "ExileTextPNCrew";
+                    }
+                    else
+                    {
+                        transCrewStringKey = "ExileTextSNCrew";
+                    }
+                    completeString = string.Format(
+                        Helper.Translation.GetString(transCrewStringKey), playerName);
+                    break;
+                case ConfirmExilMode.Neutral:
+                    int allNeutNum = Enumerable.Count(
+                        allPlayer, (GameData.PlayerInfo p) => allRoles[p.PlayerId].IsCrewmate());
+                    bool isExiledIsNeut = exiledPlayerRole.IsCrewmate();
+                    string transNeutStringKey;
+                    if (isExiledIsNeut && allNeutNum > 1)
+                    {
+                        transNeutStringKey = "ExileTextPPNeut";
+                    }
+                    else if (isExiledIsNeut)
+                    {
+                        transNeutStringKey = "ExileTextSPNeut";
+                    }
+                    else if (allNeutNum > 1)
+                    {
+                        transNeutStringKey = "ExileTextPNNeut";
+                    }
+                    else
+                    {
+                        transNeutStringKey = "ExileTextSNNeut";
+                    }
+                    completeString = string.Format(
+                        Helper.Translation.GetString(transNeutStringKey), playerName);
+                    break;
+                case ConfirmExilMode.AllTeam:
+                    completeString = string.Format(
+                        Helper.Translation.GetString("ExileTextAllTeam"),
+                        exiledPlayerRole.Team,
+                        playerName);
+                    break;
+                default:
+                    break;
+            }
+
+            instance.Player.UpdateFromEitherPlayerDataOrCache(
+                exiled, PlayerOutfitType.Default, PlayerMaterial.MaskType.Exile, false);
+            instance.Player.ToggleName(false);
+            instance.Player.SetCustomHatPosition(instance.exileHatPosition);
+            instance.Player.SetCustomVisorPosition(instance.exileVisorPosition);
+
+            SkinViewData skin = ShipStatus.Instance.CosmeticsCache.GetSkin(
+                exiled.Outfits[PlayerOutfitType.Default].SkinId);
+            instance.Player.FixSkinSprite(skin.EjectFrame);
+        }
+        else
+        {
+            completeString = transController.GetString(
+                tie ? StringNames.NoExileTie : StringNames.NoExileSkip,
+                Array.Empty<Il2CppObject>());
+            instance.Player.gameObject.SetActive(false);
+        }
+        
+        instance.completeString = completeString;
+        instance.ImpostorText.text = mode switch
+        {
+            ConfirmExilMode.Impostor => transController.GetString(
+                aliveImpNum == 1 ? StringNames.ImpostorsRemainS : StringNames.ImpostorsRemainP,
+                new Il2CppObject[] { aliveImpNum }),
+
+            ConfirmExilMode.Crewmate => string.Format(
+                Helper.Translation.GetString(
+                    aliveCrewNum == 1 ?
+                        "CrewmateRemainS" :
+                        "CrewmateRemainP"), aliveCrewNum),
+
+            ConfirmExilMode.Neutral => string.Format(
+                Helper.Translation.GetString(
+                    aliveNeutNum == 1 ?
+                        "NeutralRemainS" :
+                        "NeutralRemainP"), aliveNeutNum),
+
+            ConfirmExilMode.AllTeam => string.Format(
+                Helper.Translation.GetString("AllTeamAlive"),
+                aliveCrewNum, aliveImpNum, aliveNeutNum),
+
+            _ => string.Empty
+        };
+    }
+
+    private static void setExiledTarget(
+        ExileController instance, GameData.PlayerInfo player)
+    {
+        if (instance.specialInputHandler != null)
+        {
+            instance.specialInputHandler.disableVirtualCursor = true;
+        }
+        ExileController.Instance = instance;
+        ControllerManager.Instance.CloseAndResetAll();
+
+        instance.exiled = player;
+        instance.Text.gameObject.SetActive(false);
+        instance.Text.text = string.Empty;
     }
 }
 
