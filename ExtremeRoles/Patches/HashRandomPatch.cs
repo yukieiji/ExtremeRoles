ccd575b7f3a6f2428621e59b6aa49fefb16feb4a
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-04-21 21:41:57 +0900

    refactor:HashRandom

--- ExtremeRoles/Patches/HashRandomPatch.cs
+++ ExtremeRoles/Patches/HashRandomPatch.cs
@@ -1,41 +1,43 @@
 ﻿using System;
 using HarmonyLib;
 
-namespace ExtremeRoles.Patches
+using ExtremeRoles.Module.CustomOption;
+
+namespace ExtremeRoles.Patches;
+
+[HarmonyPatch(typeof(HashRandom), nameof(HashRandom.FastNext))]
+public static class HashRandomFastNextPatch
 {
-    [HarmonyPatch(typeof(HashRandom), nameof(HashRandom.FastNext))]
-    public static class HashRandomFastNextPatch
+    public static bool Prefix(
+        ref int __result,
+        [HarmonyArgument(0)] int maxInt)
     {
-        public static bool Prefix(
-            ref int __result,
-            [HarmonyArgument(0)] int maxInt)
+        if (AllOptionHolder.Instance.GetValue<bool>(
+            (int)OptionHolder.CommonOptionKey.UseStrongRandomGen))
         {
-            if (OptionHolder.AllOption[
-                (int)OptionHolder.CommonOptionKey.UseStrongRandomGen].GetValue())
-            {
-                __result = RandomGenerator.Instance.Next(maxInt);
-                return false;
-            }
-            return true;
+            __result = RandomGenerator.Instance.Next(maxInt);
+            return false;
         }
+        return true;
     }
-    [HarmonyPatch(
-        typeof(HashRandom),
-        nameof(HashRandom.Next),
-        new Type[] { typeof(int) })]
-    public static class HashRandomNextPatch
+}
+
+[HarmonyPatch(
+    typeof(HashRandom),
+    nameof(HashRandom.Next),
+    new Type[] { typeof(int) })]
+public static class HashRandomNextPatch
+{
+    public static bool Prefix(
+        ref int __result,
+        [HarmonyArgument(0)] int maxInt)
     {
-        public static bool Prefix(
-            ref int __result,
-            [HarmonyArgument(0)] int maxInt)
+        if (AllOptionHolder.Instance.GetValue<bool>(
+            (int)OptionHolder.CommonOptionKey.UseStrongRandomGen))
         {
-            if (OptionHolder.AllOption[
-                (int)OptionHolder.CommonOptionKey.UseStrongRandomGen].GetValue())
-            {
-                __result = RandomGenerator.Instance.Next(maxInt);
-                return false;
-            }
-            return true;
+            __result = RandomGenerator.Instance.Next(maxInt);
+            return false;
         }
+        return true;
     }
 }
