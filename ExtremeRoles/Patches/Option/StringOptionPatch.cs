d9485cf27edbb3c227d62698ee42d49d706bf2dd
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-04-22 00:04:39 +0900

    refactor:StringOption

--- ExtremeRoles/Patches/Option/StringOptionPatch.cs
+++ ExtremeRoles/Patches/Option/StringOptionPatch.cs
@@ -3,65 +3,43 @@
 using HarmonyLib;
 using UnityEngine;
 
-using ExtremeRoles.Module;
+using ExtremeRoles.Module.CustomOption;
 using ExtremeRoles.Module.CustomMonoBehaviour;
 
-namespace ExtremeRoles.Patches.Option
+namespace ExtremeRoles.Patches.Option;
+
+[HarmonyPatch]
+public static class StringOptionSelectionUpdatePatch
 {
-    [HarmonyPatch]
-    public static class StringOptionSelectionUpdatePatch
+    private const KeyCode maxSelectionKey = KeyCode.LeftControl;
+    private const KeyCode skipSelectionKey = KeyCode.LeftShift;
+    
+    private const int defaultStep = 1;
+    private const int skipStep = 10;
+
+    [HarmonyPatch(typeof(StringOption), nameof(StringOption.Decrease))]
+    public static class StringOptionDecreasePatch
     {
-        private const KeyCode maxSelectionKey = KeyCode.LeftControl;
-        private const KeyCode skipSelectionKey = KeyCode.LeftShift;
-        
-        private const int defaultStep = 1;
-        private const int skipStep = 10;
-
-        [HarmonyPatch(typeof(StringOption), nameof(StringOption.Decrease))]
-        public static class StringOptionDecreasePatch
+        public static bool Prefix(StringOption __instance)
         {
-            public static bool Prefix(StringOption __instance)
-            {
 
-                string idStr = __instance.gameObject.name.Replace(
-                    OptionMenuTab.StringOptionName, string.Empty);
+            string idStr = __instance.gameObject.name.Replace(
+                OptionMenuTab.StringOptionName, string.Empty);
 
-                if (!int.TryParse(idStr, out int id)) { return true; };
+            if (!int.TryParse(idStr, out int id)) { return true; };
 
-                IOption option = OptionHolder.AllOption[id];
+            IOptionInfo option = AllOptionHolder.Instance.GetIOption(id);
 
-                int step = Input.GetKey(skipSelectionKey) ? skipStep : defaultStep;
-                int newSelection = Input.GetKey(maxSelectionKey) ? 0 : option.CurSelection - step;
-                option.UpdateSelection(newSelection);
-                
-                return false;
-            }
-        }
-
-        [HarmonyPatch(typeof(StringOption), nameof(StringOption.Increase))]
-        public static class StringOptionIncreasePatch
-        {
-            public static bool Prefix(StringOption __instance)
-            {
-                string idStr = __instance.gameObject.name.Replace(
-                    OptionMenuTab.StringOptionName, string.Empty);
-
-                if (!int.TryParse(idStr, out int id)) { return true; };
-
-                IOption option = OptionHolder.AllOption[id];
-
-                int step = Input.GetKey(skipSelectionKey) ? skipStep : defaultStep;
-                int newSelection = Input.GetKey(maxSelectionKey) ? 
-                    option.ValueCount - 1 : option.CurSelection + step;
-                option.UpdateSelection(newSelection);
-
-                return false;
-            }
+            int step = Input.GetKey(skipSelectionKey) ? skipStep : defaultStep;
+            int newSelection = Input.GetKey(maxSelectionKey) ? 0 : option.CurSelection - step;
+            option.UpdateSelection(newSelection);
+            
+            return false;
         }
     }
 
-    [HarmonyPatch(typeof(StringOption), nameof(StringOption.OnEnable))]
-    public static class StringOptionOnEnablePatch
+    [HarmonyPatch(typeof(StringOption), nameof(StringOption.Increase))]
+    public static class StringOptionIncreasePatch
     {
         public static bool Prefix(StringOption __instance)
         {
@@ -70,14 +48,35 @@ namespace ExtremeRoles.Patches.Option
 
             if (!int.TryParse(idStr, out int id)) { return true; };
 
-            IOption option = OptionHolder.AllOption[id];
+            IOptionInfo option = AllOptionHolder.Instance.GetIOption(id);
 
-            __instance.OnValueChanged = new Action<OptionBehaviour>((o) => { });
-            __instance.TitleText.text = option.GetTranslatedName();
-            __instance.Value = __instance.oldValue = option.CurSelection;
-            __instance.ValueText.text = option.GetTranslatedValue();
+            int step = Input.GetKey(skipSelectionKey) ? skipStep : defaultStep;
+            int newSelection = Input.GetKey(maxSelectionKey) ? 
+                option.ValueCount - 1 : option.CurSelection + step;
+            option.UpdateSelection(newSelection);
 
             return false;
         }
+    }
+}
+
+[HarmonyPatch(typeof(StringOption), nameof(StringOption.OnEnable))]
+public static class StringOptionOnEnablePatch
+{
+    public static bool Prefix(StringOption __instance)
+    {
+        string idStr = __instance.gameObject.name.Replace(
+            OptionMenuTab.StringOptionName, string.Empty);
+
+        if (!int.TryParse(idStr, out int id)) { return true; };
+
+        IOptionInfo option = AllOptionHolder.Instance.GetIOption(id);
+
+        __instance.OnValueChanged = new Action<OptionBehaviour>((o) => { });
+        __instance.TitleText.text = option.GetTranslatedName();
+        __instance.Value = __instance.oldValue = option.CurSelection;
+        __instance.ValueText.text = option.GetTranslatedValue();
+
+        return false;
     }
 }
