30b8bd88b65d9295ab7be56ba25b986d60669607
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-04-21 23:25:08 +0900

    refactor:Jester

--- ExtremeRoles/Roles/Solo/Neutral/Jester.cs
+++ ExtremeRoles/Roles/Solo/Neutral/Jester.cs
@@ -1,4 +1,5 @@
 ﻿using ExtremeRoles.Module;
+using ExtremeRoles.Module.CustomOption;
 using ExtremeRoles.Resources;
 using ExtremeRoles.Roles.API;
 using ExtremeRoles.Roles.API.Extension.State;
@@ -6,189 +7,187 @@ using ExtremeRoles.Roles.API.Extension.Neutral;
 using ExtremeRoles.Roles.API.Interface;
 using ExtremeRoles.Performance;
 
-namespace ExtremeRoles.Roles.Solo.Neutral
+namespace ExtremeRoles.Roles.Solo.Neutral;
+
+public sealed class Jester : SingleRoleBase, IRoleAbility
 {
-    public sealed class Jester : SingleRoleBase, IRoleAbility
+    public enum JesterOption
     {
+        UseSabotage,
+        OutburstDistance
+    }
 
-        public enum JesterOption
+    public ExtremeAbilityButton Button
+    { 
+        get => this.outburstButton;
+        set
         {
-            UseSabotage,
-            OutburstDistance
+            this.outburstButton = value;
         }
+    }
 
-        public ExtremeAbilityButton Button
-        { 
-            get => this.outburstButton;
-            set
+    private float outburstDistance;
+    private PlayerControl tmpTarget;
+    private PlayerControl outburstTarget;
+    private ExtremeAbilityButton outburstButton;
+
+    public Jester(): base(
+        ExtremeRoleId.Jester,
+        ExtremeRoleType.Neutral,
+        ExtremeRoleId.Jester.ToString(),
+        ColorPalette.JesterPink,
+        false, false, false, false)
+    { }
+
+    public static void OutburstKill(
+        byte outburstTargetPlayerId, byte killTargetPlayerId)
+    {
+        if (outburstTargetPlayerId != CachedPlayerControl.LocalPlayer.PlayerId) { return; }
+
+        PlayerControl killer = Helper.Player.GetPlayerControlById(outburstTargetPlayerId);
+        PlayerControl target = Helper.Player.GetPlayerControlById(killTargetPlayerId);
+
+        if (killer == null || target == null) { return; }
+
+        byte killerId = killer.PlayerId;
+        byte targetId = target.PlayerId;
+
+        var killerRole = ExtremeRoleManager.GameRole[killerId];
+        var targetRole = ExtremeRoleManager.GameRole[targetId];
+
+        if (!killer.CanMove) { return; }
+
+        bool canKill = killerRole.TryRolePlayerKillTo(
+             killer, target);
+        if (!canKill) { return; }
+
+        canKill = targetRole.TryRolePlayerKilledFrom(
+            target, killer);
+        if (!canKill) { return; }
+
+        var multiAssignRole = killerRole as MultiAssignRoleBase;
+        if (multiAssignRole != null)
+        {
+            if (multiAssignRole.AnotherRole != null)
             {
-                this.outburstButton = value;
+                canKill = multiAssignRole.AnotherRole.TryRolePlayerKillTo(
+                    killer, target);
+                if (!canKill) { return; }
             }
         }
 
-        private float outburstDistance;
-        private PlayerControl tmpTarget;
-        private PlayerControl outburstTarget;
-        private ExtremeAbilityButton outburstButton;
-
-        public Jester(): base(
-            ExtremeRoleId.Jester,
-            ExtremeRoleType.Neutral,
-            ExtremeRoleId.Jester.ToString(),
-            ColorPalette.JesterPink,
-            false, false, false, false)
-        { }
-
-        public static void OutburstKill(
-            byte outburstTargetPlayerId, byte killTargetPlayerId)
+        multiAssignRole = targetRole as MultiAssignRoleBase;
+        if (multiAssignRole != null)
         {
-            if (outburstTargetPlayerId != CachedPlayerControl.LocalPlayer.PlayerId) { return; }
-
-            PlayerControl killer = Helper.Player.GetPlayerControlById(outburstTargetPlayerId);
-            PlayerControl target = Helper.Player.GetPlayerControlById(killTargetPlayerId);
-
-            if (killer == null || target == null) { return; }
-
-            byte killerId = killer.PlayerId;
-            byte targetId = target.PlayerId;
-
-            var killerRole = ExtremeRoleManager.GameRole[killerId];
-            var targetRole = ExtremeRoleManager.GameRole[targetId];
-
-            if (!killer.CanMove) { return; }
-
-            bool canKill = killerRole.TryRolePlayerKillTo(
-                killer, target);
-            if (!canKill) { return; }
-
-            canKill = targetRole.TryRolePlayerKilledFrom(
-                target, killer);
-            if (!canKill) { return; }
-
-            var multiAssignRole = killerRole as MultiAssignRoleBase;
-            if (multiAssignRole != null)
+            if (multiAssignRole.AnotherRole != null)
             {
-                if (multiAssignRole.AnotherRole != null)
-                {
-                    canKill = multiAssignRole.AnotherRole.TryRolePlayerKillTo(
-                        killer, target);
-                    if (!canKill) { return; }
-                }
+                canKill = multiAssignRole.AnotherRole.TryRolePlayerKilledFrom(
+                    target, killer);
+                if (!canKill) { return; }
             }
-
-            multiAssignRole = targetRole as MultiAssignRoleBase;
-            if (multiAssignRole != null)
-            {
-                if (multiAssignRole.AnotherRole != null)
-                {
-                    canKill = multiAssignRole.AnotherRole.TryRolePlayerKilledFrom(
-                        target, killer);
-                    if (!canKill) { return; }
-                }
-            }
-
-            if (Crewmate.BodyGuard.TryRpcKillGuardedBodyGuard(
-                    killer.PlayerId, target.PlayerId) ||
-                Patches.Button.KillButtonDoClickPatch.IsMissMuderKill(
-                    killer, target))
-            {
-                return;
-            }
-
-            Helper.Player.RpcUncheckMurderPlayer(
-                killerId, target.PlayerId,
-                byte.MaxValue);
         }
 
-        public void CreateAbility()
-        {
-            this.CreateAbilityCountButton(
-                "outburst",
-                Loader.CreateSpriteFromResources(
-                    Path.JesterOutburst),
-                abilityOff: CleanUp,
-                forceAbilityOff: () => { });
-        }
-
-        public override bool IsSameTeam(SingleRoleBase targetRole) =>
-            this.IsNeutralSameTeam(targetRole);
-
-        public bool IsAbilityUse()
-        {
-            this.tmpTarget = Helper.Player.GetClosestPlayerInRange(
-                CachedPlayerControl.LocalPlayer, this,
-                this.outburstDistance);
-            return this.IsCommonUse() && this.tmpTarget != null;
-        }
-
-        public override void ExiledAction(PlayerControl rolePlayer)
-        {
-            this.IsWin = true;
-        }
-
-        public bool UseAbility()
-        {
-            this.outburstTarget = this.tmpTarget;
-            return true;
-        }
-        public void CleanUp()
-        {
-            if (this.outburstTarget == null) { return; }
-            if (this.outburstTarget.Data.IsDead || this.outburstTarget.Data.Disconnected) { return; }
-            if (ExtremeRoleManager.GameRole.Count == 0) { return; }
-
-            var role = ExtremeRoleManager.GameRole[this.outburstTarget.PlayerId];
-            if (!role.CanKill()) { return; }
-
-            PlayerControl killTarget = Helper.Player.GetClosestPlayerInKillRange(
-                this.outburstTarget);
-
-            if (killTarget == null) { return; }
-            if (killTarget.Data.IsDead || killTarget.Data.Disconnected) { return; }
-            if (killTarget.PlayerId == CachedPlayerControl.LocalPlayer.PlayerId) { return; }
-            
-            using (var caller = RPCOperator.CreateCaller(
-                RPCOperator.Command.JesterOutburstKill))
-            {
-                caller.WriteByte(this.outburstTarget.PlayerId);
-                caller.WriteByte(killTarget.PlayerId);
-            }
-            OutburstKill(this.outburstTarget.PlayerId, killTarget.PlayerId);
-        }
-
-        protected override void CreateSpecificOption(
-            IOption parentOps)
-        {
-            CreateFloatOption(
-                JesterOption.OutburstDistance,
-                1.0f, 0.0f, 2.0f, 0.1f,
-                parentOps);
-
-            CreateBoolOption(
-                JesterOption.UseSabotage,
-                true, parentOps);
-
-            this.CreateAbilityCountOption(
-                parentOps, 5, 100, 2.0f);
-        }
-
-        protected override void RoleSpecificInit()
-        {
-            this.UseSabotage = OptionHolder.AllOption[
-                GetRoleOptionId(JesterOption.UseSabotage)].GetValue();
-            this.outburstDistance = OptionHolder.AllOption[
-                GetRoleOptionId(JesterOption.OutburstDistance)].GetValue();
-            this.RoleAbilityInit();
-        }
-
-        public void ResetOnMeetingStart()
+        if (Crewmate.BodyGuard.TryRpcKillGuardedBodyGuard(
+                killer.PlayerId, target.PlayerId) ||
+            Patches.Button.KillButtonDoClickPatch.IsMissMuderKill(
+                killer, target))
         {
             return;
         }
 
-        public void ResetOnMeetingEnd(GameData.PlayerInfo exiledPlayer = null)
+        Helper.Player.RpcUncheckMurderPlayer(
+            killerId, target.PlayerId,
+            byte.MaxValue);
+    }
+
+    public void CreateAbility()
+    {
+        this.CreateAbilityCountButton(
+            "outburst",
+            Loader.CreateSpriteFromResources(
+                Path.JesterOutburst),
+            abilityOff: CleanUp,
+            forceAbilityOff: () => { });
+    }
+
+    public override bool IsSameTeam(SingleRoleBase targetRole) =>
+        this.IsNeutralSameTeam(targetRole);
+
+    public bool IsAbilityUse()
+    {
+        this.tmpTarget = Helper.Player.GetClosestPlayerInRange(
+            CachedPlayerControl.LocalPlayer, this,
+            this.outburstDistance);
+        return this.IsCommonUse() && this.tmpTarget != null;
+    }
+
+    public override void ExiledAction(PlayerControl rolePlayer)
+    {
+        this.IsWin = true;
+    }
+
+    public bool UseAbility()
+    {
+        this.outburstTarget = this.tmpTarget;
+        return true;
+    }
+    public void CleanUp()
+    {
+        if (this.outburstTarget == null) { return; }
+        if (this.outburstTarget.Data.IsDead || this.outburstTarget.Data.Disconnected) { return; }
+        if (ExtremeRoleManager.GameRole.Count == 0) { return; }
+
+        var role = ExtremeRoleManager.GameRole[this.outburstTarget.PlayerId];
+        if (!role.CanKill()) { return; }
+
+        PlayerControl killTarget = Helper.Player.GetClosestPlayerInKillRange(
+            this.outburstTarget);
+
+        if (killTarget == null) { return; }
+        if (killTarget.Data.IsDead || killTarget.Data.Disconnected) { return; }
+        if (killTarget.PlayerId == CachedPlayerControl.LocalPlayer.PlayerId) { return; }
+        
+        using (var caller = RPCOperator.CreateCaller(
+            RPCOperator.Command.JesterOutburstKill))
         {
-            return;
+            caller.WriteByte(this.outburstTarget.PlayerId);
+            caller.WriteByte(killTarget.PlayerId);
         }
+        OutburstKill(this.outburstTarget.PlayerId, killTarget.PlayerId);
+    }
+
+    protected override void CreateSpecificOption(
+        IOptionInfo parentOps)
+    {
+        CreateFloatOption(
+            JesterOption.OutburstDistance,
+            1.0f, 0.0f, 2.0f, 0.1f,
+            parentOps);
+
+        CreateBoolOption(
+            JesterOption.UseSabotage,
+            true, parentOps);
+
+        this.CreateAbilityCountOption(
+            parentOps, 5, 100, 2.0f);
+    }
+
+    protected override void RoleSpecificInit()
+    {
+        this.UseSabotage = AllOptionHolder.Instance.GetValue<bool>(
+            GetRoleOptionId(JesterOption.UseSabotage));
+        this.outburstDistance = AllOptionHolder.Instance.GetValue<float>(
+            GetRoleOptionId(JesterOption.OutburstDistance));
+        this.RoleAbilityInit();
+    }
+
+    public void ResetOnMeetingStart()
+    {
+        return;
+    }
+
+    public void ResetOnMeetingEnd(GameData.PlayerInfo exiledPlayer = null)
+    {
+        return;
     }
 }
