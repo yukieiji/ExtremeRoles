0e15a674d8c831e3f231fbdfbae2d18fbfb59fd8
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-04-21 15:27:34 +0900

    change:key

--- ExtremeRoles/Patches/KeyboardJoystickPatch.cs
+++ ExtremeRoles/Patches/KeyboardJoystickPatch.cs
@@ -15,204 +15,203 @@ using ExtremeRoles.Roles.API.Extension.State;
 using ExtremeRoles.Performance;
 using ExtremeRoles.GameMode.RoleSelector;
 
-namespace ExtremeRoles.Patches
-{
+namespace ExtremeRoles.Patches;
+
 
 #if DEBUG
-    [HarmonyPatch(typeof(KeyboardJoystick), nameof(KeyboardJoystick.Update))]
-    public static class DebugTool
+[HarmonyPatch(typeof(KeyboardJoystick), nameof(KeyboardJoystick.Update))]
+public static class DebugTool
+{
+    private static List<PlayerControl> bots = new List<PlayerControl>();
+    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    public static void Postfix(KeyboardJoystick __instance)
     {
-        private static List<PlayerControl> bots = new List<PlayerControl>();
-        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        public static void Postfix(KeyboardJoystick __instance)
+        // ExtremeRolesPlugin.Logger.LogInfo($"DebugMode: {ExtremeRolesPlugin.DebugMode.Value}");
+
+        if (!ExtremeRolesPlugin.DebugMode.Value || 
+            AmongUsClient.Instance == null || 
+            PlayerControl.LocalPlayer == null) { return; }
+        if (!AmongUsClient.Instance.AmHost) { return; }
+
+        if (Input.GetKeyDown(KeyCode.F9))
         {
-            // ExtremeRolesPlugin.Logger.LogInfo($"DebugMode: {ExtremeRolesPlugin.DebugMode.Value}");
-
-            if (!ExtremeRolesPlugin.DebugMode.Value || 
-                AmongUsClient.Instance == null || 
-                PlayerControl.LocalPlayer == null) { return; }
-            if (!AmongUsClient.Instance.AmHost) { return; }
-
-            if (Input.GetKeyDown(KeyCode.F7))
+            foreach (var (key, value) in TranslationController.Instance.currentLanguage.AllStrings)
             {
-                foreach (var (key, value) in TranslationController.Instance.currentLanguage.AllStrings)
-                {
-                    Helper.Logging.Debug(
-                        $"TransKey:{key}    Value:{value}");
-                }
-            }
-
-            // Spawn dummys
-            if ((Input.GetKeyDown(KeyCode.F)) && GameSystem.IsLobby)
-            {
-                GameSystem.SpawnDummyPlayer();
-            }
-
-            // Terminate round
-            if (Input.GetKeyDown(KeyCode.F1) && !GameSystem.IsLobby)
-            {
-                GameSystem.ForceEndGame();
-            }
-
-            // See All roles
-            if (Input.GetKeyDown(KeyCode.K))
-            {
-                var dict = Roles.ExtremeRoleManager.GameRole;
-                if (dict.Count == 0) { return; }
-
-                foreach (KeyValuePair<byte, SingleRoleBase> value in dict)
-                {
-                    Logging.Debug(
-                        $"PlayerId:{value.Key}    AssignedTo:{value.Value.RoleName}   Team:{value.Value.Team}");
-                }
-            }
-
-            // See All task
-            if (Input.GetKeyDown(KeyCode.P))
-            {
-                var dict = Roles.ExtremeRoleManager.GameRole;
-                if (dict.Count == 0) { return; }
-                for (int i = 0; i < GameData.Instance.PlayerCount; i++)
-                {
-                    GameData.PlayerInfo playerInfo = GameData.Instance.AllPlayers[i];
-                    var role = dict[playerInfo.PlayerId];
-                    if (!role.HasTask())
-                    {
-                        continue;
-                    }
-                    var (playerCompleted, playerTotal) = GameSystem.GetTaskInfo(playerInfo);
-                    Logging.Debug($"PlayerName:{playerInfo.PlayerName}  TotalTask:{playerTotal}   ComplatedTask:{playerCompleted}");
-                }
-            }
-
-            // See Player TaskInfo
-            if (Input.GetKeyDown(KeyCode.I))
-            {
-                var dict = Roles.ExtremeRoleManager.GameRole;
-                if (dict.Count == 0) { return; }
-                for (int i = 0; i < GameData.Instance.PlayerCount; i++)
-                {
-                    GameData.PlayerInfo playerInfo = GameData.Instance.AllPlayers[i];
-                    var role = dict[playerInfo.PlayerId];
-                    if (!role.HasTask())
-                    {
-                        continue;
-                    }
-                    var (_, totalTask) = GameSystem.GetTaskInfo(playerInfo);
-                    if (totalTask == 0)
-                    {
-                        int taskId = GameSystem.GetRandomCommonTaskId();
-                        Logging.Debug($"PlayerName:{playerInfo.PlayerName}  AddTask:{taskId}");
-                        GameSystem.SetTask(
-                            playerInfo, taskId);
-                    }
-
-                }
-            }
-
-            if (Input.GetKeyDown(KeyCode.J))
-            {
-                var player = CachedPlayerControl.LocalPlayer;
-                GameSystem.CreateNoneReportableDeadbody(
-                    player, player.transform.position + new Vector3(0.75f, 0.75f));
+                Helper.Logging.Debug(
+                    $"TransKey:{key}    Value:{value}");
             }
         }
+
+        // Spawn dummys
+        if ((Input.GetKeyDown(KeyCode.F)) && GameSystem.IsLobby)
+        {
+            GameSystem.SpawnDummyPlayer();
+        }
+
+        // Terminate round
+        if (Input.GetKeyDown(KeyCode.F1) && !GameSystem.IsLobby)
+        {
+            GameSystem.ForceEndGame();
+        }
+
+        // See All roles
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            var dict = Roles.ExtremeRoleManager.GameRole;
+            if (dict.Count == 0) { return; }
+
+            foreach (KeyValuePair<byte, SingleRoleBase> value in dict)
+            {
+                Logging.Debug(
+                    $"PlayerId:{value.Key}    AssignedTo:{value.Value.RoleName}   Team:{value.Value.Team}");
+            }
+        }
+
+        // See All task
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            var dict = Roles.ExtremeRoleManager.GameRole;
+            if (dict.Count == 0) { return; }
+            for (int i = 0; i < GameData.Instance.PlayerCount; i++)
+            {
+                GameData.PlayerInfo playerInfo = GameData.Instance.AllPlayers[i];
+                var role = dict[playerInfo.PlayerId];
+                if (!role.HasTask())
+                {
+                    continue;
+                }
+                var (playerCompleted, playerTotal) = GameSystem.GetTaskInfo(playerInfo);
+                Logging.Debug($"PlayerName:{playerInfo.PlayerName}  TotalTask:{playerTotal}   ComplatedTask:{playerCompleted}");
+            }
+        }
+
+        // See Player TaskInfo
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            var dict = Roles.ExtremeRoleManager.GameRole;
+            if (dict.Count == 0) { return; }
+            for (int i = 0; i < GameData.Instance.PlayerCount; i++)
+            {
+                GameData.PlayerInfo playerInfo = GameData.Instance.AllPlayers[i];
+                var role = dict[playerInfo.PlayerId];
+                if (!role.HasTask())
+                {
+                    continue;
+                }
+                var (_, totalTask) = GameSystem.GetTaskInfo(playerInfo);
+                if (totalTask == 0)
+                {
+                    int taskId = GameSystem.GetRandomCommonTaskId();
+                    Logging.Debug($"PlayerName:{playerInfo.PlayerName}  AddTask:{taskId}");
+                    GameSystem.SetTask(
+                        playerInfo, taskId);
+                }
+
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            var player = CachedPlayerControl.LocalPlayer;
+            GameSystem.CreateNoneReportableDeadbody(
+                player, player.transform.position + new Vector3(0.75f, 0.75f));
+        }
     }
+}
 #endif
 
-    [HarmonyPatch(typeof(KeyboardJoystick), nameof(KeyboardJoystick.Update))]
-    public static class KeyboardJoystickPatch
+[HarmonyPatch(typeof(KeyboardJoystick), nameof(KeyboardJoystick.Update))]
+public static class KeyboardJoystickPatch
+{
+    public static void Postfix()
     {
-        public static void Postfix()
+        if (AmongUsClient.Instance == null || CachedPlayerControl.LocalPlayer == null) 
+        { return; }
+
+        if (ExtremeGameModeManager.Instance.RoleSelector.CanUseXion &&
+            OptionHolder.AllOption[(int)RoleGlobalOption.UseXion].GetValue() &&
+            !ExtremeRolesPlugin.DebugMode.Value)
         {
-            if (AmongUsClient.Instance == null || CachedPlayerControl.LocalPlayer == null) 
-            { return; }
+            Roles.Solo.Host.Xion.SpecialKeyShortCut();
+        }
 
-            if (ExtremeGameModeManager.Instance.RoleSelector.CanUseXion &&
-                OptionHolder.AllOption[(int)RoleGlobalOption.UseXion].GetValue() &&
-                !ExtremeRolesPlugin.DebugMode.Value)
+        if (GameSystem.IsLobby)
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
             {
-                Roles.Solo.Host.Xion.SpecialKeyShortCut();
+                OptionHolder.OptionsPage = OptionHolder.OptionsPage + 1;
             }
-
-            if (GameSystem.IsLobby)
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.I))
             {
-                if (Input.GetKeyDown(KeyCode.Tab))
+                ExtremeRolesPlugin.Info.ToggleInfoOverlay(
+                    Module.InfoOverlay.InfoOverlay.ShowType.AllRole);
+            }
+            if (Input.GetKeyDown(KeyCode.U))
+            {
+                ExtremeRolesPlugin.Info.ToggleInfoOverlay(
+                    Module.InfoOverlay.InfoOverlay.ShowType.AllGhostRole);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            ExtremeRolesPlugin.Info.ToggleInfoOverlay(
+                Module.InfoOverlay.InfoOverlay.ShowType.LocalPlayerRole);
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            ExtremeRolesPlugin.Info.ToggleInfoOverlay(
+                Module.InfoOverlay.InfoOverlay.ShowType.LocalPlayerGhostRole);
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            ExtremeRolesPlugin.Info.ToggleInfoOverlay(
+                Module.InfoOverlay.InfoOverlay.ShowType.VanilaOption);
+        }
+
+        if (Input.GetKeyDown(KeyCode.PageDown))
+        {
+            ExtremeRolesPlugin.Info.ChangePage(1);
+        }
+        if (Input.GetKeyDown(KeyCode.PageUp))
+        {
+            ExtremeRolesPlugin.Info.ChangePage(-1);
+        }
+
+       
+        // キルとベントボタン
+        if (CachedPlayerControl.LocalPlayer.Data == null ||
+            CachedPlayerControl.LocalPlayer.Data.Role == null ||
+            !RoleAssignState.Instance.IsRoleSetUpEnd) { return; }
+
+        var role = Roles.ExtremeRoleManager.GetLocalPlayerRole();
+
+        if (role.IsImpostor()) { return; }
+
+        var player = KeyboardJoystick.player;
+        var hudManager = FastDestroyableSingleton<HudManager>.Instance;
+
+        if (player.GetButtonDown(8) && role.CanKill())
+        {
+            hudManager.KillButton.DoClick();
+        }
+
+        if (player.GetButtonDown(50) && role.CanUseVent())
+        {
+            if (role.TryGetVanillaRoleId(out RoleTypes roleId))
+            {
+                if (roleId != RoleTypes.Engineer || 
+                    ExtremeGameModeManager.Instance.ShipOption.EngineerUseImpostorVent)
                 {
-                    OptionHolder.OptionsPage = OptionHolder.OptionsPage + 1;
+                    hudManager.ImpostorVentButton.DoClick();
                 }
             }
             else
             {
-                if (Input.GetKeyDown(KeyCode.I))
-                {
-                    ExtremeRolesPlugin.Info.ToggleInfoOverlay(
-                        Module.InfoOverlay.InfoOverlay.ShowType.AllRole);
-                }
-                if (Input.GetKeyDown(KeyCode.U))
-                {
-                    ExtremeRolesPlugin.Info.ToggleInfoOverlay(
-                        Module.InfoOverlay.InfoOverlay.ShowType.AllGhostRole);
-                }
-            }
-
-            if (Input.GetKeyDown(KeyCode.H))
-            {
-                ExtremeRolesPlugin.Info.ToggleInfoOverlay(
-                    Module.InfoOverlay.InfoOverlay.ShowType.LocalPlayerRole);
-            }
-            if (Input.GetKeyDown(KeyCode.G))
-            {
-                ExtremeRolesPlugin.Info.ToggleInfoOverlay(
-                    Module.InfoOverlay.InfoOverlay.ShowType.LocalPlayerGhostRole);
-            }
-            if (Input.GetKeyDown(KeyCode.O))
-            {
-                ExtremeRolesPlugin.Info.ToggleInfoOverlay(
-                    Module.InfoOverlay.InfoOverlay.ShowType.VanilaOption);
-            }
-
-            if (Input.GetKeyDown(KeyCode.PageDown))
-            {
-                ExtremeRolesPlugin.Info.ChangePage(1);
-            }
-            if (Input.GetKeyDown(KeyCode.PageUp))
-            {
-                ExtremeRolesPlugin.Info.ChangePage(-1);
-            }
-
-           
-            // キルとベントボタン
-            if (CachedPlayerControl.LocalPlayer.Data == null ||
-                CachedPlayerControl.LocalPlayer.Data.Role == null ||
-                !RoleAssignState.Instance.IsRoleSetUpEnd) { return; }
-
-            var role = Roles.ExtremeRoleManager.GetLocalPlayerRole();
-
-            if (role.IsImpostor()) { return; }
-
-            var player = KeyboardJoystick.player;
-            var hudManager = FastDestroyableSingleton<HudManager>.Instance;
-
-            if (player.GetButtonDown(8) && role.CanKill())
-            {
-                hudManager.KillButton.DoClick();
-            }
-
-            if (player.GetButtonDown(50) && role.CanUseVent())
-            {
-                if (role.TryGetVanillaRoleId(out RoleTypes roleId))
-                {
-                    if (roleId != RoleTypes.Engineer || 
-                        ExtremeGameModeManager.Instance.ShipOption.EngineerUseImpostorVent)
-                    {
-                        hudManager.ImpostorVentButton.DoClick();
-                    }
-                }
-                else
-                {
-                    hudManager.ImpostorVentButton.DoClick();
-                }
+                hudManager.ImpostorVentButton.DoClick();
             }
         }
     }
