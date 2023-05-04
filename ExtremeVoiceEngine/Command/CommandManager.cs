a86afee5dbb18ea61b8651d3548b8a3813c286f9
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-05-04 16:15:27 +0900

    feat:alias

--- ExtremeVoiceEngine/Command/CommandManager.cs
+++ ExtremeVoiceEngine/Command/CommandManager.cs
@@ -6,11 +6,31 @@ using ExtremeRoles.Performance;
 
 namespace ExtremeVoiceEngine.Command;
 
+public record class ParseActions(Parser Parser, Action<Result?> ParseAction)
+{
+    public void ParseAndAction(string[] args)
+    {
+        Result? result = null;
+        try
+        {
+            result = this.Parser.Parse(args);
+        }
+        catch
+        {
+            FastDestroyableSingleton<HudManager>.Instance.Chat.AddChat(
+                CachedPlayerControl.LocalPlayer, "Can't parse");
+        }
+        this.ParseAction.Invoke(result);
+    }
+}
+
 public sealed class CommandManager : NullableSingleton<CommandManager>
 {
-    private Dictionary<string, Parser> masterCmd = new Dictionary<string, Parser>();
-    private Dictionary<string, string> subCmdLink = new Dictionary<string, string>();
-    private Dictionary<string, Parser> subCmd = new Dictionary<string, Parser>();
+    private Dictionary<string, ParseActions> masterCmd = new Dictionary<string, ParseActions>();
+    private Dictionary<string, HashSet<string>> subCmdLink = new Dictionary<string, HashSet<string>>();
+    private Dictionary<string, ParseActions> subCmd = new Dictionary<string, ParseActions>();
+
+    private Dictionary<string, string> alias = new Dictionary<string, string>();
 
     private const char cmdChar = '/';
 
@@ -21,63 +41,74 @@ public sealed class CommandManager : NullableSingleton<CommandManager>
         this.subCmd.Clear();
     }
 
-    public Result? ParseCmd(string text)
+    public void AddAlias(string target, params string[] newAlias)
+    {
+        foreach (string alias in newAlias)
+        {
+            this.alias.Add(alias, target);
+        }
+    }
+
+    public void ExcuteCmd(string text)
     {
         if (!text.StartsWith(cmdChar) ||
-            !DestroyableSingleton<HudManager>.InstanceExists) { return null; }
+            !DestroyableSingleton<HudManager>.InstanceExists) { return; }
 
+        string cleanedText = text.Substring(1);
         HudManager hud = FastDestroyableSingleton<HudManager>.Instance;
         PlayerControl localPlayer = CachedPlayerControl.LocalPlayer;
-        string[] args = text.Split(' ');
+        string[] args = cleanedText.Split(' ');
 
-        string masterArgs = args[0];
-        if (!this.masterCmd.TryGetValue(masterArgs, out Parser? masterParser))
+        string masterArgs = cleanedArg(args[0]);
+        if (!this.masterCmd.TryGetValue(masterArgs, out ParseActions? masterParser))
         {
             hud.Chat.AddChat(localPlayer, "Can't Find cmd");
-            return null;
+            return;
         }
         if (args.Length == 1)
         {
-            hud.Chat.AddChat(localPlayer, masterParser.ToString(masterArgs));
-            return null;
+            hud.Chat.AddChat(localPlayer, masterParser.Parser.ToString(masterArgs));
+            return;
         }
-        string subCmd = args[1];
-        if (this.subCmdLink.TryGetValue(masterArgs, out string? hasSubCmd) &&
-            !string.IsNullOrEmpty(hasSubCmd) && hasSubCmd == subCmd)
+        string subCmd = cleanedArg(args[1]);
+        if (this.subCmdLink.TryGetValue(masterArgs, out HashSet<string>? subCmds) &&
+            subCmds is not null && subCmds.Contains(subCmd))
         {
 
-            Parser subCmdParser = this.subCmd[subCmd];
+            ParseActions subCmdParser = this.subCmd[subCmd];
             if (args.Length == 2)
             {
-                hud.Chat.AddChat(localPlayer, subCmdParser.ToString($"{masterArgs}{subCmd}"));
-                return null;
+                hud.Chat.AddChat(localPlayer, subCmdParser.Parser.ToString($"{masterArgs}{subCmd}"));
+                return;
             }
-            return subCmdParser.Parse(args[2..~0]);
+            subCmdParser.ParseAndAction(args[2..~0]);
         }
 
-        return masterParser.Parse(args[1..~0]);
+        masterParser.ParseAndAction(args[1..~0]);
     }
 
-    public void AddCommand(string cmd, Parser cmdParser)
+    public void AddCommand(string cmd, ParseActions cmdParser)
     {
-        if (!cmd.StartsWith(cmdChar))
-        {
-            throw new ArgumentException("cmmand start with '/'");
-        }
         this.masterCmd.Add(cmd, cmdParser);
     }
 
-    public void AddSubCommand(string masterCmd, string subCommand, Parser parser)
+    public void AddSubCommand(string masterCmd, string subCommand, ParseActions parser)
     {
-        if (!masterCmd.StartsWith(cmdChar))
-        {
-            throw new ArgumentException("cmmand start with '/'");
-        }
         if (!this.masterCmd.ContainsKey(masterCmd))
         {
             throw new ArgumentException("master cmmand can't find");
         }
-        this.subCmdLink.Add(masterCmd, subCommand);
-        this.subCmd.Add(subCommand, parser);
+        this.subCmdLink[masterCmd].Add(subCommand);
+        this.subCmd.Add($"{masterCmd} {subCommand}", parser);
+    }
+
+    private string cleanedArg(string arg)
+    {
+        arg = arg.ToLower();
+        if (this.alias.TryGetValue(arg, out string? newalias))
+        {
+            return newalias;
+        }
+        return arg;
     }
 }
