8279fc9d7fa83a1d931868d211db670b2561535e
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-05-05 17:14:04 +0900

    refactor: and update method

--- ExtremeVoiceEngine/Extension/TranslationControllerExtension.cs
+++ ExtremeVoiceEngine/Extension/TranslationControllerExtension.cs
@@ -8,18 +8,21 @@ using ExtremeRoles.Extension;
 
 namespace ExtremeVoiceEngine.Extension;
 
-public static class StringIdExtension
+public static class TranslationControllerExtension
 {
+    public static string GetString(string id, params Il2CppObject[] parts)
+        => TranslationController.Instance.GetString(id, defaultStr: string.Empty, parts: parts);
+
+    public static string GetString(string id)
+        => TranslationController.Instance.GetString(
+            id, defaultStr: string.Empty, parts: Array.Empty<Il2CppObject>());
+
     public static string GetString(this TranslationController cont, string id)
         => cont.GetString(id, string.Empty, Array.Empty<Il2CppObject>());
 
 #pragma warning disable CS8619
     public static unsafe string GetString(
         this TranslationController cont, string id, params Il2CppObject[] parts)
-    {
-        return cont.GetString(
-            id, defaultStr: string.Empty,
-            parts: parts);
-    }
+        => cont.GetString(id, defaultStr: string.Empty, parts: parts);
 #pragma warning restore CS8619
 }
