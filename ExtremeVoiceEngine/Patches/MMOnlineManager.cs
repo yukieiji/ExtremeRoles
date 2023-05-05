378e6c57d5da34e230b8826cc4d9a2f892f55b23
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-05-05 20:14:49 +0900

    feat:chated on login

---
+++ ExtremeVoiceEngine/Patches/MMOnlineManager.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+
+namespace ExtremeVoiceEngine.Patches;
+
+[HarmonyPatch(typeof(MMOnlineManager), nameof(MMOnlineManager.Start))]
+public static class MMOnlineManagerPatch
+{
+    public static void Postfix()
+    {
+        ChatCurrentSettingPatch.Chated = false;
+    }
+}
---
+++ ExtremeVoiceEngine/Patches/MainMenuManagerPatch.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+
+namespace ExtremeVoiceEngine.Patches;
+
+[HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
+public static class MainMenuManagerStartPatch
+{
+    public static void Postfix(MainMenuManager __instance)
+    {
+        ChatCurrentSettingPatch.Chated = false;
+    }
+}
