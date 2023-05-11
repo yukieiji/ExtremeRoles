bb626618a24534a9fa6d0f446a5b47c4fd302551
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-05-12 00:53:11 +0900

    feat:class template

---
+++ ExtremeRoles/Module/RoleAssign/RoleFilterSet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtremeRoles.Module.RoleAssign;
+
+public sealed class RoleFilterSet
+{
+}
