65fc4f11162ccc4db22abd1968a411ff551f2ba6
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-05-13 21:47:30 +0900

    feat:option names

--- ExtremeRoles/Roles/API/CombinationRoleManagerBase.cs
+++ ExtremeRoles/Roles/API/CombinationRoleManagerBase.cs
@@ -5,54 +5,53 @@ using AmongUs.GameOptions;
 
 using ExtremeRoles.Module;
 
-namespace ExtremeRoles.Roles.API
+namespace ExtremeRoles.Roles.API;
+
+public enum CombinationRoleCommonOption
 {
+    IsMultiAssign = 30,
+    AssignsNum,
+    IsAssignImposter,
+    ImposterSelectedRate,
+}
 
-    public enum CombinationRoleCommonOption
+public abstract class CombinationRoleManagerBase : RoleOptionBase
+{
+    public List<MultiAssignRoleBase> Roles = new List<MultiAssignRoleBase>();
+
+    protected Color OptionColor;
+    protected string RoleName = "";
+    internal CombinationRoleManagerBase(
+        string roleName,
+        Color optionColor)
     {
-        IsMultiAssign = 30,
-        AssignsNum,
-        IsAssignImposter,
-        ImposterSelectedRate,
+        this.OptionColor = optionColor;
+        this.RoleName = roleName;
     }
 
-    public abstract class CombinationRoleManagerBase : RoleOptionBase
+    public abstract string GetOptionName();
+    public abstract void AssignSetUpInit(int curImpNum);
+
+    public abstract MultiAssignRoleBase GetRole(
+        int roleId, RoleTypes playerRoleType);
+
+    protected override void CreateKillerOption(
+        IOption parentOps)
     {
-        public List<MultiAssignRoleBase> Roles = new List<MultiAssignRoleBase>();
-
-        protected Color OptionColor;
-        protected string RoleName = "";
-        internal CombinationRoleManagerBase(
-            string roleName,
-            Color optionColor)
-        {
-            this.OptionColor = optionColor;
-            this.RoleName = roleName;
-        }
-
-        public abstract void AssignSetUpInit(int curImpNum);
-
-        public abstract MultiAssignRoleBase GetRole(
-            int roleId, RoleTypes playerRoleType);
-
-        protected override void CreateKillerOption(
-            IOption parentOps)
-        {
-            // 複数ロールの中に殺戮者がいる可能性がため、管理ロールで殺戮者の設定はしない
-            return;
-        }
-
-        protected override void CreateVisionOption(
-            IOption parentOps)
-        {
-            // 複数のロールがまとまっているため、管理ロールで視界の設定はしない
-            return;
-        }
-        protected override void RoleSpecificInit()
-        {
-            // 複数のロールがまとまっているため、設定はしない
-            return;
-        }
-
+        // 複数ロールの中に殺戮者がいる可能性がため、管理ロールで殺戮者の設定はしない
+        return;
     }
+
+    protected override void CreateVisionOption(
+        IOption parentOps)
+    {
+        // 複数のロールがまとまっているため、管理ロールで視界の設定はしない
+        return;
+    }
+    protected override void RoleSpecificInit()
+    {
+        // 複数のロールがまとまっているため、設定はしない
+        return;
+    }
+
 }
