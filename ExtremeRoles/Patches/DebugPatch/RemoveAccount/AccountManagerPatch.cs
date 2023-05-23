8ace91900f53aae5dc30049de3a10041b8ffb395
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-05-23 21:16:48 +0900

    refactor:patch namespace

--- ExtremeRoles/Patches/DebugPatch/RemoveAccount/AccountManagerPatch.cs
+++ ExtremeRoles/Patches/DebugPatch/RemoveAccount/AccountManagerPatch.cs
@@ -3,7 +3,7 @@ using HarmonyLib;
 
 // From Reactor.RemoveAccounts by MIT License : https://github.com/NuclearPowered/Reactor.RemoveAccounts
 
-namespace ExtremeRoles.Patches.RemoveAccount
+namespace ExtremeRoles.Patches.DebugPatch.RemoveAccount
 {
     [HarmonyPatch(typeof(AccountManager), nameof(AccountManager.CanPlayOnline))]
     public static class AccountManagerCanPlayOnlinePatch
--- ExtremeRoles/Patches/DebugPatch/RemoveAccount/HttpMatchmakerManagerPatch.cs
+++ ExtremeRoles/Patches/DebugPatch/RemoveAccount/HttpMatchmakerManagerPatch.cs
@@ -3,7 +3,7 @@ using HarmonyLib;
 
 // From Reactor.RemoveAccounts by MIT License : https://github.com/NuclearPowered/Reactor.RemoveAccounts
 
-namespace ExtremeRoles.Patches.RemoveAccount
+namespace ExtremeRoles.Patches.DebugPatch.RemoveAccount
 {
     [HarmonyPatch(typeof(HttpMatchmakerManager), nameof(HttpMatchmakerManager.TryReadCachedToken))]
     public static class HttpMatchmakerManagerTryReadCachedTokenPatch
