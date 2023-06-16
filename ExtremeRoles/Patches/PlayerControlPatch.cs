5bb6dce0bbc88167d1cb4317c57885eb00c70ecf
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-06-16 19:47:23 +0900

    fix:pets bugs

--- ExtremeRoles/Patches/PlayerControlPatch.cs
+++ ExtremeRoles/Patches/PlayerControlPatch.cs
@@ -77,6 +77,22 @@ public static class PlayerControlAwakePatch
 #endif
     }
 }
+
+// 死人のペットが普通に見えるバグ修正
+[HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.Die))]
+public static class PlayerControlDiePatch
+{
+	public static void Postfix(
+		PlayerControl __instance)
+	{
+		if (__instance.Data.IsDead &&
+			__instance.cosmetics.CurrentPet != null)
+		{
+			__instance.cosmetics.CurrentPet.gameObject.layer = LayerMask.NameToLayer("Ghost");
+		}
+	}
+}
+
 [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.Exiled))]
 public static class PlayerControlExiledPatch
 {
@@ -1093,7 +1109,14 @@ public static class PlayerControlRevivePatch
 
         ExtremeRolesPlugin.ShipState.RemoveDeadInfo(__instance.PlayerId);
 
-        if (ExtremeRoleManager.GameRole.Count == 0) { return; }
+		// 外していたレイヤーマスクをもとに戻しておく
+		if (!__instance.Data.IsDead &&
+			__instance.cosmetics.CurrentPet != null)
+		{
+			__instance.cosmetics.CurrentPet.gameObject.layer = LayerMask.NameToLayer("Players");
+		}
+
+		if (ExtremeRoleManager.GameRole.Count == 0) { return; }
         if (!RoleAssignState.Instance.IsRoleSetUpEnd) { return; }
 
         var (onRevive, onReviveOther) = ExtremeRoleManager.GetInterfaceCastedRole<
