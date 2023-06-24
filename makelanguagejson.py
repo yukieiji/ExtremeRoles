af941cb07bf9cc721e475759cd57ffa87eb0d494
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-06-24 19:29:13 +0900

    feat:ExcelToCsvlogic

--- makelanguagejson.py
+++ makelanguagejson.py
@@ -4,6 +4,8 @@ from typing import Dict
 
 from pylightxl import readxl
 
+import pandas as pd
+
 WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
 
 EXTREMERORLS_IN_FILE = os.path.join(WORKING_DIR, "ExtremeRolesTransData.xlsx")
@@ -68,7 +70,50 @@ def xlsx_to_json(file_name : str, output_file : str) -> None:
     with open(output_file, "w") as f:
       json.dump(xlsx_data, f, indent=4)
 
+def xlsx_to_lang_csv(file_name : str, output_dir : str):
+
+  xlsx = pd.read_excel(file_name, sheet_name=None)
+
+  all_trans_data = {}
+
+  for df in xlsx.values():
+
+    key_data = None
+
+    for header in df.axes[1]:
+
+      if header == 'Unnamed: 0':
+        key_data = df[header]
+        key_data = key_data.dropna()
+        continue
+
+      if key_data is None:
+        continue
+
+      lang_data = df[header]
+
+      for index, value in enumerate(key_data):
+        data = lang_data[index]
+        if pd.isnull(data):
+          continue
+
+        if not (header in all_trans_data):
+          all_trans_data[header] = {}
+
+        all_trans_data[header][value] = data
+
+  for lang, data in all_trans_data.items():
+
+    file = os.path.join(output_dir, f'{lang}.csv')
+    if os.path.exists(file):
+      prev_df = pd.read_csv(file, header=None, index_col=0)
+      if data == prev_df.to_dict()[1]:
+        continue
+
+    output_df = pd.DataFrame(data.values(), index=data.keys())
+    output_df.to_csv(file, index=True, header=False, encoding='utf_8_sig')
 
 if __name__ == "__main__":
   xlsx_to_json(EXTREMERORLS_IN_FILE, EXTREMERORLS_OUT_FILE)
   xlsx_to_json(EXTREMESKIN_IN_FILE, EXTREMESKIN_OUT_FILE)
+  # xlsx_to_lang_csv(EXTREMESKIN_IN_FILE, os.path.join(WORKING_DIR, "ExtremeSkins", "Resources", "LangData"))
