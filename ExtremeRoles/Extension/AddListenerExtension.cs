7928943d101178a7de78093bdd13db4bed77efb3
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-07-11 23:29:47 +0900

    feat:extentions

---
+++ ExtremeRoles/Extension/AddListenerExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+namespace ExtremeRoles.Extension.UnityEvent;
+
+public static class AddListenerExtension
+{
+    public static void AddListener(this Button.ButtonClickedEvent events, Delegate delegateFunc)
+    {
+		events.AddListener((Action)delegateFunc);
+    }
+}
