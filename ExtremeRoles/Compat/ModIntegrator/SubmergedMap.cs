c5c2bd8b0ab842ed8f95e042e5c9ffd27bc5b00e
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-07-15 01:52:51 +0900

    replace:airship door logic

--- ExtremeRoles/Compat/ModIntegrator/SubmergedMap.cs
+++ ExtremeRoles/Compat/ModIntegrator/SubmergedMap.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
+using Il2CppInterop.Runtime;
+using HarmonyLib;
+
 using BepInEx;
 using UnityEngine;
-using Il2CppInterop.Runtime;
+using UnityEngine.AddressableAssets;
 using AmongUs.GameOptions;
-using HarmonyLib;
 
 using ExtremeRoles.Compat.Interface;
 using ExtremeRoles.Helper;
 using ExtremeRoles.Performance;
 using ExtremeRoles.Performance.Il2Cpp;
+
+
+using UnityObject = UnityEngine.Object;
 
 #nullable enable
 
@@ -73,6 +78,7 @@ public sealed class SubmergedIntegrator : ModIntegratorBase, IMultiFloorModMap
 	private Type? submarineStatusType;
 	private MethodInfo? calculateLightRadiusMethod;
 	private MonoBehaviour? submarineStatus;
+	private FieldInfo? submarineStatusReference;
 
 	private float crewVision;
 	private float impostorVision;
@@ -113,8 +119,9 @@ public sealed class SubmergedIntegrator : ModIntegratorBase, IMultiFloorModMap
 		this.submarineStatusType = ClassType.First(
 			t => t.Name == "SubmarineStatus");
 		this.calculateLightRadiusMethod = AccessTools.Method(
-			submarineStatusType, "CalculateLightRadius");
-
+			this.submarineStatusType, "CalculateLightRadius");
+		this.submarineStatusReference = AccessTools.Field(
+			this.submarineStatusType, "referenceHolder");
 
 		Type ventMoveToVentPatchType = ClassType.First(t => t.Name == "VentMoveToVentPatch");
 		this.inTransitionField = AccessTools.Field(ventMoveToVentPatchType, "inTransition");
@@ -125,11 +132,11 @@ public sealed class SubmergedIntegrator : ModIntegratorBase, IMultiFloorModMap
 	{
 		Patches.HudManagerUpdatePatchPostfixPatch.ButtonTriggerReset();
 
-		var component = map.GetComponent(Il2CppType.From(submarineStatusType));
+		var component = map.GetComponent(Il2CppType.From(this.submarineStatusType));
 		if (component)
 		{
-			submarineStatus = component.TryCast(
-				submarineStatusType) as MonoBehaviour;
+			this.submarineStatus = component.TryCast(
+				this.submarineStatusType) as MonoBehaviour;
 		}
 
 		// 毎回毎回取得すると重いのでキャッシュ化
@@ -172,6 +179,50 @@ public sealed class SubmergedIntegrator : ModIntegratorBase, IMultiFloorModMap
 			default:
 				break;
 		}
+
+		if (CachedShipStatus.Instance != null)
+		{
+			object? transformValue = this.submarineStatusReference?.GetValue(this.submarineStatus);
+			if (transformValue == null ||
+				transformValue is not Transform transform) { return; }
+
+			AssetReference airshipAsset = AmongUsClient.Instance.ShipPrefabs[4];
+
+			if (!airshipAsset.IsValid()) { return; }
+
+			ShipStatus ship = airshipAsset
+				.OperationHandle
+				.Result
+				.Cast<GameObject>()
+				.GetComponent<ShipStatus>();
+			Minigame? doorMinigame = ship.AllDoors
+				.Select(x =>
+				{
+					var door = x.gameObject.GetComponent<DoorConsole>();
+					if (door != null && door.MinigamePrefab != null)
+					{
+						return door.MinigamePrefab;
+					}
+					else
+					{
+						return null;
+					}
+				})
+				.FirstOrDefault(x => x != null);
+
+			if (doorMinigame == null) { return; }
+
+			foreach (var doorConsole in CachedShipStatus.Instance.GetComponentsInChildren<DoorConsole>())
+			{
+				if (doorConsole == null)
+				{
+					continue;
+				}
+
+				doorConsole.MinigamePrefab = UnityObject.Instantiate(
+					doorMinigame, transform);
+			}
+		}
 	}
 
 	public override void CreateIntegrateOption(Factory factory)
@@ -194,7 +245,7 @@ public sealed class SubmergedIntegrator : ModIntegratorBase, IMultiFloorModMap
 	public float CalculateLightRadius(GameData.PlayerInfo player, bool neutral, bool neutralImpostor)
 	{
 		object? value = calculateLightRadiusMethod?.Invoke(
-		submarineStatus, new object?[] { null, neutral, neutralImpostor });
+			this.submarineStatus, new object?[] { null, neutral, neutralImpostor });
 		return value != null ? (float)value : 1.0f;
 	}
 
