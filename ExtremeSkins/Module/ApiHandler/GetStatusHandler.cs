48cd0d4423f6d21d8b1dc848fc7ca3c591f904b7
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-08-17 17:29:43 +0900

    refactor:move exs.core.api

--- ExtremeSkins/Module/ApiHandler/GetStatusHandler.cs
+++ ExtremeSkins/Module/ApiHandler/GetStatusHandler.cs
@@ -4,53 +4,13 @@ using System.Net.Http;
 using System.Text.Json.Serialization;
 
 using ExtremeRoles.Module.Interface;
+using ExtremeSkins.Core.API;
 using ExtremeSkins.SkinManager;
 
 namespace ExtremeSkins.Module.ApiHandler;
 
 public sealed class GetStatusHandler : IRequestHandler
 {
-	private enum CurExSStatus
-	{
-		Booting,
-		OK,
-	}
-	private enum ModuleStatus
-	{
-		Arrive,
-		NotLoad,
-		NoData,
-	}
-
-	private readonly record struct ExSStatus(CurExSStatus RowStatus, ModuleStatusData Module)
-	{
-		[JsonIgnore(Condition = JsonIgnoreCondition.Always)]
-		public readonly CurExSStatus RowStatus = RowStatus;
-		public string Status => RowStatus.ToString();
-	}
-	private readonly record struct ModuleStatusData(
-		ModuleStatus ExCStatus,
-		ModuleStatus ExHStatus,
-		ModuleStatus ExVStatus,
-		ModuleStatus ExNStatus)
-	{
-		[JsonIgnore(Condition = JsonIgnoreCondition.Always)]
-		public readonly ModuleStatus ExCStatus = ExCStatus;
-		public string ExtremeColor => ExCStatus.ToString();
-
-		[JsonIgnore(Condition = JsonIgnoreCondition.Always)]
-		public readonly ModuleStatus ExHStatus = ExHStatus;
-		public string ExtremeHat => ExHStatus.ToString();
-
-		[JsonIgnore(Condition = JsonIgnoreCondition.Always)]
-		public readonly ModuleStatus ExVStatus = ExVStatus;
-		public string ExtremeVisor => ExVStatus.ToString();
-
-		[JsonIgnore(Condition = JsonIgnoreCondition.Always)]
-		public readonly ModuleStatus ExNStatus = ExNStatus;
-		public string ExtremeNamePlate => ExNStatus.ToString();
-	}
-
 	public Action<HttpListenerContext> Request => this.requestAction;
 
 	private void requestAction(HttpListenerContext context)
@@ -76,8 +36,8 @@ public sealed class GetStatusHandler : IRequestHandler
 		ModuleStatus exnStatus = ModuleStatus.NotLoad;
 #endif
 		var moduleState = new ModuleStatusData(excStatus, exhStatus, exvStatus, exnStatus);
-		var curState = new ExSStatus(
-			Patches.AmongUs.SplashManagerUpdatePatch.IsSkinLoad ? CurExSStatus.Booting : CurExSStatus.OK,
+		var curState = new StatusData(
+			Patches.AmongUs.SplashManagerUpdatePatch.IsSkinLoad ? ExSStatus.Booting : ExSStatus.OK,
 			moduleState);
 
 		IRequestHandler.Write(response, curState);
