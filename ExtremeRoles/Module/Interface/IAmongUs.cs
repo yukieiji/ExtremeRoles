8979b61b914eb08e10e2fc6fa80f93dcc084f32e
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-08-19 23:08:35 +0900

    feat:ISystemType

--- ExtremeRoles/Module/Interface/IAmongUs.cs
+++ ExtremeRoles/Module/Interface/IAmongUs.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hazel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,5 +23,20 @@ public static class IAmongUs
 		public float CanUse(GameData.PlayerInfo pc, out bool canUse, out bool couldUse);
 
 		public void Use();
+	}
+
+	public interface ISystemType
+	{
+		public bool IsDirty { get; }
+
+		public void Detoriorate(float deltaTime);
+
+		public void RepairDamage(PlayerControl player, byte amount);
+
+		public void UpdateSystem(PlayerControl player, MessageReader msgReader);
+
+		public void Serialize(MessageWriter writer, bool initialState);
+
+		public void Deserialize(MessageReader reader, bool initialState);
 	}
 }
