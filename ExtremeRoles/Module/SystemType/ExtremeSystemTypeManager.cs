2012ef3d27d12088589968498a0d5d5888f74806
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-09-06 11:55:30 +0900

    refactor:systems

--- ExtremeRoles/Module/SystemType/ExtremeSystemTypeManager.cs
+++ ExtremeRoles/Module/SystemType/ExtremeSystemTypeManager.cs
@@ -1,12 +1,9 @@
-﻿using ExtremeRoles.Module.Interface;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Hazel;
+
+using ExtremeRoles.Module.Interface;
 
 #nullable enable
 
