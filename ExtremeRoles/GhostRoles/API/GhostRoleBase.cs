597187145e8ad422fb8315b2d5c7aa93685751b7
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-09-09 03:16:54 +0900

    fix:trans name

--- ExtremeRoles/GhostRoles/API/GhostRoleBase.cs
+++ ExtremeRoles/GhostRoles/API/GhostRoleBase.cs
@@ -245,7 +245,7 @@ public abstract class GhostRoleBase
             RoleCommonOption.RoleNum,
             1, 1, spawnNum, 1);
 
-		factory.CreateIntOption(RoleCommonOption.AssignWeight, 500, 1, 1000, 1);
+		factory.CreateIntOption(RoleCommonOption.AssignWeight, 500, 1, 1000, 1, ignorePrefix: true);
 
 		return factory;
     }
