f6ddc546ea56e963e794a76c8f0707e49e26d919
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-09-09 01:59:57 +0900

    fix:compile error

--- ExtremeRoles/GameMode/Option/ShipGlobal/IShipGlobalOption.cs
+++ ExtremeRoles/GameMode/Option/ShipGlobal/IShipGlobalOption.cs
@@ -3,7 +3,7 @@ using System.Text;
 
 using ExtremeRoles.GameMode.Option.MapModule;
 
-using static ExtremeRoles.Module.CustomOption.Factory;
+using static ExtremeRoles.Module.CustomOption.Factorys.SimpleFactory;
 
 namespace ExtremeRoles.GameMode.Option.ShipGlobal;
 
