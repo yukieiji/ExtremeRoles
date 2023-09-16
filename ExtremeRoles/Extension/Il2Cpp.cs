b07a89cefb98266d8f3cd22a0964f00f783a3319
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-09-16 23:22:59 +0900

    feat:null able

--- ExtremeRoles/Extension/Il2Cpp.cs
+++ ExtremeRoles/Extension/Il2Cpp.cs
@@ -3,17 +3,21 @@ using HarmonyLib;
 
 using Il2CppInterop.Runtime.InteropTypes;
 
+#nullable enable
+
 namespace ExtremeRoles.Extension;
 
 public static class Il2Cpp
 {
-	public static object TryCast(this Il2CppObjectBase self, Type type)
+	public static object? TryCast(this Il2CppObjectBase self, Type type)
 	{
 		return AccessTools.Method(
 			self.GetType(),
-			nameof(Il2CppObjectBase.TryCast)).MakeGenericMethod(type).Invoke(self, Array.Empty<object>());
+			nameof(Il2CppObjectBase.TryCast))
+			.MakeGenericMethod(type)
+			.Invoke(self, Array.Empty<object>());
 	}
-	public static bool IsTryCast<T>(this Il2CppObjectBase self, out T obj) where T : Il2CppObjectBase
+	public static bool IsTryCast<T>(this Il2CppObjectBase self, out T? obj) where T : Il2CppObjectBase
 	{
 		obj = self.TryCast<T>();
 		return obj != null;
