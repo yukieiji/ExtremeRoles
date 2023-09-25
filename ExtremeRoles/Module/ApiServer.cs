a0959b949e4eb4db05ae028ff013ac240c51eaac
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-09-25 20:29:49 +0900

    feat:exception catch

--- ExtremeRoles/Module/ApiServer.cs
+++ ExtremeRoles/Module/ApiServer.cs
@@ -72,8 +72,15 @@ public class ApiServer : IDisposable
 			ExtremeRolesPlugin.Logger.LogError($"ExR ApiServer: Can't boot. port:{port} used already!!");
 			return;
 		}
-
+		try
+		{
 		instance = new ApiServer();
+	}
+		catch (Exception ex)
+		{
+			ExtremeRolesPlugin.Logger.LogError($"ExR ApiServer: Can't boot: {ex}");
+			instance = null;
+		}
 	}
 
 	public static void Register(string url, HttpMethod method, IRequestHandler handle)
