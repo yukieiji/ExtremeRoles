cd54d042a14204329974baf004f5bb16358e610c
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-10-26 18:11:36 +0900

    fix:fungle security block logic

---
+++ ExtremeRoles/Patches/MiniGame/FungleSurveillanceMinigamePatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using ExtremeRoles.Roles.API.Extension.State;
+
+namespace ExtremeRoles.Patches.MiniGame;
+
+[HarmonyPatch(typeof(FungleSurveillanceMinigame), nameof(FungleSurveillanceMinigame.Update))]
+public static class FungleSurveillanceMinigameePatch
+{
+    public static void Prefix(FungleSurveillanceMinigame __instance)
+    {
+        if (Roles.ExtremeRoleManager.GameRole.Count == 0) { return; }
+
+		bool enable =
+			Roles.ExtremeRoleManager.GetLocalPlayerRole().CanUseSecurity() ||
+			SecurityHelper.IsAbilityUse();
+
+		__instance.viewport.enabled = enable;
+    }
+    public static void Postfix(FungleSurveillanceMinigame __instance)
+    {
+        SecurityHelper.PostUpdate(__instance);
+    }
+}
