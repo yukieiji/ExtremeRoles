04768f93f6f8ac52c88f0a7c16516262ee817548
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-11-07 00:05:39 +0900

    fix:conditions

--- ExtremeRoles/Patches/MapModule/MushroomPatch.cs
+++ ExtremeRoles/Patches/MapModule/MushroomPatch.cs
@@ -12,9 +12,12 @@ public static class MushroomStartSporeTriggerPatch
 	{
 		string name = __instance.name;
 
-		if (name.StartsWith(ModedMushroomSystem.MushroomName)) { return true; }
+		Helper.Logging.Debug(name);
+
+		if (!name.StartsWith(ModedMushroomSystem.MushroomName)) { return true; }
 
 		string idStr = name.Split('_')[^1];
+		Helper.Logging.Debug(idStr);
 		if (!ExtremeSystemTypeManager.Instance.ExistSystem(ModedMushroomSystem.Type) ||
 			!int.TryParse(idStr, out int id))
 		{
