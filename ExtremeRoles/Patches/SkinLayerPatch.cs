ccd1304478f5612ead7a1a39ffd5a5e3d70a924f
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-11-09 23:01:17 +0900

    fix:skin layer null error

---
+++ ExtremeRoles/Patches/SkinLayerPatch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace ExtremeRoles.Patches;
+
+[HarmonyPatch(typeof(SkinLayer), nameof(SkinLayer.IsPlayingRunAnim))]
+public static class SkinLayerIsPlayingRunAnimPatch
+{
+	public static bool Prefix(SkinLayer __instance, ref bool __result)
+	{
+		__result = false;
+		return __instance.skin != null && __instance.animator != null;
+	}
+}
