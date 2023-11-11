1bf1130fd6ebca01c5dd76f5f1608ce20c98097e
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-11-12 00:33:59 +0900

    feat:select ops

--- ExtremeRoles/Module/CustomMonoBehaviour/Minigames/ExtremeSpawnSelectorMinigame.cs
+++ ExtremeRoles/Module/CustomMonoBehaviour/Minigames/ExtremeSpawnSelectorMinigame.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections;
-using System.Linq;
-using System.Text;
+using System.Collections.Generic;
 
 using TMPro;
 using UnityEngine;
+using Newtonsoft.Json.Linq;
 
 using BepInEx.Unity.IL2CPP.Utils.Collections;
 
 using ExtremeRoles.Helper;
 using ExtremeRoles.Performance;
+using ExtremeRoles.Performance.Il2Cpp;
 using ExtremeRoles.Extension.Task;
+using ExtremeRoles.Module.CustomMonoBehaviour.UIPart;
 
 using Il2CppObject = Il2CppSystem.Object;
 
@@ -21,13 +23,12 @@ namespace ExtremeRoles.Module.CustomMonoBehaviour.Minigames;
 [Il2CppRegister]
 public sealed class ExtremeSpawnSelectorMinigame : Minigame
 {
-	private bool gotButton;
+	private bool selected;
+	private readonly Controller controller = new Controller();
+	private readonly List<SpriteButton> button = new List<SpriteButton>(3);
 
-	private readonly record struct SpawnSelector(
-		GameObject Obj,
-		SpriteRenderer Renderer,
-		UiElement Element);
-
+	private const float buttonYOffset = 0.25f;
+	private static JObject? json;
 
 #pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
 	private TextMeshPro text;
@@ -35,26 +36,51 @@ public sealed class ExtremeSpawnSelectorMinigame : Minigame
 #pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
 	{ }
 
-	public void Awake()
-	{
-		text = Instantiate(Prefab.Text, base.transform);
-		text.alignment = TextAlignmentOptions.Center;
-		text.gameObject.SetActive(true);
-		text.transform.localPosition = new Vector3(0.0f, -1.0f);
-		text.fontSize = text.fontSizeMin = text.fontSizeMax = 4.0f;
-
-		this.MyTask = null;
-		this.multistageMinigameChecked = true;
-		this.TransType = TransitionType.SlideBottom;
-	}
-
 	public override void Begin(PlayerTask? task)
 	{
+		string mapKey = GameSystem.CurMapKey;
+		if (mapKey == GameSystem.SubmergedKey ||
+			mapKey == GameSystem.AirShipKey)
+		{
+			return;
+		}
+
 		this.AbstractBegin(task);
 
-		base.StartCoroutine(runTimer().WrapToIl2Cpp());
+		for (int i = -1; i <= 1; ++i)
+		{
+			GameObject obj = new GameObject("selector");
+			obj.transform.SetParent(base.transform);
+			obj.transform.localPosition = new Vector3(2.5f * i, buttonYOffset);
+			obj.SetActive(true);
+			obj.layer = 5;
 
-		// ControllerManager.Instance.OpenOverlayMenu(base.name, null, this.DefaultButtonSelected, this.ControllerSelectable, false);
+			var button = obj.AddComponent<SpriteButton>();
+			button.Text.text = "Test";
+			button.Rend.sprite = Resources.Loader.CreateSpriteFromResources(
+				Resources.Path.MoverMove);
+			button.Colider.size = new Vector2(1.25f, 1.25f);
+
+			this.button.Add(button);
+		}
+
+		if (GameManager.Instance != null && GameManager.Instance.IsNormal())
+		{
+			foreach (GameData.PlayerInfo playerInfo in GameData.Instance.AllPlayers.GetFastEnumerator())
+			{
+				if (playerInfo != null &&
+					playerInfo.Object != null &&
+					!playerInfo.Disconnected)
+				{
+					var player = playerInfo.Object.NetTransform;
+
+					player.transform.position = new Vector2(-25f, 40f);
+					player.Halt();
+				}
+			}
+		}
+
+		base.StartCoroutine(runTimer().WrapToIl2Cpp());
 
 		PlayerControl.HideCursorTemporarily();
 		ConsoleJoystick.SetMode_Menu();
@@ -62,12 +88,45 @@ public sealed class ExtremeSpawnSelectorMinigame : Minigame
 
 	public override void Close()
 	{
-		// ControllerManager.Instance.CloseOverlayMenu(base.name);
-		if (!gotButton)
+		if (!this.selected)
 		{
-
+			spawnToRandom();
 		}
 		this.AbstractClose();
+	}
+
+	public void Awake()
+	{
+		this.text = Instantiate(Prefab.Text, base.transform);
+		this.text.alignment = TextAlignmentOptions.Center;
+		this.text.gameObject.SetActive(true);
+		this.text.transform.localPosition = new Vector3(0.0f, -1.5f);
+		this.text.fontSize = text.fontSizeMin = text.fontSizeMax = 4.0f;
+
+		this.MyTask = null;
+		this.multistageMinigameChecked = true;
+		this.TransType = TransitionType.SlideBottom;
+	}
+
+	public void Update()
+	{
+		if (this.selected ||
+			FastDestroyableSingleton<HudManager>.Instance == null ||
+			FastDestroyableSingleton<HudManager>.Instance.Chat.IsOpenOrOpening)
+		{
+			return;
+		}
+
+		this.controller.Update();
+
+		foreach (var button in this.button)
+		{
+			if (this.controller.CheckHover(button.Colider))
+			{
+				button.OnClick?.Invoke();
+				return;
+			}
+		}
 	}
 
 	public IEnumerator WaitForFinish()
@@ -85,32 +144,39 @@ public sealed class ExtremeSpawnSelectorMinigame : Minigame
 	{
 		for (float time = 128f; time >= 0f; time -= Time.deltaTime)
 		{
-			text.text = FastDestroyableSingleton<TranslationController>.Instance.GetString(
+			this.text.text = FastDestroyableSingleton<TranslationController>.Instance.GetString(
 				StringNames.TimeRemaining, new Il2CppObject[] { Mathf.CeilToInt(time) });
 			yield return null;
 		}
+		spawnToRandom();
 		yield break;
 	}
 
-	private void spawnAt(SpawnInMinigame.SpawnLocation spawnPoint)
+	private Action createSpawnAtAction(Vector2 pos, string name)
 	{
-		if (this.amClosing != Minigame.CloseState.None)
+		return () =>
 		{
-			return;
-		}
-		Logger.GlobalInstance.Info(string.Format("Player selected spawn point {0}", spawnPoint.Name), null);
-		gotButton = true;
-		PlayerControl.LocalPlayer.SetKinematic(true);
-		PlayerControl.LocalPlayer.NetTransform.SetPaused(true);
-		PlayerControl.LocalPlayer.NetTransform.RpcSnapTo(spawnPoint.Location);
-		DestroyableSingleton<HudManager>.Instance.PlayerCam.SnapToTarget();
+			if (this.amClosing != CloseState.None)
+			{
+				return;
+			}
+			ExtremeRolesPlugin.Logger.LogInfo($"Player selected spawn point {name}");
 
-		base.StopAllCoroutines();
-		base.StartCoroutine(
-			coSpawnAt(CachedPlayerControl.LocalPlayer, spawnPoint).WrapToIl2Cpp());
+			this.selected = true;
+			PlayerControl localPlayer = CachedPlayerControl.LocalPlayer;
+
+			localPlayer.SetKinematic(true);
+			localPlayer.NetTransform.SetPaused(true);
+			Player.RpcUncheckSnap(localPlayer.PlayerId, pos);
+
+			FastDestroyableSingleton<HudManager>.Instance.PlayerCam.SnapToTarget();
+
+			base.StopAllCoroutines();
+			base.StartCoroutine(coSpawnAt(localPlayer).WrapToIl2Cpp());
+		};
 	}
 
-	private IEnumerator coSpawnAt(PlayerControl playerControl, SpawnInMinigame.SpawnLocation spawnLocation)
+	private IEnumerator coSpawnAt(PlayerControl playerControl)
 	{
 		yield return new WaitForFixedUpdate();
 		yield return new WaitForFixedUpdate();
@@ -118,19 +184,14 @@ public sealed class ExtremeSpawnSelectorMinigame : Minigame
 
 		playerControl.SetKinematic(false);
 		playerControl.NetTransform.SetPaused(false);
-		Close();
+		this.Close();
 
 		yield break;
 	}
-	/*
-	private SpawnSelector createButton(string name)
-	{
-		var obj = new GameObject(name);
-		obj.transform.SetParent(base.transform);
 
-		var renderer =　obj.AddComponent<SpriteRenderer>();
-		renderer.sprite = Resources.Loader.CreateSpriteFromResources(
-			Resources.Path.MoverMove);
+	private void spawnToRandom()
+	{
+		int index = RandomGenerator.Instance.Next(0, this.button.Count);
+		this.button[index].OnClick?.Invoke();
 	}
-	*/
 }
---
+++ ExtremeRoles/Module/CustomMonoBehaviour/UIPart/SpriteButton.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using TMPro;
+using UnityEngine;
+
+namespace ExtremeRoles.Module.CustomMonoBehaviour.UIPart;
+
+#nullable enable
+
+[Il2CppRegister]
+public sealed class SpriteButton : MonoBehaviour
+{
+	public Action? OnClick { get; set; }
+
+
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+	public TextMeshPro Text { get; private set; }
+	public SpriteRenderer Rend { get; private set; }
+	public BoxCollider2D Colider { get; private set; }
+
+	public SpriteButton(IntPtr ptr) : base(ptr) {  }
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+
+	public void Awake()
+	{
+		this.Rend = base.gameObject.AddComponent<SpriteRenderer>();
+		this.Colider = base.gameObject.AddComponent<BoxCollider2D>();
+
+		this.Text = Instantiate(Prefab.Text, base.transform);
+		this.Text.alignment = TextAlignmentOptions.Center;
+		this.Text.gameObject.SetActive(true);
+		this.Text.transform.localPosition = new Vector3(0.0f, -1.0f);
+		this.Text.fontSize = this.Text.fontSizeMin = this.Text.fontSizeMax = 3.0f;
+	}
+
+	public void OnMouseDown()
+	{
+		this.OnClick?.Invoke();
+	}
+
+	public void OnMouseEnter()
+	{
+		this.Rend.color = Color.green;
+		this.Text.color = Color.green;
+	}
+	public void OnMouseExit()
+	{
+		this.Rend.color = Color.white;
+		this.Text.color = Color.white;
+	}
+}
