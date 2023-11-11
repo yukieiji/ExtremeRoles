64462211d60181b25598d96523f4170ae351a18b
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-11-12 06:24:39 +0900

    feat:other exiled controller patch

---
+++ ExtremeRoles/Patches/Controller/PbExileControllerPatch.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+using BepInEx.Unity.IL2CPP.Utils.Collections;
+
+using UnityEngine;
+
+using HarmonyLib;
+using ExtremeRoles.Module.CustomMonoBehaviour.Minigames;
+using ExtremeRoles.Performance;
+
+using Il2CppEnumerator = Il2CppSystem.Collections.IEnumerator;
+
+namespace ExtremeRoles.Patches.Controller;
+
+[HarmonyPatch(typeof(PbExileController), nameof(PbExileController.Animate))]
+public static class PbExileControllerAnimePatch
+{
+	public static bool Prefix(PbExileController __instance, ref Il2CppEnumerator __result)
+	{
+		__result = animateWithRandomSpawn(__instance).WrapToIl2Cpp();
+		return false;
+	}
+
+	private static IEnumerator animateWithRandomSpawn(PbExileController __instance)
+	{
+		yield return Effects.Wait(0.75f);
+		yield return Effects.All(new Il2CppEnumerator[]
+		{
+			__instance.PlayerFall(),
+			__instance.PlayerSpin(),
+			__instance.HandleText(__instance.Duration * 0.5f, __instance.Duration * 0.5f)
+		});
+
+		if (GameManager.Instance.LogicOptions.GetConfirmImpostor())
+		{
+			__instance.ImpostorText.gameObject.SetActive(true);
+		}
+
+		yield return Effects.Bloop(0f, __instance.ImpostorText.transform, 1f, 0.5f);
+		yield return new WaitForSeconds(0.5f);
+		yield return FastDestroyableSingleton<HudManager>.Instance.CoFadeFullScreen(Color.clear, Color.black, 0.2f, false);
+
+		yield return ExtremeSpawnSelectorMinigame.WrapUpAndSpawn(__instance);
+
+		yield break;
+	}
+}
