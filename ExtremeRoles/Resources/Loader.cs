bff3a3d7858d23b9f7c093fa8dacbc9095792d0b
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-12-17 23:04:49 +0900

    feat:crewshroom obj

--- ExtremeRoles/Resources/Loader.cs
+++ ExtremeRoles/Resources/Loader.cs
@@ -108,6 +108,7 @@ public static class Path
 	public const string TheifTimeParts = "ExtremeRoles.Resources.TheifTimePart.png";
 	public const string TheifMagicCircle = "ExtremeRoles.Resources.TheifMagicCircle.png";
 	public const string TheifMagicCircleVideo = "theifmagiccircle";
+	public const string CrewshroomSet = "ExtremeRoles.Resources.CrewshroomSetMushroom.png";
 	public const string TeroristTeroMinigameAsset = "ExtremeRoles.Resources.Asset.teroristminigame.asset";
 	public const string TeroristTeroMinigamePrefab = "assets/roles/teroristminigame.prefab";
 	public const string TeroristTeroSabotageBomb = "ExtremeRoles.Resources.TeroristoBomb.png";
