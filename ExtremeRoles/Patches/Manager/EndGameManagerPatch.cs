037ee2f8103039d16cb408c5dd3bef49ab74579f
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-01-26 00:14:55 +0900

    fix:hatter win color

--- ExtremeRoles/Patches/Manager/EndGameManagerPatch.cs
+++ ExtremeRoles/Patches/Manager/EndGameManagerPatch.cs
@@ -353,7 +353,7 @@ public static class EndGameManagerSetUpPatch
 
 			RoleGameOverReason.HatterEndlessTeaTime or
 			RoleGameOverReason.HatterTeaPartyTime =>
-				WinTextInfo.Create(ExtremeRoleId.Hatter, ColorPalette.AliceGold),
+				WinTextInfo.Create(ExtremeRoleId.Hatter, ColorPalette.HatterYanagizome),
 
             _ => WinTextInfo.Create(RoleGameOverReason.UnKnown, Color.black)
         };
