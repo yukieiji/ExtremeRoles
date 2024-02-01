83c67d6d5ae485a00040216d072296ebd1e8ed43
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-02-01 20:40:33 +0900

    fix:airship vent size

--- ExtremeRoles/Roles/Solo/Impostor/Mery.cs
+++ ExtremeRoles/Roles/Solo/Impostor/Mery.cs
@@ -141,7 +141,14 @@ public sealed class Mery : SingleRoleBase, IRoleAbility
                 string.Format(Path.MeryCustomVentAnime, "0"), 125f);
 
 			vent.myRend = ventRenderer;
-			vent.myRend.transform.localPosition = Vector3.zero;
+
+			var transform = vent.myRend.transform;
+			if (transform.localPosition != vent.transform.localPosition)
+			{
+				transform.localPosition = Vector3.zero;
+			}
+
+			vent.transform.localScale = Vector3.one;
 			vent.name = "MaryVent_" + vent.Id;
             vent.gameObject.SetActive(this.body.active);
 
