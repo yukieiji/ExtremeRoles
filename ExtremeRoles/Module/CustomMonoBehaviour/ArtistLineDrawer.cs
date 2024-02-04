a60ecfe761f6cf474da83facf5140ece0caa2c21
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-02-04 10:10:25 +0900

    refactor

--- ExtremeRoles/Module/CustomMonoBehaviour/ArtistLineDrawer.cs
+++ ExtremeRoles/Module/CustomMonoBehaviour/ArtistLineDrawer.cs
@@ -1,9 +1,5 @@
 ﻿using ExtremeRoles.Performance;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using UnityEngine;
 
 #nullable enable
