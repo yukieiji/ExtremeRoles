ed5104f43905c6994b0a93972e3098676ec57fc6
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-02-13 21:56:15 +0900

    refactor and fix: can't create opsions

--- ExtremeRoles/Roles/API/Interface/IRoleAbility.cs
+++ ExtremeRoles/Roles/API/Interface/IRoleAbility.cs
@@ -68,6 +68,84 @@ public interface IRoleAbility : IRoleResetMeeting
 	}
 }
 
+public static class IRoleAbilityMixin
+{
+	private const float defaultCoolTime = 30.0f;
+	private const float minCoolTime = 0.5f;
+	private const float maxCoolTime = 120.0f;
+	private const float minActiveTime = 0.5f;
+	private const float maxActiveTime = 60.0f;
+	private const float step = 0.5f;
+
+
+	public static void CreateCommonAbilityOption(
+		this IRoleAbility self,
+		IOptionInfo parentOps,
+		float defaultActiveTime = float.MaxValue)
+	{
+
+		SingleRoleBase role = (SingleRoleBase)self;
+
+		new FloatCustomOption(
+			self.GetRoleOptionId(RoleAbilityCommonOption.AbilityCoolTime),
+			string.Concat(
+				role.RoleName,
+				RoleAbilityCommonOption.AbilityCoolTime.ToString()),
+			defaultCoolTime, minCoolTime, maxCoolTime, step,
+			parentOps, format: OptionUnit.Second,
+			tab: role.Tab);
+
+		if (defaultActiveTime != float.MaxValue)
+		{
+			defaultActiveTime = Mathf.Clamp(
+				defaultActiveTime, minActiveTime, maxActiveTime);
+
+			new FloatCustomOption(
+				self.GetRoleOptionId(RoleAbilityCommonOption.AbilityActiveTime),
+				string.Concat(
+					role.RoleName,
+					RoleAbilityCommonOption.AbilityActiveTime.ToString()),
+				defaultActiveTime, minActiveTime, maxActiveTime, step,
+				parentOps, format: OptionUnit.Second,
+				tab: role.Tab);
+		}
+
+	}
+
+	public static void CreateAbilityCountOption(
+		this IRoleAbility self,
+		IOptionInfo parentOps,
+		int defaultAbilityCount,
+		int maxAbilityCount,
+		float defaultActiveTime = float.MaxValue,
+		int minAbilityCount = 1)
+	{
+
+		SingleRoleBase role = (SingleRoleBase)self;
+
+		self.CreateCommonAbilityOption(
+			parentOps,
+			defaultActiveTime);
+
+		new IntCustomOption(
+			self.GetRoleOptionId(RoleAbilityCommonOption.AbilityCount),
+			string.Concat(
+				role.RoleName,
+				RoleAbilityCommonOption.AbilityCount.ToString()),
+			defaultAbilityCount, minAbilityCount,
+			maxAbilityCount, 1,
+			parentOps, format: OptionUnit.Shot,
+			tab: role.Tab);
+
+	}
+
+	public static int GetRoleOptionId(
+		this IRoleAbility self,
+		RoleAbilityCommonOption option) => ((RoleOptionBase)self).GetRoleOptionId((int)option);
+}
+
+
+
 public interface IRoleAutoBuildAbility : IRoleAbility
 {
     public bool UseAbility();
@@ -75,214 +153,141 @@ public interface IRoleAutoBuildAbility : IRoleAbility
     public bool IsAbilityUse();
 }
 
-public static class IRoleAbilityMixin
+public static class IRoleAutoBuildAbilityMixin
 {
-    private const float defaultCoolTime = 30.0f;
-    private const float minCoolTime = 0.5f;
-    private const float maxCoolTime = 120.0f;
-    private const float minActiveTime = 0.5f;
-    private const float maxActiveTime = 60.0f;
-    private const float step = 0.5f;
-
-    public static void CreateNormalAbilityButton(
-        this IRoleAutoBuildAbility self,
-        string textKey,
-        Sprite sprite,
-        Func<bool> checkAbility = null,
-        Action abilityOff = null,
-        Action forceAbilityOff = null,
-        KeyCode hotkey = KeyCode.F)
-    {
-
-        self.Button = RoleAbilityFactory.CreateReusableAbility(
-            textKey: textKey,
-            img: sprite,
-            canUse: self.IsAbilityUse,
-            ability: self.UseAbility,
-            canActivating: checkAbility,
-            abilityOff: abilityOff,
-            forceAbilityOff: forceAbilityOff,
-            hotKey: hotkey);
-
-        self.RoleAbilityInit();
-    }
-
-    public static void CreateAbilityCountButton(
-        this IRoleAutoBuildAbility self,
-        string textKey,
-        Sprite sprite,
-        Func<bool> checkAbility = null,
-        Action abilityOff = null,
-        Action forceAbilityOff = null,
-        bool isReduceOnActive = false,
-        KeyCode hotkey = KeyCode.F)
-    {
-        self.Button = RoleAbilityFactory.CreateCountAbility(
-            textKey: textKey,
-            img: sprite,
-            canUse: self.IsAbilityUse,
-            ability: self.UseAbility,
-            canActivating: checkAbility,
-            abilityOff: abilityOff,
-            forceAbilityOff: forceAbilityOff,
-            isReduceOnActive: isReduceOnActive,
-            hotKey: hotkey);
-
-        self.RoleAbilityInit();
-
-    }
-
-
-    public static void CreateReclickableAbilityButton(
-        this IRoleAutoBuildAbility self,
-        string textKey,
-        Sprite sprite,
-        Func<bool> checkAbility = null,
-        Action abilityOff = null,
-        KeyCode hotkey = KeyCode.F)
-    {
-        self.Button = RoleAbilityFactory.CreateReclickAbility(
-            textKey: textKey,
-            img: sprite,
-            canUse: self.IsAbilityUse,
-            ability: self.UseAbility,
-            canActivating: checkAbility,
-            abilityOff: abilityOff,
-            hotKey: hotkey);
-
-        self.RoleAbilityInit();
-    }
-
-    public static void CreateReclickableCountAbilityButton(
-        this IRoleAutoBuildAbility self,
-        string textKey,
-        Sprite sprite,
-        Func<bool> checkAbility = null,
-        Action abilityOff = null,
-        KeyCode hotkey = KeyCode.F)
-    {
-        self.Button = RoleAbilityFactory.CreateReclickCountAbility(
-            textKey: textKey,
-            img: sprite,
-            canUse: self.IsAbilityUse,
-            ability: self.UseAbility,
-            canActivating: checkAbility,
-            abilityOff: abilityOff,
-            hotKey: hotkey);
-
-        self.RoleAbilityInit();
-    }
-
-    public static void CreateChargeAbilityButton(
-        this IRoleAutoBuildAbility self,
-        string textKey,
-        Sprite sprite,
-        Func<bool> checkAbility = null,
-        Action abilityOff = null,
-        Action forceAbilityOff = null,
-        KeyCode hotkey = KeyCode.F)
-    {
-
-        self.Button = RoleAbilityFactory.CreateChargableAbility(
-            textKey: textKey,
-            img: sprite,
-            canUse: self.IsAbilityUse,
-            ability: self.UseAbility,
-            canActivating: checkAbility,
-            abilityOff: abilityOff,
-            forceAbilityOff: forceAbilityOff,
-            hotKey: hotkey);
-
-        self.RoleAbilityInit();
-    }
-
-    public static void CreatePassiveAbilityButton(
-        this IRoleAutoBuildAbility self,
-        string activateButtonName,
-        string deactivateButtonName,
-        Sprite activateSprite,
-        Sprite deactivateSprite,
-        Action abilityCleanUp,
-        Func<bool> checkAbility = null,
-        KeyCode hotkey = KeyCode.F)
-    {
-        self.Button = RoleAbilityFactory.CreatePassiveAbility(
-            activateTextKey: activateButtonName,
-            activateImg: activateSprite,
-            deactivateTextKey: deactivateButtonName,
-            deactivateImg: deactivateSprite,
-            canUse: self.IsAbilityUse,
-            ability: self.UseAbility,
-            canActivating: checkAbility,
-            abilityOff: abilityCleanUp,
-            hotKey: hotkey);
-
-        self.RoleAbilityInit();
-    }
-
-
-    public static void CreateCommonAbilityOption(
-        this IRoleAutoBuildAbility self,
-        IOptionInfo parentOps,
-        float defaultActiveTime = float.MaxValue)
-    {
-
-        SingleRoleBase role = (SingleRoleBase)self;
-
-        new FloatCustomOption(
-            self.GetRoleOptionId(RoleAbilityCommonOption.AbilityCoolTime),
-            string.Concat(
-                role.RoleName,
-                RoleAbilityCommonOption.AbilityCoolTime.ToString()),
-            defaultCoolTime, minCoolTime, maxCoolTime, step,
-            parentOps, format: OptionUnit.Second,
-            tab: role.Tab);
-
-        if (defaultActiveTime != float.MaxValue)
-        {
-            defaultActiveTime = Mathf.Clamp(
-                defaultActiveTime, minActiveTime, maxActiveTime);
-
-            new FloatCustomOption(
-                self.GetRoleOptionId(RoleAbilityCommonOption.AbilityActiveTime),
-                string.Concat(
-                    role.RoleName,
-                    RoleAbilityCommonOption.AbilityActiveTime.ToString()),
-                defaultActiveTime, minActiveTime, maxActiveTime, step,
-                parentOps, format: OptionUnit.Second,
-                tab: role.Tab);
-        }
-
-    }
-
-	public static void CreateAbilityCountOption(
+	public static void CreateNormalAbilityButton(
 		this IRoleAutoBuildAbility self,
-		IOptionInfo parentOps,
-		int defaultAbilityCount,
-		int maxAbilityCount,
-		float defaultActiveTime = float.MaxValue,
-		int minAbilityCount = 1)
-    {
+		string textKey,
+		Sprite sprite,
+		Func<bool> checkAbility = null,
+		Action abilityOff = null,
+		Action forceAbilityOff = null,
+		KeyCode hotkey = KeyCode.F)
+	{
 
-        SingleRoleBase role = (SingleRoleBase)self;
+		self.Button = RoleAbilityFactory.CreateReusableAbility(
+			textKey: textKey,
+			img: sprite,
+			canUse: self.IsAbilityUse,
+			ability: self.UseAbility,
+			canActivating: checkAbility,
+			abilityOff: abilityOff,
+			forceAbilityOff: forceAbilityOff,
+			hotKey: hotkey);
 
-        self.CreateCommonAbilityOption(
-            parentOps,
-            defaultActiveTime);
+		self.RoleAbilityInit();
+	}
 
-        new IntCustomOption(
-            self.GetRoleOptionId(RoleAbilityCommonOption.AbilityCount),
-            string.Concat(
-                role.RoleName,
-                RoleAbilityCommonOption.AbilityCount.ToString()),
-            defaultAbilityCount, minAbilityCount,
-            maxAbilityCount, 1,
-            parentOps, format: OptionUnit.Shot,
-            tab: role.Tab);
+	public static void CreateAbilityCountButton(
+		this IRoleAutoBuildAbility self,
+		string textKey,
+		Sprite sprite,
+		Func<bool> checkAbility = null,
+		Action abilityOff = null,
+		Action forceAbilityOff = null,
+		bool isReduceOnActive = false,
+		KeyCode hotkey = KeyCode.F)
+	{
+		self.Button = RoleAbilityFactory.CreateCountAbility(
+			textKey: textKey,
+			img: sprite,
+			canUse: self.IsAbilityUse,
+			ability: self.UseAbility,
+			canActivating: checkAbility,
+			abilityOff: abilityOff,
+			forceAbilityOff: forceAbilityOff,
+			isReduceOnActive: isReduceOnActive,
+			hotKey: hotkey);
 
-    }
+		self.RoleAbilityInit();
 
-    public static int GetRoleOptionId(
-        this IRoleAbility self,
-        RoleAbilityCommonOption option) => ((RoleOptionBase)self).GetRoleOptionId((int)option);
+	}
+
+
+	public static void CreateReclickableAbilityButton(
+		this IRoleAutoBuildAbility self,
+		string textKey,
+		Sprite sprite,
+		Func<bool> checkAbility = null,
+		Action abilityOff = null,
+		KeyCode hotkey = KeyCode.F)
+	{
+		self.Button = RoleAbilityFactory.CreateReclickAbility(
+			textKey: textKey,
+			img: sprite,
+			canUse: self.IsAbilityUse,
+			ability: self.UseAbility,
+			canActivating: checkAbility,
+			abilityOff: abilityOff,
+			hotKey: hotkey);
+
+		self.RoleAbilityInit();
+	}
+
+	public static void CreateReclickableCountAbilityButton(
+		this IRoleAutoBuildAbility self,
+		string textKey,
+		Sprite sprite,
+		Func<bool> checkAbility = null,
+		Action abilityOff = null,
+		KeyCode hotkey = KeyCode.F)
+	{
+		self.Button = RoleAbilityFactory.CreateReclickCountAbility(
+			textKey: textKey,
+			img: sprite,
+			canUse: self.IsAbilityUse,
+			ability: self.UseAbility,
+			canActivating: checkAbility,
+			abilityOff: abilityOff,
+			hotKey: hotkey);
+
+		self.RoleAbilityInit();
+	}
+
+	public static void CreateChargeAbilityButton(
+		this IRoleAutoBuildAbility self,
+		string textKey,
+		Sprite sprite,
+		Func<bool> checkAbility = null,
+		Action abilityOff = null,
+		Action forceAbilityOff = null,
+		KeyCode hotkey = KeyCode.F)
+	{
+
+		self.Button = RoleAbilityFactory.CreateChargableAbility(
+			textKey: textKey,
+			img: sprite,
+			canUse: self.IsAbilityUse,
+			ability: self.UseAbility,
+			canActivating: checkAbility,
+			abilityOff: abilityOff,
+			forceAbilityOff: forceAbilityOff,
+			hotKey: hotkey);
+
+		self.RoleAbilityInit();
+	}
+
+	public static void CreatePassiveAbilityButton(
+		this IRoleAutoBuildAbility self,
+		string activateButtonName,
+		string deactivateButtonName,
+		Sprite activateSprite,
+		Sprite deactivateSprite,
+		Action abilityCleanUp,
+		Func<bool> checkAbility = null,
+		KeyCode hotkey = KeyCode.F)
+	{
+		self.Button = RoleAbilityFactory.CreatePassiveAbility(
+			activateTextKey: activateButtonName,
+			activateImg: activateSprite,
+			deactivateTextKey: deactivateButtonName,
+			deactivateImg: deactivateSprite,
+			canUse: self.IsAbilityUse,
+			ability: self.UseAbility,
+			canActivating: checkAbility,
+			abilityOff: abilityCleanUp,
+			hotKey: hotkey);
+
+		self.RoleAbilityInit();
+	}
 }
