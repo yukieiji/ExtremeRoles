a2f581e55a73b8568ccf7bbadfc96398815aebb4
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-04-26 23:10:14 +0900

    fix:some env, infinity loop on intro

--- ExtremeRoles/Module/SystemType/GlobalCheckpointSystem.cs
+++ ExtremeRoles/Module/SystemType/GlobalCheckpointSystem.cs
@@ -1,16 +1,16 @@
-﻿using Hazel;
-using System;
-
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+
+using Hazel;
 
 using ExtremeRoles.Performance;
 using ExtremeRoles.Module.Interface;
-using System.Linq;
 using ExtremeRoles.Module.SystemType.CheckPoint;
 
 namespace ExtremeRoles.Module.SystemType;
 
-public sealed class GlobalCheckpointSystem : IDirtableSystemType
+public sealed class GlobalCheckpointSystem : IExtremeSystemType
 {
 	public abstract class CheckpointHandler
 	{
@@ -30,73 +30,62 @@ public sealed class GlobalCheckpointSystem : IDirtableSystemType
 		AssassinMeeting,
 	}
 
-	public bool IsDirty => false;
-
 	public const ExtremeSystemType Type = ExtremeSystemType.GlobalCheckpoint;
 
 	private readonly Dictionary<CheckpointType, CheckpointHandler> checkpoints = new Dictionary<CheckpointType, CheckpointHandler>();
 
-	public void Deteriorate(float deltaTime)
-	{
-		if (this.checkpoints.Count == 0)
-		{
-			return;
-		}
-
-		var removed = new HashSet<CheckpointType>();
-		var invalidPlayer = CachedPlayerControl.AllPlayerControls.Where(
-			x => x != null && x.Data != null && !x.Data.Disconnected);
-
-		foreach (var (type, checkPoint) in this.checkpoints)
-		{
-			var checkedPlayer = checkPoint.CheckedPlayer;
-
-			if (checkedPlayer.Count == 0 ||
-				!invalidPlayer.All(
-					x => checkedPlayer.Contains(x.PlayerId))
-				)
-			{
-				continue;
-			}
-			checkPoint.HandleChecked();
-			removed.Add(type);
-		}
-		foreach (var type in removed)
-		{
-			this.checkpoints.Remove(type);
-		}
-	}
-
-	public void Deserialize(MessageReader reader, bool initialState)
-	{ }
-
 	public void Reset(ResetTiming timing, PlayerControl resetPlayer = null)
-	{ }
-
-	public void Serialize(MessageWriter writer, bool initialState)
 	{ }
 
 	public void UpdateSystem(PlayerControl player, MessageReader msgReader)
 	{
-		var checkPointType = (CheckpointType)msgReader.ReadByte();
 		lock (this.checkpoints)
 		{
-			if (!this.checkpoints.TryGetValue(checkPointType, out var handler) ||
-				handler is null)
-			{
-				handler = checkPointType switch
-				{
-					CheckpointType.RoleAssign => new RoleAssignCheckPoint(),
-					CheckpointType.AssassinMeeting => new AssassinMeetingCheckpoint(msgReader),
-					_ => null,
-				};
-				this.checkpoints.Add(checkPointType, handler);
-			}
-			if (handler is null)
-			{
-				throw new ArgumentException("InvalidType");
-			}
+			var checkPointType = (CheckpointType)msgReader.ReadByte();
+			var handler = tryGetHandler(checkPointType, msgReader);
+
 			handler.AddCheckPoint(player.PlayerId);
+
+			if (isCheckpointOk(handler))
+			{
+				handler.HandleChecked();
+				this.checkpoints.Remove(checkPointType);
+			}
 		}
+	}
+
+	private CheckpointHandler tryGetHandler(CheckpointType type, in MessageReader msgReader)
+	{
+		if (!this.checkpoints.TryGetValue(type, out var handler) ||
+				handler is null)
+		{
+			handler = type switch
+			{
+				CheckpointType.RoleAssign => new RoleAssignCheckPoint(),
+				CheckpointType.AssassinMeeting => new AssassinMeetingCheckpoint(msgReader),
+				_ => null,
+			};
+			this.checkpoints.Add(type, handler);
+		}
+		if (handler is null)
+		{
+			throw new ArgumentException("InvalidType");
+		}
+		return handler;
+	}
+
+	private bool isCheckpointOk(in CheckpointHandler handler)
+	{
+		var checkedPlayer = handler.CheckedPlayer;
+
+		if (checkedPlayer.Count == 1)
+		{
+			return false;
+		}
+
+		var invalidPlayer = CachedPlayerControl.AllPlayerControls.Where(
+			x => x != null && x.Data != null && !x.Data.Disconnected);
+
+		return invalidPlayer.All(x => checkedPlayer.Contains(x.PlayerId));
 	}
 }
