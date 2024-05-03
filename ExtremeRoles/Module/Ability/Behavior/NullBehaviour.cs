1db04274fc20bcb740d6d1695fc6d400acc1b32d
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-05-03 19:04:56 +0900

    feat:nullbehaviour

---
+++ ExtremeRoles/Module/Ability/Behavior/NullBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace ExtremeRoles.Module.Ability.Behavior;
+
+public sealed class NullBehaviour : BehaviorBase
+{
+	public NullBehaviour() : base("", null)
+	{ }
+
+	public override void AbilityOff()
+	{ }
+
+	public override void ForceAbilityOff()
+	{ }
+
+	public override void Initialize(ActionButton button)
+	{ }
+
+	public override bool IsUse() => false;
+
+	public override bool TryUseAbility(float timer, AbilityState curState, out AbilityState newState)
+	{
+		newState = curState;
+		return false;
+	}
+
+	public override AbilityState Update(AbilityState curState)
+		=> curState;
+}
