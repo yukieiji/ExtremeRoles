49c3ec283baaed24c41a4c533674baad96a43511
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-05-05 13:02:52 +0900

    feat:count behaviour

---
+++ ExtremeRoles/Module/Ability/Behavior/ChargingCountBehaviour.cs
@@ -0,0 +1,160 @@
+﻿using System;
+
+using UnityEngine;
+
+using ExtremeRoles.Helper;
+using ExtremeRoles.Module.Ability.Behavior.Interface;
+
+#nullable enable
+
+namespace ExtremeRoles.Module.Ability.Behavior;
+
+public sealed class ChargingCountBehaviour : BehaviorBase, IChargingBehavior, ICountBehavior
+{
+	public float ChargeGage { get; set; }
+	public float ChargeTime { get; set; }
+
+	public bool IsCharging => throw new NotImplementedException();
+
+	public int AbilityCount { get; private set; }
+
+	public enum ReduceTiming
+	{
+		OnActive,
+		OnCharge,
+	}
+
+	private bool isUpdate = false;
+	private bool isCharge = false;
+	private TMPro.TextMeshPro? abilityCountText = null;
+	private string buttonTextFormat = ICountBehavior.DefaultButtonCountText;
+
+	private readonly Func<bool, float, bool> isUse;
+	private readonly Func<float, bool> ability;
+	private readonly ReduceTiming reduceTiming;
+	private readonly Action? forceAbilityOff;
+	private readonly Action? abilityOff;
+
+	public ChargingCountBehaviour(
+		string text, Sprite img,
+		Func<bool, float, bool> isUse,
+		Func<float, bool> ability,
+		ReduceTiming reduceTiming,
+		Action? abilityOff = null,
+		Action? forceAbilityOff = null) : base(text, img)
+	{
+		this.isUse = isUse;
+		this.ability = ability;
+		this.reduceTiming = reduceTiming;
+		this.abilityOff = abilityOff;
+		this.forceAbilityOff = forceAbilityOff;
+	}
+
+	public override void AbilityOff()
+	{
+		this.abilityOff?.Invoke();
+	}
+
+	public override void ForceAbilityOff()
+	{
+		this.forceAbilityOff?.Invoke();
+	}
+
+	public override void Initialize(ActionButton button)
+	{
+		this.abilityCountText = ICountBehavior.CreateCountText(button);
+		updateAbilityCountText();
+	}
+
+	public override bool IsUse() => this.isUse.Invoke(
+		this.isCharge, this.ChargeGage);
+
+	public void SetAbilityCount(int newAbilityNum)
+	{
+		this.AbilityCount = newAbilityNum;
+		this.isUpdate = true;
+		updateAbilityCountText();
+	}
+
+	public void SetButtonTextFormat(string newTextFormat)
+	{
+		this.buttonTextFormat = newTextFormat;
+	}
+
+	public override bool TryUseAbility(float timer, AbilityState curState, out AbilityState newState)
+	{
+		newState = curState;
+		switch (curState)
+		{
+			case AbilityState.Ready:
+				if (timer > 0 ||
+					this.AbilityCount <= 0 ||
+					this.IsUse())
+				{
+					return false;
+				}
+				if (this.reduceTiming is ReduceTiming.OnCharge)
+				{
+					reduceAbilityCount();
+				}
+				newState = AbilityState.Charging;
+				break;
+			case AbilityState.Charging:
+
+				bool isUse = this.IsUse();
+
+				if (isUse &&
+					(
+						(this.AbilityCount > 0) ||
+						(this.reduceTiming is ReduceTiming.OnCharge && this.AbilityCount >= 0)
+					)  &&
+					this.ability.Invoke(this.ChargeGage))
+				{
+					return false;
+				}
+				if (this.reduceTiming is ReduceTiming.OnActive)
+				{
+					reduceAbilityCount();
+				}
+				newState = AbilityState.CoolDown;
+				break;
+			default:
+				return false;
+		}
+		return true;
+	}
+
+	public override AbilityState Update(AbilityState curState)
+	{
+		if (curState == AbilityState.Charging)
+		{
+			return curState;
+		}
+
+		if (this.isUpdate)
+		{
+			this.isUpdate = false;
+			return AbilityState.CoolDown;
+		}
+
+		return
+			this.AbilityCount > 0 ? curState : AbilityState.None;
+	}
+
+	private void updateAbilityCountText()
+	{
+		if (this.abilityCountText == null)
+		{
+			return;
+		}
+
+		this.abilityCountText.text = string.Format(
+			Translation.GetString(this.buttonTextFormat),
+			this.AbilityCount);
+	}
+	private void reduceAbilityCount()
+	{
+		--this.AbilityCount;
+		updateAbilityCountText();
+	}
+}
