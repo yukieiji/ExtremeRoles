43aaf4b2dd4d634153ab3d234a7a3c4fcb746419
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-05-06 03:20:35 +0900

    refactor

--- ExtremeSkins/Patches/AmongUs/TranslationControllerPatch.cs
+++ ExtremeSkins/Patches/AmongUs/TranslationControllerPatch.cs
@@ -7,13 +7,12 @@ using ExtremeSkins.SkinManager;
 namespace ExtremeSkins.Patches.AmongUs
 {
     [HarmonyPatch(
-        typeof(TranslationController), 
+        typeof(TranslationController),
         nameof(TranslationController.GetString),
-        new[]
-        {
+		[
                 typeof(StringNames),
                 typeof(Il2CppReferenceArray<Il2CppSystem.Object>)
-        })]
+        ])]
     public static class ColorStringPatch
     {
         public static bool Prefix(ref string __result, [HarmonyArgument(0)] StringNames name)
