d0d38d079b8013d8ba884a33abf8b4f4e87cb877
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-05-07 19:38:56 +0900

    change:exr trans datas

--- makelanguagejson.py
+++ makelanguagejson.py
@@ -2,14 +2,14 @@ import os
 import json
 import glob
 
-from typing import Dict
+from typing import Dict, Any
 
 from pylightxl import readxl
 
 WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
 
 EXTREMERORLS_IN_FILE = os.path.join(WORKING_DIR, "ExtremeRolesTransData.xlsx")
-EXTREMERORLS_OUT_FILE = os.path.join(WORKING_DIR, "ExtremeRoles", "Resources", "JsonData", "Language.json")
+EXTREMERORLS_OUT_FILE = os.path.join(WORKING_DIR, "ExtremeRoles", "Resources", "JsonData", "Language")
 
 EXTREMESKIN_IN_FILE = os.path.join(WORKING_DIR, "ExtremeSkinsTransData.xlsx")
 EXTREMESKIN_OUT_FILE = os.path.join(WORKING_DIR, "ExtremeSkins", "Resources", "LangData", "stringData.json")
@@ -26,7 +26,7 @@ def create_public_beta_transdata():
     xlsx_to_json(file_path, os.path.join(PUBLIC_BETA_OUT_FILE, f'{file_name}.json'))
 
 
-def is_require_update(new_json : Dict[str, str], output_file : str) -> bool:
+def is_require_update(new_json : Any, output_file : str) -> bool:
 
   try:
     with open(output_file, "r") as f:
@@ -39,6 +39,49 @@ def is_require_update(new_json : Dict[str, str], output_file : str) -> bool:
 
     return old_json_str != new_json_str
 
+def export_to_json(xlsx_data:Any, output_file: str) -> None:
+
+    if not is_require_update(xlsx_data, output_file):
+      return
+
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
+    with open(output_file, "w", encoding='utf-8') as f:
+      json.dump(xlsx_data, f, indent=4)
+
+def xlsx_to_new_json(file_name : str, output_file : str) -> None:
+  wb = readxl(file_name)
+
+  xlsx_data = {}
+
+  for name in wb.ws_names:
+
+    sheat = wb.ws(name)
+
+    row, col = sheat.size
+
+    # 行を回す
+    for i in range(2, row + 1):
+
+      # i行目の1列目はキー
+      key = sheat.index(i, 1)
+      if key == "":
+        continue
+
+      # i行目j列がデータ、jは2以上であり2が0(英語)である
+      for j in range(2, col + 1):
+          cell_data = sheat.index(i, j)
+
+          if type(cell_data) != str:
+            continue
+          lang = sheat.index(1, j)
+          if not (lang in xlsx_data):
+            xlsx_data[lang] = {}
+
+          xlsx_data[lang][key] = cell_data.replace("\r", "").replace("_x000D_", "").replace("\\n", "\n")
+
+  for lang, data in xlsx_data.items():
+    export_to_json(data, os.path.join(output_file, f'{lang}.json'))
 
 def xlsx_to_json(file_name : str, output_file : str) -> None:
 
@@ -75,15 +118,10 @@ def xlsx_to_json(file_name : str, output_file : str) -> None:
       if data != {}:
         xlsx_data[key] = data
 
-  if is_require_update(xlsx_data, output_file):
-
-    os.makedirs(os.path.dirname(output_file), exist_ok=True)
-
-    with open(output_file, "w") as f:
-      json.dump(xlsx_data, f, indent=4)
+  export_to_json(xlsx_data, output_file)
 
 def main() -> None:
-  xlsx_to_json(EXTREMERORLS_IN_FILE, EXTREMERORLS_OUT_FILE)
+  xlsx_to_new_json(EXTREMERORLS_IN_FILE, EXTREMERORLS_OUT_FILE)
   xlsx_to_json(EXTREMESKIN_IN_FILE, EXTREMESKIN_OUT_FILE)
   create_public_beta_transdata()
 
