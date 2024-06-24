8b890d95e2f88db43b1cc2bb4d4588a1cf3abde4
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-06-24 22:36:43 +0900

    fix:shere

--- ExtremeRoles/Patches/Player/RpcSyncSettings.cs
+++ ExtremeRoles/Patches/Player/RpcSyncSettings.cs
@@ -4,13 +4,11 @@ namespace ExtremeRoles.Patches.Player;
 
 #nullable enable
 
-
-
 [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.RpcSyncSettings))]
 public static class PlayerControlRpcSyncSettingsPatch
 {
 	public static void Postfix()
 	{
-		// OptionManager.Instance.ShareOptionSelections();
+		OptionManager.Instance.ShereAllOption();
 	}
 }
