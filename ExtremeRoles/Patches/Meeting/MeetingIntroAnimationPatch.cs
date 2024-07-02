7d37957b09ac9806b65a92c5dd71f3a3ddc18474
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-07-03 00:20:13 +0900

    fix:ghostrole strs

--- ExtremeRoles/Patches/Meeting/MeetingIntroAnimationPatch.cs
+++ ExtremeRoles/Patches/Meeting/MeetingIntroAnimationPatch.cs
@@ -5,6 +5,7 @@ using TMPro;
 using ExtremeRoles.Module;
 using ExtremeRoles.Performance;
 using ExtremeRoles.GameMode;
+using System.Text;
 
 namespace ExtremeRoles.Patches.Meeting;
 
@@ -54,27 +55,31 @@ public static class MeetingIntroAnimationInitPatch
 
 		TMP_Text text = textSubMesh.textComponent;
 
-		string gaProtectText = string.Empty;
+		var builder = new StringBuilder();
 
 		if (someoneWasProtected && !ExtremeGameModeManager.Instance.ShipOption.GhostRole.IsBlockGAAbilityReport)
 		{
-			gaProtectText = text.text;
+            builder.Append(text.text);
 		}
 
-		string exrAbiltyText =
-			MeetingReporter.IsExist ?
-			MeetingReporter.Instance.GetMeetingStartReport() : string.Empty;
+		if (MeetingReporter.IsExist)
+		{
+			if (builder.Length > 0)
+			{
+				builder.AppendLine();
+			}
+			//builder.AppendLine(); (改行)
 
-        bool isGaAbilityTextEmpty = string.IsNullOrEmpty(gaProtectText);
-		bool isExrAbilityTextEmpty = string.IsNullOrEmpty(exrAbiltyText);
+            builder.Append(
+				MeetingReporter.Instance.GetMeetingEndReport());
+		}
 
-		if (isGaAbilityTextEmpty && isExrAbilityTextEmpty)
+		if (builder.Length <= 0)
 		{
 			return;
 		}
 
-		text.text = isGaAbilityTextEmpty ?
-			exrAbiltyText : string.Concat(gaProtectText, "\n", exrAbiltyText);
+		text.text = builder.ToString();
 		SoundManager.Instance.PlaySound(__instance.ProtectedRecentlySound, false, 1f);
 		__instance.ProtectedRecently.SetActive(true);
     }
