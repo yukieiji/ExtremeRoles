b5d1ab02b29634301eb590a36e4f15abbba62967
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-08-06 01:49:42 +0900

    feat:lang tests

--- ExtremeRoles.Test/ExtremeRolesTestPlugin.cs
+++ ExtremeRoles.Test/ExtremeRolesTestPlugin.cs
@@ -40,7 +40,7 @@ public static class ChatControllerSendChatPatch
 		{
 
 			GameUtility.ChangePresetTo(19);
-			TestRunnerBase.Run<AllAssetLoadRunner>();
+			TestRunnerBase.Run<TranslationTestRunner>();
 		}
 	}
 }
