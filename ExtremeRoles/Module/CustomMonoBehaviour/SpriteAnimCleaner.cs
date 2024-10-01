515a69ead1dba74a77039bd13b635368fbfb00ab
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-10-01 13:05:09 +0900

    fix:dupe AnimatorOverrideController

---
+++ ExtremeRoles/Module/CustomMonoBehaviour/SpriteAnimCleaner.cs
@@ -0,0 +1,21 @@
+﻿using PowerTools;
+using UnityEngine;
+
+namespace ExtremeRoles.Module.CustomMonoBehaviour;
+
+[Il2CppRegister]
+public sealed class SpriteAnimCleaner : MonoBehaviour
+{
+	public SpriteAnim Anim { private get; set; }
+	public void OnDestroy()
+	{
+		if (Anim == null ||
+			!Anim.m_initialized ||
+			Anim.m_controller == null)
+		{
+			return;
+		}
+		Destroy(Anim.m_controller);
+		Anim.m_controller = null;
+	}
+}
