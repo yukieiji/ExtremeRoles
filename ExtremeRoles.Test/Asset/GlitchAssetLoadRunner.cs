33245e4eecf040498336d6303f46ad01972fff3e
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-12-12 22:12:24 +0900

    feat:asset tests

---
+++ ExtremeRoles.Test/Asset/GlitchAssetLoadRunner.cs
@@ -0,0 +1,14 @@
+﻿using ExtremeRoles.Roles;
+
+namespace ExtremeRoles.Test.Asset;
+
+internal sealed class GlitchAssetLoadRunner
+	: AssetLoadRunner
+{
+	public override void Run()
+	{
+		Log.LogInfo($"----- Unit:GlitchAsset Test -----");
+
+		LoadFromExR(ExtremeRoleId.Glitch);
+	}
+}
---
+++ ExtremeRoles.Test/Asset/RaiderAssetLoadRunner.cs
@@ -0,0 +1,14 @@
+﻿using ExtremeRoles.Roles;
+
+namespace ExtremeRoles.Test.Asset;
+
+internal sealed class RaiderAssetLoadRunner
+	: AssetLoadRunner
+{
+	public override void Run()
+	{
+		Log.LogInfo($"----- Unit:RaiderAsset Test -----");
+
+		LoadFromExR(ExtremeRoleId.Raider);
+	}
+}
