31b65d51b344f95560d900d735dd046d5be4aed3
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-12-22 14:51:23 +0900

    refactor:winner builds

--- ExtremeRoles/Module/GameResult/ExtremeGameResult.cs
+++ ExtremeRoles/Module/GameResult/ExtremeGameResult.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 using ExtremeRoles.Helper;
-using ExtremeRoles.GameMode;
-using ExtremeRoles.Roles;
-using ExtremeRoles.Roles.API;
-using ExtremeRoles.Roles.API.Interface;
-using ExtremeRoles.GhostRoles;
-using ExtremeRoles.GhostRoles.API;
-using ExtremeRoles.GhostRoles.API.Interface;
 using ExtremeRoles.Module.CustomMonoBehaviour;
 using ExtremeRoles.Performance.Il2Cpp;
 
-
-using TempWinData = Il2CppSystem.Collections.Generic.List<CachedPlayerData>;
 using Player = NetworkedPlayerInfo;
 
 #nullable enable
@@ -27,7 +15,7 @@ public sealed class ExtremeGameResultManager : NullableSingleton<ExtremeGameResu
 	public readonly record struct TaskInfo(int CompletedTask, int TotalTask);
 
 	public WinnerTempData.Result Winner => winner.Convert();
-	public List<FinalSummary.PlayerSummary> PlayerSummaries { get; init; }
+	public IReadOnlyList<FinalSummary.PlayerSummary> PlayerSummaries { get; private set; }
 
 	private readonly int winGameControlId;
 	private readonly Dictionary<byte, TaskInfo> playerTaskInfo = new Dictionary<byte, TaskInfo>();
@@ -35,9 +23,9 @@ public sealed class ExtremeGameResultManager : NullableSingleton<ExtremeGameResu
 
 	public ExtremeGameResultManager()
 	{
-		winner = new WinnerTempData();
-		PlayerSummaries = new List<FinalSummary.PlayerSummary>();
-		winGameControlId = ExtremeRolesPlugin.ShipState.WinGameControlId;
+		this.winner = new WinnerTempData();
+		this.PlayerSummaries = new List<FinalSummary.PlayerSummary>();
+		this.winGameControlId = ExtremeRolesPlugin.ShipState.WinGameControlId;
 	}
 
 	public void CreateTaskInfo()
@@ -45,372 +33,18 @@ public sealed class ExtremeGameResultManager : NullableSingleton<ExtremeGameResu
 		foreach (Player playerInfo in GameData.Instance.AllPlayers.GetFastEnumerator())
 		{
 			var (completedTask, totalTask) = GameSystem.GetTaskInfo(playerInfo);
-			playerTaskInfo.Add(
+			this.playerTaskInfo.Add(
 				playerInfo.PlayerId,
 				new TaskInfo(completedTask, totalTask));
-			winner.AddPool(playerInfo);
+			this.winner.AddPool(playerInfo);
 		}
 	}
 
 	public void CreateEndGameManagerResult()
 	{
-		PlayerSummaries.Clear();
-		winner.SetWinner();
+		this.winner.SetWinner();
 
-		var logger = ExtremeRolesPlugin.Logger;
-
-		int playerNum = GameData.Instance.AllPlayers.Count;
-
-		PlayerSummaries.Capacity = playerNum;
-
-		var neutralNoWinner = new List<Player>(playerNum);
-		var modRole = new List<(Player, IRoleWinPlayerModifier)>(playerNum);
-		var ghostWinCheckRole = new List<(Player, IGhostRoleWinable)>(playerNum);
-
-		var roleData = ExtremeRoleManager.GameRole;
-		var gameData = ExtremeRolesPlugin.ShipState;
-
-		GameOverReason reason = gameData.EndReason;
-		RoleGameOverReason modReason = (RoleGameOverReason)reason;
-
-		string resonStr = Enum.IsDefined(modReason) ? modReason.ToString() : reason.ToString();
-		logger.LogInfo($"GameEnd : {resonStr}");
-
-		logger.LogInfo("---- Start: Creating Winner ----");
-
-		foreach (Player playerInfo in GameData.Instance.AllPlayers.GetFastEnumerator())
-		{
-			byte playerId = playerInfo.PlayerId;
-			if (!ExtremeRoleManager.TryGetRole(playerId, out var role))
-			{
-				continue;
-			}
-
-			string playerName = playerInfo.PlayerName;
-			if (role.IsNeutral())
-			{
-				if (ExtremeRoleManager.IsAliveWinNeutral(role, playerInfo))
-				{
-					logger.LogInfo($"AddPlusWinner(Reason:Alive Win) : {playerName}");
-					winner.AddPlusWinner(playerInfo);
-				}
-				else
-				{
-					neutralNoWinner.Add(playerInfo);
-				}
-				logger.LogInfo($"Remove Winner(Reason:Neutral) : {playerName}");
-				winner.Remove(playerInfo);
-			}
-			else if (role.Id == ExtremeRoleId.Xion)
-			{
-				logger.LogInfo($"Remove Winner(Reason:Xion Player) : {playerName}");
-				winner.Remove(playerInfo);
-			}
-
-			if (role is IRoleWinPlayerModifier winModRole)
-			{
-				modRole.Add((playerInfo, winModRole));
-			}
-
-			if (role is MultiAssignRoleBase multiAssignRole &&
-				multiAssignRole.AnotherRole is IRoleWinPlayerModifier multiWinModRole)
-			{
-				modRole.Add((playerInfo, multiWinModRole));
-			}
-
-			if (ExtremeGhostRoleManager.GameRole.TryGetValue(
-					playerId, out GhostRoleBase? ghostRole) &&
-				ghostRole is not null &&
-				ghostRole.IsNeutral() &&
-				ghostRole is IGhostRoleWinable winCheckGhostRole)
-			{
-				ghostWinCheckRole.Add((playerInfo, winCheckGhostRole));
-			}
-
-			if (playerTaskInfo.TryGetValue(playerId, out var taskInfo))
-			{
-				var summary =
-					FinalSummary.PlayerSummary.Create(
-						playerInfo, role, ghostRole, taskInfo);
-
-				PlayerSummaries.Add(summary);
-			}
-		}
-
-		foreach (Player winner in winner.PlusedWinner)
-		{
-			logger.LogInfo($"Remove Winner(Dupe Player) : {winner.PlayerName}");
-			this.winner.Remove(winner);
-		}
-
-		if (ExtremeGameModeManager.Instance.ShipOption.DisableNeutralSpecialForceEnd)
-		{
-			addNeutralWinner();
-		}
-
-		replaceWinner(modReason, neutralNoWinner);
-
-		logger.LogInfo($"-- Start: merge plused win player --");
-		foreach (var player in winner.PlusedWinner)
-		{
-			logger.LogInfo($"marge to winner:{player.PlayerName}");
-			winner.Add(player);
-		}
-		logger.LogInfo($"-- End: merge plused win player --");
-
-		foreach (var (playerInfo, winCheckRole) in ghostWinCheckRole)
-		{
-			if (winCheckRole.IsWin(reason, playerInfo))
-			{
-				logger.LogInfo($"Add Winner(Reason:Ghost Role win) : {playerInfo.PlayerName}");
-				winner.AddWithPlus(playerInfo);
-			}
-		}
-
-		logger.LogInfo($"-- Start: modified win player --");
-		foreach (var (playerInfo, winModRole) in modRole)
-		{
-			winModRole.ModifiedWinPlayer(
-				playerInfo,
-				gameData.EndReason,
-				in winner);
-		}
-		logger.LogInfo($"-- End: modified win player --");
-
-
-		logger.LogInfo("--- End: Creating Winner ----");
-#if DEBUG
-		logger.LogInfo(winner.ToString());
-#endif
-	}
-
-	private void replaceWinner(RoleGameOverReason reason, in IReadOnlyList<Player> neutralNoWinner)
-	{
-		switch (reason)
-		{
-			case RoleGameOverReason.AssassinationMarin:
-			case RoleGameOverReason.TeroristoTeroWithShip:
-				winner.Clear();
-				foreach (Player player in GameData.Instance.AllPlayers.GetFastEnumerator())
-				{
-					if (ExtremeRoleManager.TryGetRole(player.PlayerId, out var role) &&
-						role.IsImpostor())
-					{
-						winner.Add(player);
-					}
-				}
-				break;
-			case RoleGameOverReason.AliceKilledByImposter:
-			case RoleGameOverReason.AliceKillAllOther:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Alice);
-				break;
-			case RoleGameOverReason.JackalKillAllOther:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Jackal, ExtremeRoleId.Sidekick);
-				break;
-			case RoleGameOverReason.LoverKillAllOther:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Lover);
-				break;
-			case RoleGameOverReason.ShipFallInLove:
-				replaceWinnerToSpecificRolePlayer(
-					ExtremeRoleId.Lover);
-				break;
-			case RoleGameOverReason.TaskMasterGoHome:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.TaskMaster);
-				break;
-			case RoleGameOverReason.MissionaryAllAgainstGod:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Missionary);
-				break;
-			case RoleGameOverReason.JesterMeetingFavorite:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Jester);
-				break;
-			case RoleGameOverReason.YandereKillAllOther:
-			case RoleGameOverReason.YandereShipJustForTwo:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Yandere);
-				break;
-			case RoleGameOverReason.VigilanteKillAllOther:
-			case RoleGameOverReason.VigilanteNewIdealWorld:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Vigilante);
-				break;
-			case RoleGameOverReason.MinerExplodeEverything:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Miner);
-				break;
-			case RoleGameOverReason.EaterAllEatInTheShip:
-			case RoleGameOverReason.EaterAliveAlone:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Eater);
-				break;
-			case RoleGameOverReason.TraitorKillAllOther:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Traitor);
-				break;
-			case RoleGameOverReason.QueenKillAllOther:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Queen, ExtremeRoleId.Servant);
-				break;
-			case RoleGameOverReason.UmbrerBiohazard:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Umbrer);
-				break;
-			case RoleGameOverReason.KidsTooBigHomeAlone:
-			case RoleGameOverReason.KidsAliveAlone:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Delinquent);
-				break;
-			case RoleGameOverReason.HatterEndlessTeaTime:
-			case RoleGameOverReason.HatterTeaPartyTime:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Hatter);
-				break;
-			case RoleGameOverReason.ArtistShipToArt:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Artist);
-				break;
-			case RoleGameOverReason.TuckerShipIsExperimentStation:
-				replaceWinnerToSpecificNeutralRolePlayer(
-					neutralNoWinner, ExtremeRoleId.Tucker, ExtremeRoleId.Chimera);
-				break;
-			default:
-				break;
-		}
-	}
-
-	private void replaceWinnerToSpecificRolePlayer(
-		ExtremeRoleId roleId)
-	{
-		winner.Clear();
-
-		foreach (var player in GameData.Instance.AllPlayers.GetFastEnumerator())
-		{
-			if (!ExtremeRoleManager.TryGetRole(player.PlayerId, out var role))
-			{
-				continue;
-			}
-
-			if (role.Id == roleId)
-			{
-				addSpecificRoleToSameControlIdPlayer(role, player);
-			}
-			else if (
-				role is MultiAssignRoleBase multiAssignRole &&
-				multiAssignRole.AnotherRole is not null &&
-				multiAssignRole.AnotherRole.Id == roleId)
-			{
-				addSpecificRoleToSameControlIdPlayer(multiAssignRole.AnotherRole, player);
-			}
-		}
-	}
-
-	private void replaceWinnerToSpecificNeutralRolePlayer(
-		in IReadOnlyList<Player> neutralNoWinner, params ExtremeRoleId[] roles)
-	{
-		ExtremeRolesPlugin.Logger.LogInfo("Clear Winner(Reason:Neautal Win)");
-		winner.Clear();
-
-		foreach (var player in neutralNoWinner)
-		{
-			if (!ExtremeRoleManager.TryGetRole(player.PlayerId, out var role))
-			{
-				continue;
-			}
-
-			if (roles.Contains(role.Id))
-			{
-				addSpecificNeutralRoleToSameControlIdPlayer(role, player);
-			}
-			else if (
-				role is MultiAssignRoleBase multiAssignRole &&
-				multiAssignRole.AnotherRole is not null &&
-				roles.Contains(multiAssignRole.AnotherRole.Id))
-			{
-				addSpecificNeutralRoleToSameControlIdPlayer(role, player);
-			}
-		}
-	}
-
-	private void addNeutralWinner()
-	{
-		HashSet<(ExtremeRoleId, int)> winRole = new HashSet<(ExtremeRoleId, int)>();
-
-		foreach (Player playerInfo in GameData.Instance.AllPlayers.GetFastEnumerator())
-		{
-			if (!ExtremeRoleManager.TryGetRole(playerInfo.PlayerId, out var role))
-			{
-				continue;
-			}
-
-			if (role is MultiAssignRoleBase multiAssignRole &&
-				multiAssignRole.AnotherRole is not null &&
-				tryAddWinRole(
-					multiAssignRole.AnotherRole,
-						playerInfo, in winRole))
-			{
-				continue;
-			}
-			tryAddWinRole(role, playerInfo, in winRole);
-		}
-	}
-
-	private bool tryAddWinRole(
-		in SingleRoleBase role,
-		in Player playerInfo,
-		in HashSet<(ExtremeRoleId, int)> winRole)
-	{
-		int gameControlId = role.GameControlId;
-
-		if (ExtremeGameModeManager.Instance.ShipOption.IsSameNeutralSameWin)
-		{
-			gameControlId = PlayerStatistics.SameNeutralGameControlId;
-		}
-
-		var logger = ExtremeRolesPlugin.Logger;
-		var item = (role.Id, gameControlId);
-
-		if (winRole.Contains(item))
-		{
-			logger.LogInfo($"Add Winner(Reason:Additional Neutral Win) : {playerInfo.PlayerName}");
-			winner.Add(playerInfo);
-			return true;
-		}
-		else if (role.IsNeutral() && role.IsWin)
-		{
-			winRole.Add(item);
-
-			logger.LogInfo($"Add Winner(Reason:Additional Neutral Win) : {playerInfo.PlayerName}");
-			winner.Add(playerInfo);
-			return true;
-		}
-		return false;
-	}
-
-	private void addSpecificRoleToSameControlIdPlayer(in SingleRoleBase role, in Player player)
-	{
-		if (winGameControlId != int.MaxValue &&
-			winGameControlId == role.GameControlId)
-		{
-			ExtremeRolesPlugin.Logger.LogInfo($"Add Winner(Reason:Win this role) : {player.PlayerName}");
-			winner.Add(player);
-		}
-	}
-
-	private void addSpecificNeutralRoleToSameControlIdPlayer(in SingleRoleBase role, in Player player)
-	{
-		if (ExtremeGameModeManager.Instance.ShipOption.IsSameNeutralSameWin)
-		{
-			ExtremeRolesPlugin.Logger.LogInfo($"Add Winner(Reason:Win this role) : {player.PlayerName}");
-			winner.Add(player);
-		}
-		else
-		{
-			addSpecificRoleToSameControlIdPlayer(role, player);
-		}
+		var builder = new WinnerBuilder(this.winGameControlId, this.winner, this.playerTaskInfo);
+		this.PlayerSummaries = builder.Build();
 	}
 }
---
+++ ExtremeRoles/Module/GameResult/WinnerBuilder.cs
@@ -0,0 +1,433 @@
+﻿using ExtremeRoles.GhostRoles.API.Interface;
+using ExtremeRoles.Module.CustomMonoBehaviour;
+using ExtremeRoles.Roles.API.Interface;
+using ExtremeRoles.Roles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Player = NetworkedPlayerInfo;
+
+using ExtremeRoles.GhostRoles.API;
+using ExtremeRoles.GhostRoles;
+using ExtremeRoles.Performance.Il2Cpp;
+using ExtremeRoles.Roles.API;
+using ExtremeRoles.GameMode;
+
+namespace ExtremeRoles.Module.GameResult;
+
+#nullable enable
+
+public sealed class WinnerBuilder
+{
+	private readonly WinnerTempData tempData;
+	private readonly List<Player> neutralNoWinner = [];
+	private readonly List<(Player, IRoleWinPlayerModifier)> modRole = [];
+	private readonly List<(Player, IGhostRoleWinable)> ghostWinCheckRole = [];
+	private readonly IReadOnlyDictionary<byte, ExtremeGameResultManager.TaskInfo> taskInfo;
+	private readonly int winGameControlId;
+
+	private readonly GameOverReason gameOverReason;
+	private readonly RoleGameOverReason roleGameOverReason;
+
+	public WinnerBuilder(
+		int winGameControlId,
+		WinnerTempData tempData,
+		IReadOnlyDictionary<byte, ExtremeGameResultManager.TaskInfo> taskInfo)
+	{
+		this.winGameControlId = winGameControlId;
+		this.tempData = tempData;
+		this.taskInfo = taskInfo;
+
+		int playerNum = GameData.Instance.AllPlayers.Count;
+
+		this.neutralNoWinner.Capacity = playerNum;
+		this.modRole.Capacity = playerNum;
+		this.ghostWinCheckRole.Capacity = playerNum;
+
+		this.gameOverReason = ExtremeRolesPlugin.ShipState.EndReason;
+		this.roleGameOverReason = (RoleGameOverReason)this.gameOverReason;
+	}
+
+	public IReadOnlyList<FinalSummary.PlayerSummary> Build()
+	{
+		var logger = ExtremeRolesPlugin.Logger;
+
+		string resonStr = Enum.IsDefined(this.roleGameOverReason) ?
+			this.roleGameOverReason.ToString() : this.gameOverReason.ToString();
+		logger.LogInfo($"GameEnd : {resonStr}");
+
+		logger.LogInfo("---- Start: Creating Winner ----");
+
+		var summaries = this.initialize();
+
+		removeAddPlusWinner();
+		addNeutralWiner();
+		replaceWinner();
+		mergeWinner();
+		addGhostRoleWinner();
+		modifiedWinner();
+
+		logger.LogInfo("--- End: Creating Winner ----");
+
+#if DEBUG
+		logger.LogInfo(this.tempData.ToString());
+#endif
+		return summaries;
+	}
+
+	private List<FinalSummary.PlayerSummary> initialize()
+	{
+		var summaries = new List<FinalSummary.PlayerSummary>(this.neutralNoWinner.Capacity);
+		var logger = ExtremeRolesPlugin.Logger;
+
+		foreach (Player playerInfo in GameData.Instance.AllPlayers.GetFastEnumerator())
+		{
+			byte playerId = playerInfo.PlayerId;
+			if (!ExtremeRoleManager.TryGetRole(playerId, out var role))
+			{
+				continue;
+			}
+
+			string playerName = playerInfo.PlayerName;
+			if (role.IsNeutral())
+			{
+				if (ExtremeRoleManager.IsAliveWinNeutral(role, playerInfo))
+				{
+					logger.LogInfo($"AddPlusWinner(Reason:Alive Win) : {playerName}");
+					this.tempData.AddPlusWinner(playerInfo);
+				}
+				else
+				{
+					this.neutralNoWinner.Add(playerInfo);
+				}
+				logger.LogInfo($"Remove Winner(Reason:Neutral) : {playerName}");
+				this.tempData.Remove(playerInfo);
+			}
+			else if (role.Id == ExtremeRoleId.Xion)
+			{
+				logger.LogInfo($"Remove Winner(Reason:Xion Player) : {playerName}");
+				this.tempData.Remove(playerInfo);
+			}
+
+			if (role is IRoleWinPlayerModifier winModRole)
+			{
+				this.modRole.Add((playerInfo, winModRole));
+			}
+
+			if (role is MultiAssignRoleBase multiAssignRole &&
+				multiAssignRole.AnotherRole is IRoleWinPlayerModifier multiWinModRole)
+			{
+				this.modRole.Add((playerInfo, multiWinModRole));
+			}
+
+			if (ExtremeGhostRoleManager.GameRole.TryGetValue(
+					playerId, out GhostRoleBase? ghostRole) &&
+				ghostRole is not null &&
+				ghostRole.IsNeutral() &&
+				ghostRole is IGhostRoleWinable winCheckGhostRole)
+			{
+				this.ghostWinCheckRole.Add((playerInfo, winCheckGhostRole));
+			}
+
+			if (this.taskInfo.TryGetValue(playerId, out var taskInfo))
+			{
+				var summary =
+					FinalSummary.PlayerSummary.Create(
+						playerInfo, role, ghostRole, taskInfo);
+
+				summaries.Add(summary);
+			}
+		}
+
+		return summaries;
+	}
+
+	private void removeAddPlusWinner()
+	{
+		foreach (Player winner in this.tempData.PlusedWinner)
+		{
+			ExtremeRolesPlugin.Logger.LogInfo($"Remove Winner(Dupe Player) : {winner.PlayerName}");
+			this.tempData.Remove(winner);
+		}
+	}
+
+	private void addNeutralWiner()
+	{
+		if (!ExtremeGameModeManager.Instance.ShipOption.DisableNeutralSpecialForceEnd)
+		{
+			return;
+		}
+
+		HashSet<(ExtremeRoleId, int)> winRole = new HashSet<(ExtremeRoleId, int)>();
+
+		foreach (Player playerInfo in GameData.Instance.AllPlayers.GetFastEnumerator())
+		{
+			if (!ExtremeRoleManager.TryGetRole(playerInfo.PlayerId, out var role))
+			{
+				continue;
+			}
+
+			if (role is MultiAssignRoleBase multiAssignRole &&
+				multiAssignRole.AnotherRole is not null &&
+				tryAddWinRole(
+					multiAssignRole.AnotherRole,
+						playerInfo, in winRole))
+			{
+				continue;
+			}
+			tryAddWinRole(role, playerInfo, in winRole);
+		}
+	}
+
+	private bool tryAddWinRole(
+		in SingleRoleBase role,
+		in Player playerInfo,
+		in HashSet<(ExtremeRoleId, int)> winRole)
+	{
+		int gameControlId = role.GameControlId;
+
+		if (ExtremeGameModeManager.Instance.ShipOption.IsSameNeutralSameWin)
+		{
+			gameControlId = PlayerStatistics.SameNeutralGameControlId;
+		}
+
+		var logger = ExtremeRolesPlugin.Logger;
+		var item = (role.Id, gameControlId);
+
+		if (winRole.Contains(item))
+		{
+			logger.LogInfo($"Add Winner(Reason:Additional Neutral Win) : {playerInfo.PlayerName}");
+			this.tempData.Add(playerInfo);
+			return true;
+		}
+		else if (role.IsNeutral() && role.IsWin)
+		{
+			winRole.Add(item);
+
+			logger.LogInfo($"Add Winner(Reason:Additional Neutral Win) : {playerInfo.PlayerName}");
+			this.tempData.Add(playerInfo);
+			return true;
+		}
+		return false;
+	}
+
+	private void replaceWinner()
+	{
+		switch (this.roleGameOverReason)
+		{
+			case RoleGameOverReason.AssassinationMarin:
+			case RoleGameOverReason.TeroristoTeroWithShip:
+				this.tempData.Clear();
+				foreach (Player player in GameData.Instance.AllPlayers.GetFastEnumerator())
+				{
+					if (ExtremeRoleManager.TryGetRole(player.PlayerId, out var role) &&
+						role.IsImpostor())
+					{
+						this.tempData.Add(player);
+					}
+				}
+				break;
+			case RoleGameOverReason.AliceKilledByImposter:
+			case RoleGameOverReason.AliceKillAllOther:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Alice);
+				break;
+			case RoleGameOverReason.JackalKillAllOther:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Jackal, ExtremeRoleId.Sidekick);
+				break;
+			case RoleGameOverReason.LoverKillAllOther:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Lover);
+				break;
+			case RoleGameOverReason.ShipFallInLove:
+				replaceWinnerToSpecificRolePlayer(
+					ExtremeRoleId.Lover);
+				break;
+			case RoleGameOverReason.TaskMasterGoHome:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.TaskMaster);
+				break;
+			case RoleGameOverReason.MissionaryAllAgainstGod:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Missionary);
+				break;
+			case RoleGameOverReason.JesterMeetingFavorite:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Jester);
+				break;
+			case RoleGameOverReason.YandereKillAllOther:
+			case RoleGameOverReason.YandereShipJustForTwo:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Yandere);
+				break;
+			case RoleGameOverReason.VigilanteKillAllOther:
+			case RoleGameOverReason.VigilanteNewIdealWorld:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Vigilante);
+				break;
+			case RoleGameOverReason.MinerExplodeEverything:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Miner);
+				break;
+			case RoleGameOverReason.EaterAllEatInTheShip:
+			case RoleGameOverReason.EaterAliveAlone:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Eater);
+				break;
+			case RoleGameOverReason.TraitorKillAllOther:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Traitor);
+				break;
+			case RoleGameOverReason.QueenKillAllOther:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Queen, ExtremeRoleId.Servant);
+				break;
+			case RoleGameOverReason.UmbrerBiohazard:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Umbrer);
+				break;
+			case RoleGameOverReason.KidsTooBigHomeAlone:
+			case RoleGameOverReason.KidsAliveAlone:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Delinquent);
+				break;
+			case RoleGameOverReason.HatterEndlessTeaTime:
+			case RoleGameOverReason.HatterTeaPartyTime:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Hatter);
+				break;
+			case RoleGameOverReason.ArtistShipToArt:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Artist);
+				break;
+			case RoleGameOverReason.TuckerShipIsExperimentStation:
+				replaceWinnerToSpecificNeutralRolePlayer(
+					ExtremeRoleId.Tucker, ExtremeRoleId.Chimera);
+				break;
+			default:
+				break;
+		}
+	}
+
+	private void replaceWinnerToSpecificNeutralRolePlayer(
+		params ExtremeRoleId[] roles)
+	{
+		ExtremeRolesPlugin.Logger.LogInfo("Clear Winner(Reason:Neautal Win)");
+		this.tempData.Clear();
+
+		foreach (var player in this.neutralNoWinner)
+		{
+			if (!ExtremeRoleManager.TryGetRole(player.PlayerId, out var role))
+			{
+				continue;
+			}
+
+			if (roles.Contains(role.Id))
+			{
+				addSpecificNeutralRoleToSameControlIdPlayer(role, player);
+			}
+			else if (
+				role is MultiAssignRoleBase multiAssignRole &&
+				multiAssignRole.AnotherRole is not null &&
+				roles.Contains(multiAssignRole.AnotherRole.Id))
+			{
+				addSpecificNeutralRoleToSameControlIdPlayer(role, player);
+			}
+		}
+	}
+
+	private void addSpecificRoleToSameControlIdPlayer(in SingleRoleBase role, in Player player)
+	{
+		if (this.winGameControlId != int.MaxValue &&
+			this.winGameControlId == role.GameControlId)
+		{
+			ExtremeRolesPlugin.Logger.LogInfo($"Add Winner(Reason:Win this role) : {player.PlayerName}");
+			this.tempData.Add(player);
+		}
+	}
+
+	private void replaceWinnerToSpecificRolePlayer(
+		ExtremeRoleId roleId)
+	{
+		this.tempData.Clear();
+
+		foreach (var player in GameData.Instance.AllPlayers.GetFastEnumerator())
+		{
+			if (!ExtremeRoleManager.TryGetRole(player.PlayerId, out var role))
+			{
+				continue;
+			}
+
+			if (role.Id == roleId)
+			{
+				addSpecificRoleToSameControlIdPlayer(role, player);
+			}
+			else if (
+				role is MultiAssignRoleBase multiAssignRole &&
+				multiAssignRole.AnotherRole is not null &&
+				multiAssignRole.AnotherRole.Id == roleId)
+			{
+				addSpecificRoleToSameControlIdPlayer(multiAssignRole.AnotherRole, player);
+			}
+		}
+	}
+
+	private void addSpecificNeutralRoleToSameControlIdPlayer(in SingleRoleBase role, in Player player)
+	{
+		if (ExtremeGameModeManager.Instance.ShipOption.IsSameNeutralSameWin)
+		{
+			ExtremeRolesPlugin.Logger.LogInfo($"Add Winner(Reason:Win this role) : {player.PlayerName}");
+			this.tempData.Add(player);
+		}
+		else
+		{
+			addSpecificRoleToSameControlIdPlayer(role, player);
+		}
+	}
+
+	private void mergeWinner()
+	{
+		var logger = ExtremeRolesPlugin.Logger;
+
+		logger.LogInfo($"-- Start: merge plused win player --");
+		foreach (var player in this.tempData.PlusedWinner)
+		{
+			logger.LogInfo($"marge to winner:{player.PlayerName}");
+			this.tempData.Add(player);
+		}
+		logger.LogInfo($"-- End: merge plused win player --");
+	}
+
+	private void addGhostRoleWinner()
+	{
+		var logger = ExtremeRolesPlugin.Logger;
+
+		logger.LogInfo($"-- Start: add ghostrole win player --");
+
+		foreach (var (playerInfo, winCheckRole) in this.ghostWinCheckRole)
+		{
+			if (winCheckRole.IsWin(this.gameOverReason, playerInfo))
+			{
+				ExtremeRolesPlugin.Logger.LogInfo($"Add Winner(Reason:Ghost Role win) : {playerInfo.PlayerName}");
+				this.tempData.AddWithPlus(playerInfo);
+			}
+		}
+		logger.LogInfo($"-- End: add ghostrole win player --");
+	}
+
+	private void modifiedWinner()
+	{
+		var logger = ExtremeRolesPlugin.Logger;
+		logger.LogInfo($"-- Start: modified win player --");
+		foreach (var (playerInfo, winModRole) in modRole)
+		{
+			winModRole.ModifiedWinPlayer(
+				playerInfo,
+				 ExtremeRolesPlugin.ShipState.EndReason, // 更新され続けるため、新しいのを常に渡す
+				in this.tempData);
+		}
+		logger.LogInfo($"-- End: modified win player --");
+	}
+}
