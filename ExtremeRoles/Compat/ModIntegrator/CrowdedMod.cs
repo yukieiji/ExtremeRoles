70ded310f8ab49db9c83108565ab19d7849f8033
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-12-27 22:04:50 +0900

    feat:support CrowdedMod

---
+++ ExtremeRoles/Compat/ModIntegrator/CrowdedMod.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+using BepInEx;
+
+
+#nullable enable
+
+namespace ExtremeRoles.Compat.ModIntegrator;
+
+public sealed class CrowdedMod(PluginInfo plugin) : ModIntegratorBase(Guid, plugin)
+{
+	public const string Guid = "CrowdedMod";
+
+	protected override void PatchAll(Harmony harmony)
+	{ }
+}
