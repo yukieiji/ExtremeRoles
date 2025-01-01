0d46f268fa79f23cad886fe9e3a5bce5cd49a851
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-01-01 14:56:54 +0900

    feat:disable role logics

--- ExtremeRoles.Test/Helper/RandomRoleProvider.cs
+++ ExtremeRoles.Test/Helper/RandomRoleProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 using ExtremeRoles.Roles;
 using ExtremeRoles.Roles.API;
@@ -7,8 +8,21 @@ namespace ExtremeRoles.Test.Helper;
 
 public static class RandomRoleProvider
 {
+	public static IReadOnlySet<ExtremeRoleId> IgnoreRole = new HashSet<ExtremeRoleId>()
+	{
+		ExtremeRoleId.Monika
+	};
+	public static IReadOnlySet<CombinationRoleType> IgnoreCombRole = new HashSet<CombinationRoleType>()
+	{
+		CombinationRoleType.Avalon
+	};
+
 	public static SingleRoleBase GetNormalRole()
-		=> ExtremeRoleManager.NormalRole.Values.OrderBy(x => RandomGenerator.Instance.Next()).First();
+		=> ExtremeRoleManager.NormalRole.Values.OrderBy(
+			x => IgnoreRole.Contains(x.Id) ? 
+			int.MaxValue : RandomGenerator.Instance.Next()).First();
 	public static byte GetCombRole()
-		=> ExtremeRoleManager.CombRole.Keys.OrderBy(x => RandomGenerator.Instance.Next()).First();
+		=> ExtremeRoleManager.CombRole.Keys.OrderBy(
+			x => IgnoreCombRole.Contains((CombinationRoleType)x) ? 
+				int.MaxValue : RandomGenerator.Instance.Next()).First();
 }
