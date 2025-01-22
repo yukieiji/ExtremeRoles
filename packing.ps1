b9211d9f2ee8e20fade5b6e749abd681ca7f4f94
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-01-23 01:10:35 +0900

    feat:x64 build for MS Store

--- packing.ps1
+++ packing.ps1
@@ -11,21 +11,27 @@ mkdir -Path workspace -Force
 
 Write-Host "Build Complete!!"
 
+
 Write-Host "Download BepInEx...."
-Invoke-WebRequest "https://builds.bepinex.dev/projects/bepinex_be/671/BepInEx-Unity.IL2CPP-win-x86-6.0.0-be.671%2B9caf61d.zip" -OutFile workspace/bepinex.zip
-Expand-Archive -Path workspace/bepinex.zip -DestinationPath workspace/bepinex -Force
+Invoke-WebRequest "https://builds.bepinex.dev/projects/bepinex_be/671/BepInEx-Unity.IL2CPP-win-x86-6.0.0-be.671%2B9caf61d.zip" -OutFile workspace/bepinex_x86.zip
+Invoke-WebRequest "https://builds.bepinex.dev/projects/bepinex_be/671/BepInEx-Unity.IL2CPP-win-x64-6.0.0-be.671%2B9caf61d.zip" -OutFile workspace/bepinex_x64.zip
+Expand-Archive -Path workspace/bepinex_x86.zip -DestinationPath workspace/bepinex_x86 -Force
+Expand-Archive -Path workspace/bepinex_x64.zip -DestinationPath workspace/bepinex_x64 -Force
+
 
 Write-Host "Create Packing..."
-mkdir -Path workspace/bepinex/BepInEx/config -Force
-mkdir -Path workspace/bepinex/BepInEx/plugins -Force
+mkdir -Path workspace/bepinex_x86/BepInEx/config -Force
+mkdir -Path workspace/bepinex_x86/BepInEx/plugins -Force
 
-New-Item workspace/bepinex/steam_appid.txt
-Set-Content workspace/bepinex/steam_appid.txt '945360'
 
-Copy-Item -Path ExtremeRoles/Resources/Config/*.cfg -Destination workspace/bepinex/BepInEx/config -Force -Recurse
+Write-Host "Create default package"
+New-Item workspace/bepinex_x86/steam_appid.txt
+Set-Content workspace/bepinex_x86/steam_appid.txt '945360'
 
-Copy-Item -Path workspace/bepinex -Destination workspace/ExtremeRoles -Force -Recurse
-Copy-Item -Path workspace/bepinex -Destination workspace/ExtremeRolesWithSkins -Force -Recurse
+Copy-Item -Path ExtremeRoles/Resources/Config/*.cfg -Destination workspace/bepinex_x86/BepInEx/config -Force -Recurse
+
+Copy-Item -Path workspace/bepinex_x86 -Destination workspace/ExtremeRoles -Force -Recurse
+Copy-Item -Path workspace/bepinex_x86 -Destination workspace/ExtremeRolesWithSkins -Force -Recurse
 mkdir -Path workspace/dll -Force
 
 Copy-Item -Path ExtremeRoles/bin/Release/net6.0/ExtremeRoles.dll -Destination workspace/ExtremeRoles/BepInEx/plugins/ExtremeRoles.dll -Force -Recurse
@@ -33,7 +39,23 @@ Copy-Item -Path ExtremeRoles/bin/Release/net6.0/ExtremeRoles.dll -Destination wo
 Copy-Item -Path ExtremeRoles/bin/Release/net6.0/ExtremeRoles.dll -Destination workspace/ExtremeRolesWithSkins/BepInEx/plugins/ExtremeRoles.dll -Force -Recurse
 Copy-Item -Path ExtremeSkins/bin/Release/net6.0/ExtremeSkins.dll -Destination workspace/ExtremeRolesWithSkins/BepInEx/plugins/ExtremeSkins.dll -Force -Recurse
 
+
+Write-Host "Create MSStore package"
+Copy-Item -Path ExtremeRoles/Resources/Config/*.cfg -Destination workspace/bepinex_x64/BepInEx/config -Force -Recurse
+
+Copy-Item -Path workspace/bepinex_x64 -Destination workspace/MSStore_ExtremeRoles -Force -Recurse
+Copy-Item -Path workspace/bepinex_x64 -Destination workspace/MSStore_ExtremeRolesWithSkins -Force -Recurse
+mkdir -Path workspace/dll -Force
+
+Copy-Item -Path ExtremeRoles/bin/Release/net6.0/ExtremeRoles.dll -Destination workspace/MSStore_ExtremeRoles/BepInEx/plugins/ExtremeRoles.dll -Force -Recurse
+
+Copy-Item -Path ExtremeRoles/bin/Release/net6.0/ExtremeRoles.dll -Destination workspace/MSStore_ExtremeRolesWithSkins/BepInEx/plugins/ExtremeRoles.dll -Force -Recurse
+Copy-Item -Path ExtremeSkins/bin/Release/net6.0/ExtremeSkins.dll -Destination workspace/MSStore_ExtremeRolesWithSkins/BepInEx/plugins/ExtremeSkins.dll -Force -Recurse
+
+
+Write-Host "Copy DLL"
 Copy-Item -Path ExtremeRoles/bin/Release/net6.0/ExtremeRoles.dll -Destination workspace/dll/ExtremeRoles.dll -Force -Recurse
 Copy-Item -Path ExtremeSkins/bin/Release/net6.0/ExtremeSkins.dll -Destination workspace/dll/ExtremeSkins.dll -Force -Recurse
 Copy-Item -Path ExtremeVoiceEngine/bin/Release/net6.0/ExtremeVoiceEngine.dll -Destination workspace/dll/ExtremeVoiceEngine.dll -Force -Recurse
 Copy-Item -Path ExtremeRoles.Test/bin/Release/net6.0/ExtremeRoles.Test.dll -Destination workspace/dll/ExtremeRoles.Test.dll -Force -Recurse
+
