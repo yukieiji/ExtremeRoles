7d9a31f51025f6dfa3cfd5096189a7d68199705b
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-01-27 21:11:16 +0900

    feat:interfaces

--- ExtremeRoles/Roles/API/SingleRoleBase.Action.cs
+++ ExtremeRoles/Roles/API/SingleRoleBase.Action.cs
@@ -1,26 +1,31 @@
-﻿namespace ExtremeRoles.Roles.API
+﻿using ExtremeRoles.Roles.API.Interface;
+
+#nullable enable
+
+namespace ExtremeRoles.Roles.API;
+
+public abstract partial class SingleRoleBase
 {
-    public abstract partial class SingleRoleBase
+	public IAbility? Ability { get; protected set; }
+
+	public virtual void ExiledAction(
+        PlayerControl rolePlayer)
     {
-        public virtual void ExiledAction(
-            PlayerControl rolePlayer)
-        {
-            return;
-        }
-
-        public virtual void RolePlayerKilledAction(
-            PlayerControl rolePlayer,
-            PlayerControl killerPlayer)
-        {
-            return;
-        }
-
-        public virtual bool TryRolePlayerKillTo(
-            PlayerControl rolePlayer,
-            PlayerControl targetPlayer) => true;
-
-        public virtual bool TryRolePlayerKilledFrom(
-            PlayerControl rolePlayer,
-            PlayerControl fromPlayer) => true;
+        return;
     }
+
+    public virtual void RolePlayerKilledAction(
+        PlayerControl rolePlayer,
+        PlayerControl killerPlayer)
+    {
+        return;
+    }
+
+    public virtual bool TryRolePlayerKillTo(
+        PlayerControl rolePlayer,
+        PlayerControl targetPlayer) => true;
+
+    public virtual bool TryRolePlayerKilledFrom(
+        PlayerControl rolePlayer,
+        PlayerControl fromPlayer) => true;
 }
