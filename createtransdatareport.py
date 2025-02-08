7f79bcc6dcbbc3edee15611244b9fd406f012353
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-02-09 05:57:07 +0900

    feat:resx trans checker

--- createtransdatareport.py
+++ createtransdatareport.py
@@ -1,18 +1,23 @@
 import os
 import sys
+import glob
+
+from xml.etree import ElementTree as ET
 
 from pylightxl import readxl
 
 WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
 
-EXTREMERORLS_IN_FILE = os.path.join(WORKING_DIR, "ExtremeRolesTransData.xlsx")
-EXTREMERORLS_OUT_FILE = os.path.join(WORKING_DIR, "ExtremeRoles", "Resources", "JsonData", "Language.json")
-
+EXTREMERORLS_FILE = os.path.join(WORKING_DIR, "ExtremeRoles", "Translation", "resx")
 EXTREMESKIN_IN_FILE = os.path.join(WORKING_DIR, "ExtremeSkinsTransData.xlsx")
-EXTREMESKIN_OUT_FILE = os.path.join(WORKING_DIR, "ExtremeSkins", "Resources", "LangData", "stringData.json")
 
-SUPPORT_LANG = {0:'English', 11:'Japanese', 13: 'SChinese'}
+SUPPORT_LANG = {0:'English', 11:'Japanese', 13: 'SChinese', 14: "TChinese"}
+SUPPORT_RESX_LANG = {'en-US':'English', "zh-Hans": 'SChinese', "zh-Hant": "TChinese"}
 TAG = 'report'
+
+RESX_KEY_TAG = "data"
+RESX_KEY_ATTR = "name"
+RESX_VALUE_TAG = "value"
 
 def get_trans_data_check(file_name:str)-> dict[str, list[str]]:
   wb = readxl(file_name)
@@ -55,6 +60,72 @@ def get_trans_data_check(file_name:str)-> dict[str, list[str]]:
 
   return missing_data
 
+class ResXMissingData:
+  def __init__(self):
+    self.data : dict[str, list[str]] = {}
+  def append(self, lang:str, key:str):
+    if not (lang in self.data):
+      self.data[lang] = []
+    self.data[lang].append(key)
+
+
+def get_resx_trans_missing_data(path:str) -> dict[str, dict[str, list[str]]]:
+  ja_resx = set(glob.glob(os.path.join(path, "*.resx")))
+
+  # `*.*.resx` を検索
+  other_resx = set(glob.glob(os.path.join(path, "*.*.resx")))
+
+  base_trans : dict[str, dict[str, str]] = {}
+
+  for resx_file in ja_resx:
+    tree = ET.parse(resx_file)
+    root = tree.getroot()
+
+    file_name = resx_file.split('.')[0]
+    base_trans[file_name] = {}
+
+    for data in root.findall(RESX_KEY_TAG):
+        key = data.get(RESX_KEY_ATTR)  # <data name="Key"> の "Key" を取得
+        if key is None:
+          continue
+        value_element = data.find(RESX_VALUE_TAG)
+        if value_element is None:
+          continue
+        val = value_element.text
+        if val is None:
+          continue
+        base_trans[file_name][key] = val  # <value> の内容を辞書に格納
+  result : dict[str, dict[str, list[str]]] = {}
+  for resx in other_resx:
+
+    splited = resx.split('.')
+    lang = splited[1]
+    if lang not in SUPPORT_RESX_LANG:
+      continue
+    lang = SUPPORT_RESX_LANG[lang]
+    file_name = splited[0]
+
+    tree = ET.parse(resx)
+    root = tree.getroot()
+
+    missing_data = ResXMissingData()
+
+    for key in base_trans[file_name].keys():
+      data_element = root.find(f".//data[@name='{key}']")
+      if data_element is None:
+        missing_data.append(lang, key)
+        continue
+      value_element = data_element.find(RESX_VALUE_TAG)
+      if value_element is None:
+        missing_data.append(lang, key)
+        continue
+      if val is None:
+          missing_data.append(lang, key)
+    if len(missing_data.data) == 0:
+      continue
+    result[resx] = missing_data.data
+  return result
+
 def convert_md_table(data: dict[str, list[str]]) -> str:
   result = '| Languages | Missing TransKeys |\n| --- | --- |'
 
@@ -71,13 +142,19 @@ def convert_md_table(data: dict[str, list[str]]) -> str:
 
   return result
 
-def output_md_report(build_result, *check_xlsx_file) -> None:
+def output_md_report(build_result:str, check_resx:list[str], check_xlsx_file:list[str]) -> None:
   
   result = f'### GitHub Actions MSBuilds\n\n - Build Result : {build_result}\n\n'
   result = f'{result}### Translation Checker Report\n'
 
+  for path in check_resx:
+    resx_result = get_resx_trans_missing_data(path)
+    for resx, data in resx_result.items():
+      result = f'{result}\n - FileName:**{os.path.basename(resx)}**\n\n'
+      result = f'{result}{convert_md_table(data)}\n'
+
   for file in check_xlsx_file:
-    result = f'{result}\n - FileName:{os.path.basename(file)}\n\n'
+    result = f'{result}\n - FileName:**{os.path.basename(file)}**\n\n'
     result = f'{result}{convert_md_table(get_trans_data_check(file))}\n'
 
   with open(os.path.join(WORKING_DIR, '.github/workflows/comment.md'), 'w', encoding='UTF-8') as md:
@@ -85,4 +162,4 @@ def output_md_report(build_result, *check_xlsx_file) -> None:
 
 
 if __name__ == "__main__":
-  output_md_report(sys.argv[1], EXTREMESKIN_IN_FILE)
+  output_md_report(sys.argv[1], [EXTREMERORLS_FILE], [EXTREMESKIN_IN_FILE])
