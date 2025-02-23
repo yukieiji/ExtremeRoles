b3f9a6eb40e4e3182b4bc0a7b2d294de7e5ea967
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-02-23 21:18:22 +0900

    change:namespaces

--- ExtremeRoles/Roles/API/Interface/Ability/IAbility.cs
+++ ExtremeRoles/Roles/API/Interface/Ability/IAbility.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExtremeRoles.Roles.API.Interface;
+namespace ExtremeRoles.Roles.API.Interface.Ability;
 
 public interface IAbility
 { }
