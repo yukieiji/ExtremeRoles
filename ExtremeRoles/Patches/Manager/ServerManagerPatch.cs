176db38602fab4da57a6405aae0ccc6be5d35009
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-03-27 00:49:25 +0900

    hotfix : remove custom server logics

---
+++ ExtremeRoles/Patches/Manager/ServerManagerPatch.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+using HarmonyLib;
+
+using Innersloth.IO;
+using Newtonsoft.Json;
+
+namespace ExtremeRoles.Patches.Manager;
+
+// v16だとカスタムサーバー等が生き残ってると予想外の動作が発生するのでバニラサーバー以外があると消し飛ばす処理を実装しておく
+[HarmonyPatch(typeof(ServerManager), nameof(ServerManager.LoadServers))]
+public static class ServerManagerLoadServersPatch
+{
+	public static void Prefix(ServerManager __instance)
+	{
+		if (!FileIO.Exists(__instance.serverInfoFileJson))
+		{
+			return;
+		}
+
+		var jsonServerData = JsonConvert.DeserializeObject<ServerManager.JsonServerData>(
+			FileIO.ReadAllText(__instance.serverInfoFileJson), new JsonSerializerSettings
+			{
+				TypeNameHandling = TypeNameHandling.Auto
+			});
+		jsonServerData.CleanAndMerge(ServerManager.DefaultRegions);
+
+		var validData = jsonServerData.Regions.Where(
+			x => x.TranslateName is
+				StringNames.ServerAS or
+				StringNames.ServerEU or
+				StringNames.ServerNA
+			).ToArray();
+
+		if (validData.Length != jsonServerData.Regions.Count)
+		{
+			FileIO.Delete(__instance.serverInfoFileJson);
+		}
+	}
+}
