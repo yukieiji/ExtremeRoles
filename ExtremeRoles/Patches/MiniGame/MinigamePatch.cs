b9cf36983c8c9db13fdb214e0f07f3c4e954bda6
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-03-27 21:23:58 +0900

    feat: minigame patch

---
+++ ExtremeRoles/Patches/MiniGame/MinigamePatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace ExtremeRoles.Patches.MiniGame;
+
+// From : https://github.com/SubmergedAmongUs/Submerged/blob/main/Submerged/Minigames/Patches/FixMissingLoggerPatches.cs
+[HarmonyPatch(typeof(Minigame), nameof(Minigame.Begin))]
+public static class MinigameBeginPatch
+{
+	private static readonly Logger logger = new Logger(Logger.Category.Gameplay, "Minigame");
+
+	public static void Prefix(Minigame __instance)
+	{
+		__instance.logger ??= logger;
+	}
+}
