ae1e6ce9c2da2605cf793c49d2e46d2c4bf47175
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-03-30 03:16:37 +0900

    refactor : IRoleHasParents

---
+++ ExtremeRoles/Roles/Solo/Neutral/Jackal/SidekickStatus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+using ExtremeRoles.Roles.API.Interface;
+
+namespace ExtremeRoles.Roles.Solo.Neutral.Jackal;
+
+public class SidekickStatus(byte playerId, JackalRole jackal) : IStatusModel, IParentChainStatus
+{
+	public byte Parent { get; } = playerId;
+	private readonly List<byte> sks = jackal.SidekickPlayerId;
+
+	public void RemoveParent(byte rolePlayerId)
+	{
+		this.sks.Remove(rolePlayerId);
+	}
+
+	public void ClearSidekick()
+	{
+		this.sks.Clear();
+	}
+}
---
+++ ExtremeRoles/Roles/Solo/Neutral/Queen/ServantStatus.cs
@@ -0,0 +1,15 @@
+﻿using ExtremeRoles.Roles.API.Interface;
+
+namespace ExtremeRoles.Roles.Solo.Neutral.Queen;
+
+#nullable enable
+
+public sealed class ServantStatus(byte queenRolePlayerId, QueenRole queen) : IStatusModel, IParentChainStatus
+{
+	public byte Parent { get; } = queenRolePlayerId;
+
+	public void RemoveParent(byte rolePlayerId)
+	{
+		queen.RemoveServantPlayer(rolePlayerId);
+	}
+}
