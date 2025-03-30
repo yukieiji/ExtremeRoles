c055cf3ff9fe6d028770121a03cf7ea1ad690f74
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-03-30 19:59:07 +0900

    fix:null

--- ExtremeRoles/Roles/Solo/Impostor/Terorist.cs
+++ ExtremeRoles/Roles/Solo/Impostor/Terorist.cs
@@ -100,7 +100,8 @@ public sealed class Terorist : SingleRoleBase, IRoleAutoBuildAbility
 
     protected override void RoleSpecificInit()
     {
-		if (ShipStatus.Instance.Systems.TryGetValue(SystemTypes.Sabotage, out var system) &&
+		if (ShipStatus.Instance != null &&
+			ShipStatus.Instance.Systems.TryGetValue(SystemTypes.Sabotage, out var system) &&
 			system.IsTryCast<SabotageSystemType>(out var saboSystem))
 		{
 			this.saboSystem = saboSystem;
