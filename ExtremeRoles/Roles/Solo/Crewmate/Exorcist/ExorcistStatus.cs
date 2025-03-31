35624a4f149012e41a1b4421d606879089d6b6e6
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-03-31 22:02:49 +0900

    feat: exorcists

---
+++ ExtremeRoles/Roles/Solo/Crewmate/Exorcist/ExorcistStatus.cs
@@ -0,0 +1,37 @@
+﻿using ExtremeRoles.Helper;
+using ExtremeRoles.Roles.API.Interface;
+
+namespace ExtremeRoles.Roles.Solo.Crewmate.Exorcist;
+
+public sealed class ExorcistStatus : IStatusModel
+{
+	private readonly ExorcistRole exorcist;
+	private readonly float range;
+	private float awakeFakeImpTaskGage;
+
+	public NetworkedPlayerInfo CurTarget => Player.GetDeadBodyInfo(this.range);
+
+	public ExorcistStatus(
+		ExorcistRole exorcist,
+		float awakeFakeImpTaskGage,
+		float range)
+	{
+		this.awakeFakeImpTaskGage = awakeFakeImpTaskGage;
+		this.exorcist = exorcist;
+		this.exorcist.FakeImposter = awakeFakeImpTaskGage <= 0.0f;
+		this.range = range;
+	}
+
+	public void FrameUpdate(PlayerControl player)
+	{
+		if (this.awakeFakeImpTaskGage <= 0.0f)
+		{
+			return;
+		}
+		if (Player.GetPlayerTaskGage(player) >= this.awakeFakeImpTaskGage)
+		{
+			this.exorcist.FakeImposter = true;
+			this.awakeFakeImpTaskGage = -1.0f;
+		}
+	}
+}
