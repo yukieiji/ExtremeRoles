33a3191a41e7bd1afbcc63b445f20d54c987f309
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-04-22 04:48:12 +0900

    feat:am ban

---
+++ ExtremeRoles/Patches/PlayerBanDataPatch.cs
@@ -0,0 +1,14 @@
+﻿using AmongUs.Data.Player;
+using HarmonyLib;
+
+namespace ExtremeRoles.Patches;
+
+
+[HarmonyPatch(typeof(PlayerBanData), nameof(PlayerBanData.IsBanned))]
+public static class PlayerBanDataPatch
+{
+	public static void Postfix(ref bool __result)
+	{
+		__result = false;
+	}
+}
