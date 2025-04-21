53fd670d8b6a0fb233a5a91b9649d9174f25e792
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-04-22 05:09:50 +0900

    fix: april fools toggles

---
+++ ExtremeRoles/Helper/AprilFools.cs
@@ -0,0 +1,19 @@
+﻿using ExtremeRoles.Extension.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtremeRoles.Helper;
+
+public static class AprilFools
+{
+	public static void UpdateApilSkinToggle(
+		CreateGameOptions instance)
+	{
+		var mng = ServerManager.Instance;
+		instance.AprilFoolsToggle.SetActive(
+			mng != null && mng.IsCustomServer());
+	}
+}
