4fc81139c0b2074ff7aa62c6be3562f3d6e3c2fe
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-05-03 00:53:31 +0900

    fix:compile error

--- ExtremeRoles/Roles/Solo/Impostor/Scavenger.cs
+++ ExtremeRoles/Roles/Solo/Impostor/Scavenger.cs
@@ -1094,7 +1094,7 @@ public sealed class Scavenger : SingleRoleBase, IRoleUpdate, IRoleAbility
 		if (this.abilityText == null)
 		{
 			this.abilityText = UnityObject.Instantiate(
-				FastDestroyableSingleton<HudManager>.Instance.KillButton.cooldownTimerText,
+				HudManager.Instance.KillButton.cooldownTimerText,
 				Camera.main.transform, false);
 			this.abilityText.transform.localPosition = new Vector3(0.0f, 0.0f, -250.0f);
 			this.abilityText.enableWordWrapping = false;
