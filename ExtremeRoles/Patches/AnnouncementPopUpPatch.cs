d391c7762b6e3025ba0e0ce0fc3473f24dd96ef1
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-05-25 05:04:45 +0900

    fix: announce patch

--- ExtremeRoles/Patches/AnnouncementPopUpPatch.cs
+++ ExtremeRoles/Patches/AnnouncementPopUpPatch.cs
@@ -1,19 +1,33 @@
 ﻿
 using HarmonyLib;
-using Il2CppSystem.Collections;
 
 using ExtremeRoles.Module;
+using Il2CppSystem.Collections;
 using BepInEx.Unity.IL2CPP.Utils.Collections;
 
 namespace ExtremeRoles.Patches;
 
-[HarmonyPatch(typeof(AnnouncementPopUp), nameof(AnnouncementPopUp.Init))]
+#nullable enable
+
+[HarmonyPatch(typeof(AnnouncementPopUp._Init_d__46), nameof(AnnouncementPopUp._Init_d__46.MoveNext))]
 public static class AnnouncementPopUpInitPatch
 {
-	public static void Postfix(ref IEnumerator __result)
+	private static IEnumerator? enumerator;
+	public static void Postfix(AnnouncementPopUp._Init_d__46 __instance, ref bool __result)
 	{
-		__result = Effects.Sequence(
-			ModAnnounce.CoGetAnnounce().WrapToIl2Cpp(),
-			__result);
+		if (__result)
+		{
+			return;
+		}
+		if (enumerator == null)
+		{
+			enumerator = ModAnnounce.CoGetAnnounce().WrapToIl2Cpp();
+		}
+		__result = enumerator.MoveNext();
+		__instance.__2__current = enumerator.Current;
+		if (!__result)
+		{
+			enumerator = null;
+		}
 	}
 }
