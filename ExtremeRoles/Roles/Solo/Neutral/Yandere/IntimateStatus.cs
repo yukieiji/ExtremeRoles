bdc2eff847b63b18131ef4b1d12480afe2276df2
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-06-01 05:51:49 +0900

    feat: intimate

---
+++ ExtremeRoles/Roles/Solo/Neutral/Yandere/IntimateStatus.cs
@@ -0,0 +1,37 @@
+﻿using ExtremeRoles.Roles.API.Interface.Status;
+
+#nullable enable
+
+namespace ExtremeRoles.Roles.Solo.Neutral.Yandere;
+
+public sealed class IntimateStatus(
+	bool hasTask,
+	int qweenSeeTaskRate,
+	bool isSubTeams) : IStatusModel, ISubTeam
+{
+	private readonly bool hasTask = hasTask;
+	private readonly float qweenSeeTaskRate = qweenSeeTaskRate / 100.0f;
+
+	public bool SeeYandere { get; private set; } = false;
+
+	public NeutralSeparateTeam Main => NeutralSeparateTeam.Queen;
+	public NeutralSeparateTeam Sub => NeutralSeparateTeam.QueenSub;
+
+	public bool IsSub { get; } = isSubTeams;
+
+	public void Update(PlayerControl knightPlayer)
+	{
+		if (!(
+				this.hasTask && this.SeeYandere
+			))
+		{
+			return;
+		}
+
+		float taskGage = Helper.Player.GetPlayerTaskGage(knightPlayer);
+		if (taskGage >= this.qweenSeeTaskRate)
+		{
+			this.SeeYandere = true;
+		}
+	}
+}
