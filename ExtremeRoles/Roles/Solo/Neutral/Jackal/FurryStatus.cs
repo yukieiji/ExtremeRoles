fffb6ea6d87e3b4a61df97a8b8950ae4dbcc373a
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-06-01 01:56:41 +0900

    feat: Furry

---
+++ ExtremeRoles/Roles/Solo/Neutral/Jackal/FurryStatus.cs
@@ -0,0 +1,32 @@
+﻿using ExtremeRoles.Performance;
+using ExtremeRoles.Roles.API.Interface;
+using System.Collections.Generic;
+
+namespace ExtremeRoles.Roles.Solo.Neutral.Jackal;
+
+#nullable enable
+
+public sealed class FurryStatus : IStatusModel
+{
+	public JackalRole? TargetJackal { get; private set; }
+
+	public void Update()
+	{
+		foreach (var player in PlayerCache.AllPlayerControl)
+		{
+			if (player == null ||
+				player.Data == null ||
+				!ExtremeRoleManager.TryGetSafeCastedRole<SidekickRole>(player.PlayerId, out var sk))
+			{
+				continue;
+			}
+			var jkPlayer = GameData.Instance.GetPlayerById(sk.Parent);
+			if (jkPlayer == null ||
+				!(jkPlayer.IsDead || jkPlayer.Disconnected) ||
+				!ExtremeRoleManager.TryGetSafeCastedRole<JackalRole>(jkPlayer.PlayerId, out var jk))
+			{
+				continue;
+			}
+		}
+	}
+}
