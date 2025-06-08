9d61f3de9a90997f64cf15025b8f62394c1d984d
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-06-09 08:44:15 +0900

    feat : spawn limiter

---
+++ ExtremeRoles/Module/Interface/ISpawnLimiter.cs
@@ -0,0 +1,31 @@
+﻿using ExtremeRoles.GameMode.RoleSelector;
+using ExtremeRoles.Module.CustomOption.Interfaces;
+using ExtremeRoles.Roles.API;
+
+using System;
+
+namespace ExtremeRoles.Module.Interface;
+
+public interface ISpawnLimiter
+{
+	public int Get(ExtremeRoleType Team);
+	public bool CanSpawn(ExtremeRoleType roleType, int spawnNum = 1);
+	public void ReduceSpawnLimit(ExtremeRoleType Team, int num);
+
+	public static int ComputeSpawnNum(
+		OptionCategory category,
+		RoleSpawnOption minSpawnKey,
+		RoleSpawnOption maxSpawnKey)
+	{
+		int minSpawnNum = category.GetValue<int>((int)minSpawnKey);
+		int maxSpawnNum = category.GetValue<int>((int)maxSpawnKey);
+
+		// 最大値が最小値より小さくならないように
+		maxSpawnNum = Math.Clamp(maxSpawnNum, minSpawnNum, int.MaxValue);
+
+		return RandomGenerator.Instance.Next(minSpawnNum, maxSpawnNum + 1);
+	}
+
+	protected static int ComputePercentage(IValueOption<int> self)
+		=> (int)decimal.Multiply(self.Value, self.Range);
+}
