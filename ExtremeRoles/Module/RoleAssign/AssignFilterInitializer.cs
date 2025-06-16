8b7f8c43f11b8d22f14aa26ad214388ed48fb113
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-06-16 20:52:20 +0900

    feat : inits

--- ExtremeRoles/Module/RoleAssign/AssignFilterInitializer.cs
+++ ExtremeRoles/Module/RoleAssign/AssignFilterInitializer.cs
@@ -4,7 +4,10 @@ public class AssignFilterInitializer : IAssignFilterInitializer
 {
     public void Initialize(RoleAssignFilter filter, PreparationData data)
     {
-        // PreparationData は現在の実装では使用しないが、インターフェースに合わせて引数として受け取る
-        filter.Initialize();
+		if (data.Assign.Data.Count == 0)
+		{
+			filter.Initialize();
+			return;
+		}
     }
 }
