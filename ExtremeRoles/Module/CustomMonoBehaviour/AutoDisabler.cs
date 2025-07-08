2e6565090130a84e15f72c78d86e277fa05634f6
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-07-09 00:29:47 +0900

    fix : constructs

--- ExtremeRoles/Module/CustomMonoBehaviour/AutoDisabler.cs
+++ ExtremeRoles/Module/CustomMonoBehaviour/AutoDisabler.cs
@@ -1,10 +1,16 @@
-﻿using UnityEngine;
+using System;
+
+using UnityEngine;
 
 namespace ExtremeRoles.Module.CustomMonoBehaviour;
 
 [Il2CppRegister]
 public sealed class AutoDisabler : MonoBehaviour
 {
+	public AutoDisabler(IntPtr ptr) : base(ptr)
+	{
+	}
+
 	public void OnEnable()
 	{
 		this.gameObject.SetActive(false);
