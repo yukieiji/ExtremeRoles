d71131a10aef16a8319f8001d11faf2aac375211
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-07-19 15:51:05 +0900

    refactor : options

--- ExtremeRoles/Roles/API/SingleRoleBase.Option.cs
+++ ExtremeRoles/Roles/API/SingleRoleBase.Option.cs
@@ -1,10 +1,66 @@
 
 
-using ExtremeRoles.Module.CustomOption.Factory;
-
+using AmongUs.GameOptions;
 using ExtremeRoles.Helper;
+using ExtremeRoles.Module.CustomOption.Factory;
+using ExtremeRoles.Module.CustomOption.Interfaces;
 
 namespace ExtremeRoles.Roles.API;
+
+
+public interface IRoleSpecificOption
+{
+
+}
+
+public class CommonOption(RoleCore core, IOptionLoader loader)
+{
+	private readonly IOptionLoader loader = loader;
+	private readonly RoleCore core = core;
+
+	public bool HasOtherVison => loader.GetValue<RoleCommonOption, bool>(RoleCommonOption.HasOtherVision);
+
+	public float Vison => this.HasOtherVison ?
+		loader.GetValue<RoleCommonOption, float>(RoleCommonOption.Vision) : DefaultVison;
+	public bool IsApplyEnvironmentVision => this.HasOtherVison ?
+		loader.GetValue<RoleCommonOption, bool>(RoleCommonOption.ApplyEnvironmentVisionEffect) :
+		DefaultIsApplyEnvironmentVision;
+
+
+	private float DefaultVison => core.Team is ExtremeRoleType.Impostor ?
+		GameOptionsManager.Instance.CurrentGameOptions.GetFloat(FloatOptionNames.ImpostorLightMod) :
+		GameOptionsManager.Instance.CurrentGameOptions.GetFloat(FloatOptionNames.CrewLightMod);
+	private bool DefaultIsApplyEnvironmentVision => core.Team is not ExtremeRoleType.Impostor;
+
+	public bool HasOtherKillCool => loader.GetValue<KillerCommonOption, bool>(KillerCommonOption.HasOtherKillCool);
+	public float KillCool => HasOtherKillCool ?
+		loader.GetValue<KillerCommonOption, float>(KillerCommonOption.KillCoolDown) : 
+		Player.DefaultKillCoolTime;
+
+	public bool HasOtherKillRange => loader.GetValue<KillerCommonOption, bool>(KillerCommonOption.HasOtherKillRange);
+	public int KillRange => HasOtherKillRange ?
+		loader.GetValue<KillerCommonOption, int>(KillerCommonOption.KillRange) :
+		GameOptionsManager.Instance.CurrentGameOptions.GetInt(Int32OptionNames.KillDistance);
+}
+
+public sealed record RoleOption<T>(CommonOption Common, T Role) where T : IRoleSpecificOption;
+
+public interface IRoleOptionFactory
+{
+	public RoleCore Core { get; }
+
+	public void Build(AutoParentSetOptionCategoryFactory factory);
+}
+
+public interface ISpecificOptionLoader<T> where T : IRoleSpecificOption
+{
+	public T Load(IOptionLoader loader);
+}
+
+public interface IRoleOptionProvider
+{
+	public RoleOption<T> Provide<T>(RoleCore core) where T : IRoleSpecificOption;
+}
 
 public abstract partial class SingleRoleBase
 {
