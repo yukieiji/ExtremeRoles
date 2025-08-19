e0cb6e4ae1fc21edee2bc26fb23db014ba37b1d2
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-08-19 21:40:44 +0900

    fix: toweles

--- ExtremeRoles/Helper/GameSystem.cs
+++ ExtremeRoles/Helper/GameSystem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -46,6 +46,7 @@ public static class GameSystem
 	{
 		TaskTypes.FixWiring,
 		TaskTypes.VentCleaning,
+		TaskTypes.PickUpTowels,
 	};
 
 	public static GameObject CreateNoneReportableDeadbody(
