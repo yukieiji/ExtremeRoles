b8a2225289162d2ab6c7fcda5c8ad89589339c36
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-09-14 01:30:37 +0900

    fix : triple assigns

--- ExtremeRoles/Module/RoleAssign/RoleAssignDataBuildBehaviour/CombinationRoleAssignDataBuilder.cs
+++ ExtremeRoles/Module/RoleAssign/RoleAssignDataBuildBehaviour/CombinationRoleAssignDataBuilder.cs
@@ -65,7 +65,12 @@ public sealed class CombinationRoleAssignDataBuilder : IRoleAssignDataBuildBehav
 						Logging.Debug($"Assign missing!!");
 						continue;
 					}
-					if (role.CanHasAnotherRole)
+
+					if (role.CanHasAnotherRole && 
+						(
+							VanillaRoleProvider.IsDefaultCrewmateRole(vanillaRole) ||
+							VanillaRoleProvider.IsDefaultImpostorRole(vanillaRole)
+						))
 					{
 						anotherRoleAssignPlayer.Add(player);
 					}
@@ -171,7 +176,10 @@ public sealed class CombinationRoleAssignDataBuilder : IRoleAssignDataBuildBehav
 						combSpawnData.IsMultiAssign) &&
 					!RoleAssignFilter.Instance.IsBlock(combType));
 
-				if (!isSpawn) { continue; }
+				if (!isSpawn)
+				{
+					continue;
+				}
 
 				limiter.Reduce(ExtremeRoleType.Crewmate, reduceCrewmateRole);
 				limiter.Reduce(ExtremeRoleType.Impostor, reduceImpostorRole);
