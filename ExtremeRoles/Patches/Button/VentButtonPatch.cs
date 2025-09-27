5c9bc7ffe6af2fcc2930e731c6f765bc2b2bf57b
yukieiji <40425798+yukieiji@users.noreply.github.com>
2025-09-28 00:50:27 +0900

    fix : vent use patch

--- ExtremeRoles/Patches/Button/VentButtonPatch.cs
+++ ExtremeRoles/Patches/Button/VentButtonPatch.cs
@@ -1,20 +1,43 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 
-namespace ExtremeRoles.Patches.Button
+using ExtremeRoles.Module.CustomMonoBehaviour;
+using ExtremeRoles.Roles;
+using ExtremeRoles.Roles.API.Interface;
+
+namespace ExtremeRoles.Patches.Button;
+
+// VentButtonクラスに関するパッチ
+[HarmonyPatch(typeof(VentButton), nameof(VentButton.DoClick))]
+public static class VentButtonDoClickPatch
 {
-    // VentButtonクラスに関するパッチ
-    [HarmonyPatch(typeof(VentButton), nameof(VentButton.DoClick))]
-    public static class VentButtonDoClickPatch
+    public static bool Prefix(VentButton __instance)
     {
-        public static bool Prefix(VentButton __instance)
+		// Manually modifying the VentButton to use Vent.Use again in order to trigger the Vent.Use prefix patch
+
+		var pc = PlayerControl.LocalPlayer;
+		if (__instance.currentTarget == null ||
+			pc == null ||
+			pc.gameObject.TryGetComponent<BoxerButtobiBehaviour>(out _))
         {
-            // Manually modifying the VentButton to use Vent.Use again in order to trigger the Vent.Use prefix patch
-            if (__instance.currentTarget != null)
-            {
-                Helper.Logging.Debug($"VentButtonClicked");
-                __instance.currentTarget.Use();
-            }
-            return false;
+			return false;
         }
+
+		var (useRole, anotherUseRole) =
+			ExtremeRoleManager.GetInterfaceCastedLocalRole<IRoleUsableOverride>();
+
+		if (!(
+				(useRole is null && anotherUseRole is null) ||
+				(useRole.EnableVentButton && anotherUseRole is null) ||
+				(useRole is null && anotherUseRole.EnableVentButton) ||
+				(useRole.EnableVentButton && anotherUseRole.EnableVentButton)
+			))
+		{
+			return false;
+		}
+
+		Helper.Logging.Debug($"VentButtonClicked");
+		__instance.currentTarget.Use();
+
+		return false;
     }
 }
