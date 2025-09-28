20f387beb7a26197b8a7d7b96c4e040a7ce0a4fb
yukieiji <40425798+yukieiji@users.noreply.github.com>
2025-09-28 22:05:11 +0900

    change : charge time

--- ExtremeRoles/Module/Ability/ExtremeAbilityButton.cs
+++ ExtremeRoles/Module/Ability/ExtremeAbilityButton.cs
@@ -222,7 +222,7 @@ public class ExtremeAbilityButton
 				}
 
 				// 最大までチャージして1.0f秒後経過すると失敗として再チャージを要求
-				if (this.Timer > maxTimer + 1.0f)
+				if (this.Timer > maxTimer + 2.5f)
 				{
 					// 能力解除 => Ready => Chargingにする
 					// こうすることで能力を解除して再度チャージって形になる
