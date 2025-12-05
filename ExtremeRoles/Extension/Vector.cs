be360cb63b51cb2887bc001bdd9132fa37a33932
yukieiji <40425798+yukieiji@users.noreply.github.com>
2025-12-06 02:48:36 +0900

    fix: extentions

--- ExtremeRoles/Extension/Vector.cs
+++ ExtremeRoles/Extension/Vector.cs
@@ -25,19 +25,19 @@ public static class VectorExtension
 	public static bool IsNotCloseTo(this Vector2 @this, Vector2 other, float eps = 0.001f)
 	{
 		var diff = @this - other;
-		return diff.sqrMagnitude >= eps;
+		return diff.sqrMagnitude > eps;
 	}
 
 	public static bool IsNotCloseTo(this Vector3 @this, Vector3 other, float eps = 0.001f)
 	{
 		var diff = @this - other;
-		return diff.sqrMagnitude >= eps;
+		return diff.sqrMagnitude > eps;
 	}
 
 	public static bool IsNotCloseTo(this Vector4 @this, Vector4 other, float eps = 0.001f)
 	{
 		var diff = @this - other;
-		return diff.sqrMagnitude >= eps;
+		return diff.sqrMagnitude > eps;
 	}
 
 }
