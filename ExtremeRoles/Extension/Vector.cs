624e6335a535adb08c78ce736fc132b07e836b15
yukieiji <40425798+yukieiji@users.noreply.github.com>
2025-12-13 21:11:00 +0900

    refactor

--- ExtremeRoles/Extension/Vector.cs
+++ ExtremeRoles/Extension/Vector.cs
@@ -4,40 +4,40 @@ namespace ExtremeRoles.Extension.Vector;
 
 public static class VectorExtension
 {
-	public static bool IsCloseTo(this Vector2 @this, Vector2 other, float eps = 0.001f)
+	public static bool IsCloseTo(this Vector2 @this, Vector2 other, float sqrEps = 0.001f)
 	{
 		var diff = @this - other;
-		return diff.sqrMagnitude <= eps;
+		return diff.sqrMagnitude <= sqrEps;
 	}
 
-	public static bool IsCloseTo(this Vector3 @this, Vector3 other, float eps = 0.001f)
+	public static bool IsCloseTo(this Vector3 @this, Vector3 other, float sqrEps = 0.001f)
 	{
 		var diff = @this - other;
-		return diff.sqrMagnitude <= eps;
+		return diff.sqrMagnitude <= sqrEps;
 	}
 
-	public static bool IsCloseTo(this Vector4 @this, Vector4 other, float eps = 0.001f)
+	public static bool IsCloseTo(this Vector4 @this, Vector4 other, float sqrEps = 0.001f)
 	{
 		var diff = @this - other;
-		return diff.sqrMagnitude <= eps;
+		return diff.sqrMagnitude <= sqrEps;
 	}
 
-	public static bool IsNotCloseTo(this Vector2 @this, Vector2 other, float eps = 0.001f)
+	public static bool IsNotCloseTo(this Vector2 @this, Vector2 other, float sqrEps = 0.001f)
 	{
 		var diff = @this - other;
-		return diff.sqrMagnitude > eps;
+		return diff.sqrMagnitude > sqrEps;
 	}
 
-	public static bool IsNotCloseTo(this Vector3 @this, Vector3 other, float eps = 0.001f)
+	public static bool IsNotCloseTo(this Vector3 @this, Vector3 other, float sqrEps = 0.001f)
 	{
 		var diff = @this - other;
-		return diff.sqrMagnitude > eps;
+		return diff.sqrMagnitude > sqrEps;
 	}
 
-	public static bool IsNotCloseTo(this Vector4 @this, Vector4 other, float eps = 0.001f)
+	public static bool IsNotCloseTo(this Vector4 @this, Vector4 other, float sqrEps = 0.001f)
 	{
 		var diff = @this - other;
-		return diff.sqrMagnitude > eps;
+		return diff.sqrMagnitude > sqrEps;
 	}
 
 }
