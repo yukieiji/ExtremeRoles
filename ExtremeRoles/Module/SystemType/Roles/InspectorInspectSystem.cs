328b18b2478b9c1dd4c6b0acb7cbfc1b4cf5f506
yukieiji <40425798+yukieiji@users.noreply.github.com>
2026-01-04 15:02:33 +0900

    fix: usings

--- ExtremeRoles/Module/SystemType/Roles/InspectorInspectSystem.cs
+++ ExtremeRoles/Module/SystemType/Roles/InspectorInspectSystem.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 using Hazel;
@@ -10,7 +11,6 @@ using ExtremeRoles.Helper;
 using ExtremeRoles.Module.Interface;
 
 using UnityObject = UnityEngine.Object;
-using System.Linq;
 
 #nullable enable
 
