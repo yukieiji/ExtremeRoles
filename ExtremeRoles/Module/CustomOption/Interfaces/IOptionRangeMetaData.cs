a9870c2c90b845f6d45f85f0a887845f90c367f9
yukieiji <40425798+yukieiji@users.noreply.github.com>
2026-03-19 22:28:49 +0900

    fix: apis

--- ExtremeRoles/Module/CustomOption/Interfaces/IOptionRangeMetaData.cs
+++ ExtremeRoles/Module/CustomOption/Interfaces/IOptionRangeMetaData.cs
@@ -3,6 +3,6 @@ namespace ExtremeRoles.Module.CustomOption.Interfaces;
 public interface IOptionRangeMeta
 {
 	public string Type { get; }
-	public int Selection { get; }
+
 	public object[] Values { get; }
 }
