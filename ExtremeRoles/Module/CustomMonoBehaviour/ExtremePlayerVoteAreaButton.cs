92ea585d3f61e716c181bac80ab92be7386825d2
yukieiji <40425798+yukieiji@users.noreply.github.com>
2026-08-20 09:40:05 +0900

    fix: judge select logics

--- ExtremeRoles/Module/CustomMonoBehaviour/ExtremePlayerVoteAreaButton.cs
+++ ExtremeRoles/Module/CustomMonoBehaviour/ExtremePlayerVoteAreaButton.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Diagnostics.CodeAnalysis;
+
+using AmongUs.GameOptions;
 
 using Il2CppInterop.Runtime.Attributes;
+using UnityEngine;
 
+using ExtremeRoles.Extension.Il2Cpp;
 using ExtremeRoles.Helper;
 using ExtremeRoles.Module.Meeting;
 using ExtremeRoles.Module.SystemType.OnemanMeetingSystem;
@@ -12,6 +16,7 @@ using ExtremeRoles.Performance;
 using ExtremeRoles.Roles;
 using ExtremeRoles.Roles.API;
 using ExtremeRoles.Roles.API.Interface;
+using ExtremeRoles.Roles.Solo;
 
 #nullable enable
 
@@ -60,7 +65,55 @@ public sealed class ExtremePlayerVoteAreaButton(IntPtr ptr) : MonoBehaviour(ptr)
 		var role = ExtremeRoleManager.GetLocalPlayerRole();
 		var multiRole = role as MultiAssignRoleBase;
 
-		if (role is IRoleMeetingButtonAbility buttonRole &&
+		if (pva.JudgeOverruleButton != null &&
+			IsMultiedJudgeRole(pva, role, out var judge, out var vanillaRole, out var exrMeetingButtonRole))
+		{
+			Logging.Debug($"LocalPlayer is dual meeting ability button with judge");
+
+			// 2つ目のボタンをリセットしてから、JudgeOverruleButtonを追加する
+			group.ResetSecond();
+
+			var overruleButton = pva.JudgeOverruleButton;
+
+			// JudgeOverruleButtonの表示条件を判定する
+			if (!judge.IsBlockedByTasks() && !judge.HasAlreadyOverruledThisMeeting && judge.HasAnOverruleUse)
+			{
+				overruleButton.gameObject.SetActive(true);
+				if (PlayerControl.LocalPlayer.Data.Role.IsAffectedByComms)
+				{
+					pva.JudgeOverruleButtonCommsDisable.SetActive(true);
+					overruleButton.enabled = false;
+					overruleButton.GetComponent<SpriteRenderer>().color = Palette.DisabledClear;
+				}
+				else
+				{
+					pva.JudgeOverruleButtonCommsDisable.SetActive(false);
+					overruleButton.enabled = true;
+					overruleButton.GetComponent<SpriteRenderer>().color = Palette.White;
+				}
+				if (button.IsRecreateButtn(role.Core.Id, exrMeetingButtonRole, out var element1))
+				{
+					group.AddSecondRow(element1);
+					group.AddSecondRow(overruleButton);
+				}
+			}
+			else
+			{
+				overruleButton.gameObject.SetActive(false);
+
+				Logging.Debug("Judge can't use overrule button");
+
+				// ジャッジのオーバールールボタンが使えないということは会議能力ボタンは1つ
+				if (button.IsRecreateButtn(role.Core.Id, exrMeetingButtonRole, out var element1))
+				{
+					group.ResetFirst();
+					group.AddFirstRow(element1);
+				}
+			}
+			result = group.Flatten(startPos);
+		}
+		else if (
+			role is IRoleMeetingButtonAbility buttonRole &&
 			multiRole?.AnotherRole is IRoleMeetingButtonAbility anotherButtonRole &&
 			isOkRoleAbilityButton(pva, buttonRole) &&
 			isOkRoleAbilityButton(pva, anotherButtonRole))
@@ -116,7 +169,7 @@ public sealed class ExtremePlayerVoteAreaButton(IntPtr ptr) : MonoBehaviour(ptr)
 	}
 
 	[HideFromIl2Cpp]
-	private bool isOkRoleAbilityButton(
+	private static bool isOkRoleAbilityButton(
 		PlayerVoteArea pva,
 		IRoleMeetingButtonAbility buttonRole)
 		=> !(
@@ -127,4 +180,49 @@ public sealed class ExtremePlayerVoteAreaButton(IntPtr ptr) : MonoBehaviour(ptr)
 			pva.Parent == null ||
 			!pva.Parent.Select((int)pva.PlayerId)
 		);
+
+	[HideFromIl2Cpp]
+	private static bool IsMultiedJudgeRole(
+		PlayerVoteArea pva,	SingleRoleBase role,
+		[NotNullWhen(true)] out JudgeRole? judge,
+		[NotNullWhen(true)] out VanillaRoleWrapper? vanillaRole,
+		[NotNullWhen(true)] out IRoleMeetingButtonAbility? exrMeetingButtonRole)
+	{
+		vanillaRole = null;
+		exrMeetingButtonRole = null;
+		judge = null;
+
+		var localPlayer = PlayerControl.LocalPlayer;
+
+		// ジャッジ + 会議能力ボタン持ち役職組み合わせの判定
+		if (localPlayer != null &&
+			localPlayer.Data.Role.IsTryCast<JudgeRole>(out var judgeRole1) &&
+			role is VanillaRoleWrapper vr &&
+			vr.VanilaRoleId is RoleTypes.Judge &&
+			vr.AnotherRole is IRoleMeetingButtonAbility mt &&
+			isOkRoleAbilityButton(pva, mt))
+		{
+			vanillaRole = vr;
+			exrMeetingButtonRole = mt;
+			judge = judgeRole1;
+			return true;
+		}
+		// 会議能力ボタン持ち役職 + ジャッジの組み合わせの判定
+		else if (
+			localPlayer != null &&
+			localPlayer.Data.Role.IsTryCast<JudgeRole>(out var judgeRole2) &&
+			role is IRoleMeetingButtonAbility mt2 &&
+			isOkRoleAbilityButton(pva, mt2) &&
+			role is MultiAssignRoleBase multiRole &&
+			multiRole.AnotherRole is VanillaRoleWrapper vr2 &&
+			vr2.VanilaRoleId is RoleTypes.Judge)
+		{
+			vanillaRole = vr2;
+			exrMeetingButtonRole = mt2;
+			judge = judgeRole2;
+			return true;
+		}
+
+		return false;
+	}
 }
